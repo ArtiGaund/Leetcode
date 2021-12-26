6d42e286184c184cda1a9d0b45b1fd64bd0a13d2
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2021-12-26 11:11:23 +0530

    Time: 4 ms (37.18%), Space: 6.7 MB (21.68%) - LeetHub

---
+++ substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+       int len=s.size();
+        int count=0,max_c=0,k=3;
+        unordered_map<char,int> mp;
+        for(int i=0;i<k;i++) mp[s[i]]++;
+        count=mp.size();
+        if(count==k) max_c++;
+        for(int i=k;i<len;i++)
+        {
+            mp[s[i]]++;
+            if(mp.find(s[i-k])!=mp.end())
+            {
+                if(mp[s[i-k]]==1) mp.erase(s[i-k]);
+                else mp[s[i-k]]--;
+            }
+            count=mp.size();
+            if(count==k) max_c++;
+        }
+        return max_c;
+    }
+};
