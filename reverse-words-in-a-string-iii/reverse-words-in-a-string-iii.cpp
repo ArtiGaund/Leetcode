8894fe9ff4eb6bf652eb37627eff78a9b262931c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2021-12-28 15:54:55 +0530

    Time: 20 ms (30.80%), Space: 11.3 MB (29.92%) - LeetHub

---
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res="";
+        string temp="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!=' ') temp+=s[i];
+            else
+            {
+                reverse(temp.begin(),temp.end());
+                res+=temp;
+                res+=" ";
+                temp="";
+            }
+        }
+        if(!temp.empty())
+        {
+             reverse(temp.begin(),temp.end());
+            res+=temp;
+            res+=" ";
+        }
+        res.pop_back();
+        return res;
+    }
+};
