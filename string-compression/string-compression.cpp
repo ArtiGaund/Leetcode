d7e9c0525050161b46e764d514f5004ec7e5754c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2021-12-28 16:02:07 +0530

    Time: 4 ms (89.71%), Space: 9 MB (68.67%) - LeetHub

---
+++ string-compression/string-compression.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0,index=0;
+        while(i<chars.size())
+        {
+            int j=i;
+            while(j<chars.size() and chars[i]==chars[j]) j++;
+            chars[index++]=chars[i];
+            if(j-i>1)
+            {
+                for(char c:to_string(j-i)) chars[index++]=c;
+            }
+            i=j;
+        }
+        return index;
+    }
+};
