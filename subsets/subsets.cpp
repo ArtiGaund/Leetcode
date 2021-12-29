dc1769f9196f70df65df6afc67e028d894612bf1
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2021-12-29 06:23:16 +0530

    Time: 4 ms (56.64%), Space: 7.1 MB (82.05%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<int> sub;
+    vector<int> temp;
     vector<vector<int>> subsets(vector<int>& nums) {
         backtrack(nums,0);
         return res;
     }
     void backtrack(vector<int> &nums,int start)
     {
-        res.push_back(sub);
+        res.push_back(temp);
         for(int i=start;i<nums.size();i++)
         {
-            sub.push_back(nums[i]);
+            temp.push_back(nums[i]);
             backtrack(nums,i+1);
-            sub.pop_back();
+            temp.pop_back();
         }
     }
 };
