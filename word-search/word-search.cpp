5555a89a5c4bf719090519a2eed51eab58c03665
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2021-12-30 07:57:42 +0530

    Time: 389 ms (47.47%), Space: 8.3 MB (7.06%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
     int n,m;
-    bool exist(vector<vector<char>>& board, string word) {
-        n=board.size();
-        m=board[0].size();
+    bool exist(vector<vector<char>>& b, string w) {
+        n=b.size();
+        m=b[0].size();
         vector<vector<int>> v;
         vector<vector<bool>> vis(n,vector<bool>(m,false));
         for(int i=0;i<n;i++)
-        {
             for(int j=0;j<m;j++)
-            {
-                if(board[i][j]==word[0])
-                    v.push_back({i,j});
-            }
-        }
+                if(b[i][j]==w[0]) v.push_back({i,j});
         for(int i=0;i<v.size();i++)
-            if(dfs(v[i][0],v[i][1],vis,board,word,1)) return true;
+            if(dfs(v[i][0],v[i][1],b,w,vis,1)) return true;
         return false;
     }
-    bool dfs(int x,int y,vector<vector<bool>> &vis,vector<vector<char>> &matrix,string word,int index)
+    bool dfs(int x,int y,vector<vector<char>> &b,string w,vector<vector<bool>> &vis,int index)
     {
-        if(index==word.size()) return true;
+        if(index==w.size()) return true;
         vis[x][y]=true;
         if(isSafe(x+1,y,vis))
-        {
-            if(word[index]==matrix[x+1][y])
-                if(dfs(x+1,y,vis,matrix,word,index+1)) return true;
-        }
+            if(b[x+1][y]==w[index])
+                if(dfs(x+1,y,b,w,vis,index+1)) return true;
         if(isSafe(x-1,y,vis))
-        {
-            if(word[index]==matrix[x-1][y])
-                 if(dfs(x-1,y,vis,matrix,word,index+1)) return true;
-        }
+            if(b[x-1][y]==w[index])
+                if(dfs(x-1,y,b,w,vis,index+1)) return true;
         if(isSafe(x,y+1,vis))
-        {
-            if(word[index]==matrix[x][y+1])
-                 if(dfs(x,y+1,vis,matrix,word,index+1)) return true;
-        }
+            if(b[x][y+1]==w[index])
+                if(dfs(x,y+1,b,w,vis,index+1)) return true;
         if(isSafe(x,y-1,vis))
-        {
-            if(word[index]==matrix[x][y-1])
-                 if(dfs(x,y-1,vis,matrix,word,index+1)) return true;
-        }
+            if(b[x][y-1]==w[index])
+                if(dfs(x,y-1,b,w,vis,index+1)) return true;
         vis[x][y]=false;
         return false;
     }
