d11f0b13d4e9d68adddf141f301ad43d29a6fbad
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2021-12-31 11:10:35 +0530

    Time: 33 ms (35.37%), Space: 9.9 MB (54.25%) - LeetHub

--- combinations/combinations.cpp
+++ combinations/combinations.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> res;
+    vector<int> cur;
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> res;
-        vector<int> cur;
-        backtrack(1,n,k,res,cur);
+        backtrack(1,n,k);
         return res;
     }
-    void backtrack(int start,int last,int k,vector<vector<int>> &res,vector<int> &cur)
+    void backtrack(int start,int end,int k)
     {
         if(cur.size()==k)
         {
             res.push_back(cur);
             return;
         }
-        for(int i=start;i<=last;i++)
+        for(int i=start;i<=end;i++)
         {
             cur.push_back(i);
-            backtrack(i+1,last,k,res,cur);
+            backtrack(i+1,end,k);
             cur.pop_back();
         }
     }
