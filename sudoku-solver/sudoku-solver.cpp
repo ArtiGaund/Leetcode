44561716d5db1c0945b89f591ebca26a4d7403b7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-01 06:45:15 +0530

    Time: 2272 ms (5.02%), Space: 318.6 MB (5.02%) - LeetHub

--- sudoku-solver/sudoku-solver.cpp
+++ sudoku-solver/sudoku-solver.cpp
@@ -28,10 +28,10 @@ public:
     }
     bool isValid(vector<vector<char>> b,int r,int c,char ch)
     {
-        for(int i=0;i<b.size();i++)
+        for(int i=0;i<9;i++)
         {
-            if(b[i][c]==ch) return false;
             if(b[r][i]==ch) return false;
+            if(b[i][c]==ch) return false;
             if(b[3*(r/3)+i/3][3*(c/3)+i%3]==ch) return false;
         }
         return true;
