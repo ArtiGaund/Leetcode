c8be8275f5212370a2e3570c5221028b32932477
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-04 14:47:30 +0530

    Time: 936 ms (28.04%), Space: 9.1 MB (55.02%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int res=0;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        if(nums.size()==0) return 0;
+        int n=nums.size();
+        vector<int> sum(n);
+        sum[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--)
+            sum[i]=sum[i+1]+nums[i];
+        dfs(nums,sum,target,0);
+        return res;
+    }
+    void dfs(vector<int> &nums,vector<int> &sum,int target,int pos)
+    {
+        if(pos==nums.size())
+        {
+            if(target==0) res++;
+            return;
+        }
+        if(sum[pos]<abs(target)) return;
+        dfs(nums,sum,target+nums[pos],pos+1);
+        dfs(nums,sum,target-nums[pos],pos+1);
+    }
+};
