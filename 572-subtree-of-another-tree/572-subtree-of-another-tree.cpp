301b46273524a81823fa25f9e507b331467865dd
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-04 10:25:28 +0530

    Time: 38 ms (16.12%), Space: 28.8 MB (83.40%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -12,40 +12,16 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        stack<TreeNode*> s;
-        s.push(root);
-        while(!s.empty())
-        {
-            TreeNode* cur=s.top();
-            s.pop();
-            if(cur->val==subRoot->val)
-            {
-                if(isIdentical(cur,subRoot)) return true;
-            }
-            if(cur->right) s.push(cur->right);
-            if(cur->left) s.push(cur->left);
-        }
-        return false;
+        return traversal(root,subRoot);
     }
-    bool isIdentical(TreeNode *root,TreeNode *sub)
+    bool traversal(TreeNode *root,TreeNode *sub)
     {
-        stack<TreeNode*> s1,s2;
-        s1.push(root);
-        s2.push(sub);
-        while(!s1.empty() and !s2.empty())
-        {
-            TreeNode *cur1=s1.top();
-            s1.pop();
-            TreeNode* cur2=s2.top();
-            s2.pop();
-            if(cur1==nullptr and cur2==nullptr) continue;
-            if((cur1==nullptr and cur2!=nullptr) or (cur1!=nullptr and cur2==nullptr)) return false;
-            if(cur1->val!=cur2->val) return false;
-            s1.push(cur1->right);
-            s2.push(cur2->right);
-            s1.push(cur1->left);
-            s2.push(cur2->left);
-        }
-        return true;
+        return (root!=nullptr and (equal(root,sub) or traversal(root->left,sub) or traversal(root->right,sub)));
+    }
+    bool equal(TreeNode* root,TreeNode *sub)
+    {
+        if(root==nullptr and sub==nullptr) return true;
+        if(root==nullptr or sub==nullptr) return false;
+        return (root->val==sub->val and equal(root->left,sub->left) and equal(root->right,sub->right));
     }
 };
