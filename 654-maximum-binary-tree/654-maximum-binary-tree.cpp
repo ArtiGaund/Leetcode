9d8c6af608a11fb9c064723da3b799251927318c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-06 11:05:43 +0530

    Time: 112 ms (29.61%), Space: 39.4 MB (88.14%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -12,24 +12,23 @@
 class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        int n=nums.size();
-        return construct(nums,0,n);
-    }
-    TreeNode* construct(vector<int> &nums,int start,int end)
-    {
-        if(start==end) return nullptr;
-        int max_i=max_index(nums,start,end);
-        TreeNode *root=new TreeNode(nums[max_i]);
-        root->left=construct(nums,start,max_i);
-        root->right=construct(nums,max_i+1,end);
+        stack<TreeNode*> s;
+        TreeNode *dummy,*temp,*root;
+        dummy=new TreeNode(1001);
+        s.push(dummy);
+        for(int i=0;i<nums.size();i++)
+        {
+            temp=new TreeNode(nums[i]);
+            while(!s.empty() and nums[i]>s.top()->val)
+            {
+                temp->left=s.top();
+                s.pop();
+            }
+            s.top()->right=temp;
+            s.push(temp);
+        }
+        root=dummy->right;
+        dummy->right=nullptr;
         return root;
-    }
-    int max_index(vector<int> &nums,int start,int end)
-    {
-        int max_i=start;
-        for(int i=start;i<end;i++)
-            if(nums[i]>nums[max_i])
-                max_i=i;
-        return max_i;
     }
 };
