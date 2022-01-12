1f00e262cfa77d5f8ae5070257f2dbde61c438a9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-12 16:32:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (87.26%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
-        deque<int> q;
-        vector<int> par(1000002,-1);
-        q.push_back(1);
-        int f1=1,br=0,cur=2;
-        while(!br)
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                int x;
-                if(f1) //even level
-                {
-                    x=q.back();
-                    q.pop_back();
-                    q.push_front(cur);
-                    q.push_front(cur+1);
-                }
-                else
-                {
-                    x=q.front();
-                    q.pop_front();
-                    q.push_back(cur);
-                    q.push_back(cur+1);
-                }
-                cur+=2;
-                par[cur-2]=par[cur-1]=x;
-                if(cur>label)
-                {
-                    br++;
-                    break;
-                }
-            }
-            f1=1-f1;
-        }
         vector<int> res;
-        while(label!=-1)
+        res.push_back(label); //take bottom up approach so the label itself is included
+        int height=log2(label); //height of label from root
+        while(height!=0)
         {
+            int left=pow(2,height-1); //rank of left limit of prev level
+            int right=pow(2,height)-1; //rank of right limit of prev level
+            label=left+(right-label/2); //prev level is sorted in diff order than cur level so new 
+                                        // level is adjusted 
             res.push_back(label);
-            label=par[label];
+            height--;
         }
         reverse(res.begin(),res.end());
         return res;
