04ab5c800a03adb6ffa565f822498963974d919a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-12 16:35:23 +0530

    Time: 5 ms (12.03%), Space: 6.4 MB (12.74%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
         vector<int> res;
-        res.push_back(label); //take bottom up approach so the label itself is included
-        int height=log2(label); //height of label from root
-        while(height!=0)
+        res.push_back(label);
+        int height=log2(label);
+        while(height>0)
         {
-            int left=pow(2,height-1); //rank of left limit of prev level
-            int right=pow(2,height)-1; //rank of right limit of prev level
-            label=left+(right-label/2); //prev level is sorted in diff order than cur level so new 
-                                        // level is adjusted 
+            int left=pow(2,height-1);
+            int right=pow(2,height)-1;
+            label=left+(right-label/2);
             res.push_back(label);
             height--;
         }
