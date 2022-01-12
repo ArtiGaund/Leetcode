cec8f165804bd8d8cfe5b9d4d306b45b982b1758
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-12 09:43:12 +0530

    Time: 4 ms (64.16%), Space: 12.1 MB (20.17%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -21,7 +21,7 @@ public:
             int size=q.size();
             for(int i=0;i<size;i++)
             {
-                TreeNode *cur=q.front();
+                TreeNode* cur=q.front();
                 q.pop();
                 if(i==size-1) res.push_back(cur->val);
                 if(cur->left) q.push(cur->left);
