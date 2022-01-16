629a339e38c041db69b47104b2e3dd15a4ac5703
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-16 11:31:30 +0530

    Time: 108 ms (44.40%), Space: 59.7 MB (76.69%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,17 +11,22 @@
  */
 class Solution {
 public:
-    vector<int> sum;
+   
     int deepestLeavesSum(TreeNode* root) {
         if(root==nullptr) return 0;
-        dfs(root,0);
+        vector<int> sum;
+        stack<pair<TreeNode*,int>> s;
+        s.push({root,0});
+        while(!s.empty())
+        {
+            TreeNode *cur=s.top().first;
+            int level=s.top().second;
+            s.pop();
+            if(level==sum.size()) sum.push_back(cur->val);
+            else sum[level]+=cur->val;
+            if(cur->left) s.push({cur->left,level+1});
+            if(cur->right) s.push({cur->right,level+1});
+        }
         return sum.back();
-    }
-    void dfs(TreeNode* root,int level)
-    {
-        if(level==sum.size()) sum.push_back(root->val);
-        else sum[level]+=root->val;
-        if(root->left) dfs(root->left,level+1);
-        if(root->right) dfs(root->right,level+1);
     }
 };
