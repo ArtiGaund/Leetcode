f469e21ee78a74d4c558b39e611edc263150895c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-20 12:31:54 +0530

    Time: 172 ms (20.99%), Space: 45 MB (17.09%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int res=solve(coins,dp,amount,0);
-        return (res==INT_MAX?-1:res);
-    }
-    int solve(vector<int> &v,vector<vector<int>> &dp,int amount,int i)
-    {
-        if(amount==0) return 0;
-        if(i>=v.size() or v.size()==0) return INT_MAX;
-        if(dp[i][amount]==-1)
+        vector<vector<int>> dp(n,vector<int>(amount+1,INT_MAX));
+        for(int i=0;i<n;i++) dp[i][0]=0;
+        for(int i=0;i<n;i++)
         {
-            int sum1=INT_MAX;
-            if(v[i]<=amount)
+            for(int j=1;j<=amount;j++)
             {
-                int res=solve(v,dp,amount-v[i],i);
-                if(res!=INT_MAX) sum1=res+1;
+                if(i>0)
+                    dp[i][j]=dp[i-1][j];
+                if(j>=coins[i])
+                {
+                    if(dp[i][j-coins[i]]!=INT_MAX)
+                        dp[i][j]=min(dp[i][j],dp[i][j-coins[i]]+1);
+                }
             }
-            int sum2=solve(v,dp,amount,i+1);
-            dp[i][amount]=min(sum1,sum2);
         }
-        return dp[i][amount];
+        return dp[n-1][amount]==INT_MAX?-1:dp[n-1][amount];
     }
+    
 };
