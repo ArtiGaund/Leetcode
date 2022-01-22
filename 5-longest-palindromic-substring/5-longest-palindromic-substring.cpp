1737986eaed91955355f70f2c557e1a9178d337e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-22 10:58:32 +0530

    Time: 546 ms (18.74%), Space: 30.3 MB (32.31%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -3,23 +3,24 @@ public:
     string longestPalindrome(string s) {
         int n=s.size();
         vector<vector<bool>> dp(n,vector<bool>(n));
-        for(int i=0;i<n;i++) dp[i][i]=true; //all substring of len 1 is palindrome
+        for(int i=0;i<n;i++) //all substring of len 1 is palindrome
+            dp[i][i]=true;
         int maxlen=1;
         int start=0;
-        for(int i=0;i<n-1;i++) //check substring of len 2
+        for(int i=0;i<n-1;i++) // check for len of 2
         {
             if(s[i]==s[i+1])
             {
-                dp[i][i+1]=true;
                 start=i;
+                dp[i][i+1]=true;
                 maxlen=2;
             }
         }
-        for(int k=3;k<=n;k++) //check substring of len> 3. k is len of substring
+        for(int k=3;k<=n;k++) //check of len> 2. k is len
         {
-            for(int i=0;i<n-k+1;i++) //fix start
+            for(int i=0;i<n-k+1;i++) //fixing starting index
             {
-                int j=i+k-1; //end of substring from i and k
+                int j=i+k-1; // end index by i and k
                 if(dp[i+1][j-1] and s[i]==s[j])
                 {
                     dp[i][j]=true;
