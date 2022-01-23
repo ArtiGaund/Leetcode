3d8295ae1c8d5138cb39cb0c7511fa68ea94e8f7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-23 18:01:29 +0530

    Added solution - LeetHub

---
+++ Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+	int LongestBitonicSequence(vector<int>nums)
+	{
+	    // code here
+	    int n=nums.size();
+	    vector<int> a(n,1);
+	    vector<int> b(n,1);
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=i-1;j>=0;j--)
+	        {
+	            if(nums[j]<nums[i])
+	                a[i]=max(a[i],a[j]+1);
+	        }
+	    }
+	    for(int i=n-1;i>=0;i--)
+	    {
+	        for(int j=i+1;j<n;j++)
+	        {
+	            if(nums[j]<nums[i])
+	                b[i]=max(b[i],b[j]+1);
+	        }
+	    }
+	    int maxlen=0;
+	    for(int i=0;i<n;i++)
+	        maxlen=max(maxlen,a[i]+b[i]-1);
+	   return maxlen;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution ob;
+		int ans = ob.LongestBitonicSequence(nums);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
