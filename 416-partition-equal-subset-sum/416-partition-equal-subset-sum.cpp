94eca33c0407ff611cfb737052a09aef3ac9e7a7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 19:04:25 +0530

    Time: 10 ms (94.83%), Space: 9.8 MB (71.65%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -5,15 +5,15 @@ public:
         if(sum%2!=0) return false;
         int n=nums.size();
         int halfsum=sum/2;
-        vector<vector<int>> dp(n,vector<int>(halfsum+1,-1));
+        vector<int> dp(halfsum+1,-1);
         return solve(dp,nums,halfsum,0);
     }
-    bool solve(vector<vector<int>> &dp,vector<int> &v,int sum,int i)
+    bool solve(vector<int> &dp,vector<int> &v,int sum,int i)
     {
         if(sum==0) return true;
         if(sum<0 or i>=v.size()) return false;
-        if(dp[i][sum]!=-1) return dp[i][sum];
-        dp[i][sum]=(solve(dp,v,sum-v[i],i+1) or solve(dp,v,sum,i+1));
-        return dp[i][sum];
+        if(dp[sum]!=-1) return dp[sum];
+        dp[sum]=(solve(dp,v,sum-v[i],i+1) or solve(dp,v,sum,i+1));
+        return dp[sum];
     }
 };
