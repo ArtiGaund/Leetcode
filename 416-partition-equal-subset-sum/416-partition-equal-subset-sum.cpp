19e2693e1ff2ef0356865d1da94562a075ba39a0
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 19:06:24 +0530

    Time: 582 ms (6.16%), Space: 12 MB (45.84%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -5,15 +5,21 @@ public:
         if(sum%2!=0) return false;
         int n=nums.size();
         int halfsum=sum/2;
-        vector<int> dp(halfsum+1,-1);
-        return solve(dp,nums,halfsum,0);
+       vector<vector<bool>> dp(n,vector<bool>(halfsum+1,false));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        for(int j=1;j<=halfsum;j++)
+            dp[0][j]=(nums[0]==j?true:false);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=halfsum;j++)
+            {
+                if(dp[i-1][j])
+                    dp[i][j]=dp[i-1][j];
+                else if(j>=nums[i])
+                    dp[i][j]=dp[i-1][j-nums[i]];
+            }
+        }
+        return dp[n-1][halfsum];
     }
-    bool solve(vector<int> &dp,vector<int> &v,int sum,int i)
-    {
-        if(sum==0) return true;
-        if(sum<0 or i>=v.size()) return false;
-        if(dp[sum]!=-1) return dp[sum];
-        dp[sum]=(solve(dp,v,sum-v[i],i+1) or solve(dp,v,sum,i+1));
-        return dp[sum];
-    }
+   
 };
