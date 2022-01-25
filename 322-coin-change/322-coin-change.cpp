3e6989b051f06c44e063731729c94822784b78dc
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 20:00:03 +0530

    Time: 277 ms (9.90%), Space: 45.3 MB (15.72%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,16 +2,28 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        int max=amount+1;
-        vector<int> dp(amount+1,max);
-        dp[0]=0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (coins[j] <= i) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
+        if(n==0) return 0;
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int res=solve(coins,amount,dp,0);
+        return (res==INT_MAX?-1:res);
+    }
+    int solve(vector<int> &coins,int amount,vector<vector<int>> &dp,int i)
+    {
+        if(amount==0) return 0;
+        if(coins.size()==0 or i>=coins.size()) return INT_MAX;
+        if(dp[i][amount]==-1)
+        {
+            int c1=INT_MAX;
+            if(coins[i]<=amount)
+            {
+                int res=solve(coins,amount-coins[i],dp,i);
+                if(res!=INT_MAX)
+                    c1=res+1;
             }
+            int c2=solve(coins,amount,dp,i+1);
+            dp[i][amount]=min(c1,c2);
         }
-        return dp[amount]>amount?-1:dp[amount];
+        return dp[i][amount];
+            
     }
 };
