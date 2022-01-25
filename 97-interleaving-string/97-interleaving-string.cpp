894b2a9fae6a39e86f8ab763a6fcb1b66fc25fcd
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 11:30:40 +0530

    Time: 5 ms (47.37%), Space: 6.2 MB (86.91%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -4,20 +4,20 @@ public:
          if(s3.length() != s1.length() + s2.length())
         return false;
     
-    bool table[s1.length()+1][s2.length()+1];
+    bool table[s2.length()+1];
     
     for(int i=0; i<s1.length()+1; i++)
         for(int j=0; j< s2.length()+1; j++){
             if(i==0 && j==0)
-                table[i][j] = true;
+                table[j] = true;
             else if(i == 0)
-                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+                table[j] = ( table[j-1] && s2[j-1] == s3[i+j-1]);
             else if(j == 0)
-                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+                table[j] = ( table[j] && s1[i-1] == s3[i+j-1]);
             else
-                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+                table[j] = (table[j] && s1[i-1] == s3[i+j-1] ) || (table[j-1] && s2[j-1] == s3[i+j-1] );
         }
         
-    return table[s1.length()][s2.length()];
+    return table[s2.length()];
     }
 };
