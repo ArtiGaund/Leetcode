571452a74883d2ab4fa9f8dffe9a65f0f95bab19
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 11:29:32 +0530

    Time: 3 ms (77.36%), Space: 6.2 MB (94.42%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int m=s1.size(),n=s2.size(),p=s3.size();
-        if(m+n!=p) return false;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(s1,0,s2,0,s3,0,dp);
-    }
-    bool solve(string s1,int i,string s2,int j,string s3,int k,vector<vector<int>> &dp)
-    {
-        if(i==s1.size()) return s2.substr(j)==s3.substr(k);
-        if(j==s2.size()) return s1.substr(i)==s3.substr(k);
-        if(dp[i][j]>=0) return dp[i][j]==1?true:false;
-        bool ans=false;
-        if((s3[k]==s1[i] and solve(s1,i+1,s2,j,s3,k+1,dp)) or (s3[k]==s2[j] and solve(s1,i,s2,j+1,s3,k+1,dp))) ans=true;
-        dp[i][j]=ans?1:0;
-        return ans;
+         if(s3.length() != s1.length() + s2.length())
+        return false;
+    
+    bool table[s1.length()+1][s2.length()+1];
+    
+    for(int i=0; i<s1.length()+1; i++)
+        for(int j=0; j< s2.length()+1; j++){
+            if(i==0 && j==0)
+                table[i][j] = true;
+            else if(i == 0)
+                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+            else if(j == 0)
+                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+            else
+                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+        }
+        
+    return table[s1.length()][s2.length()];
     }
 };
