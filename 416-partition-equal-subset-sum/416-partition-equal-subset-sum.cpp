a1216c1aec578cbe7c9fe4acd2b19496c91dd6b5
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 19:03:24 +0530

    Time: 55 ms (81.31%), Space: 75.4 MB (17.30%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0) return false; //sum is odd
+        if(sum%2!=0) return false;
         int n=nums.size();
         int halfsum=sum/2;
-        vector<vector<bool>> dp(n,vector<bool>(halfsum+1,false));
-        for(int i=0;i<n;i++) dp[i][0]=true;
-        for(int j=1;j<=halfsum;j++)
-            dp[0][j]=(nums[0]==j?true:false);
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<=halfsum;j++)
-            {
-                if(dp[i-1][j])
-                    dp[i][j]=dp[i-1][j];
-                else if(j>=nums[i])
-                    dp[i][j]=dp[i-1][j-nums[i]];
-            }
-        }
-        return dp[n-1][halfsum];
+        vector<vector<int>> dp(n,vector<int>(halfsum+1,-1));
+        return solve(dp,nums,halfsum,0);
+    }
+    bool solve(vector<vector<int>> &dp,vector<int> &v,int sum,int i)
+    {
+        if(sum==0) return true;
+        if(sum<0 or i>=v.size()) return false;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        dp[i][sum]=(solve(dp,v,sum-v[i],i+1) or solve(dp,v,sum,i+1));
+        return dp[i][sum];
     }
 };
