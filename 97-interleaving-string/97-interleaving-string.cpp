e2d0b19a1eb294275c02a757fc4e9ff24d713b1c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-25 11:54:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (86.91%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-         if(s3.length() != s1.length() + s2.length())
-        return false;
-    
-    bool table[s2.length()+1];
-    
-    for(int i=0; i<s1.length()+1; i++)
-        for(int j=0; j< s2.length()+1; j++){
-            if(i==0 && j==0)
-                table[j] = true;
-            else if(i == 0)
-                table[j] = ( table[j-1] && s2[j-1] == s3[i+j-1]);
-            else if(j == 0)
-                table[j] = ( table[j] && s1[i-1] == s3[i+j-1]);
-            else
-                table[j] = (table[j] && s1[i-1] == s3[i+j-1] ) || (table[j-1] && s2[j-1] == s3[i+j-1] );
+        if(s3.size()!=s1.size()+s2.size()) return false;
+        bool dp[s1.size()+1][s2.size()+1];
+        for(int i=0;i<=s1.size();i++)
+        {
+            for(int j=0;j<=s2.size();j++)
+            {
+                if(i==0 and j==0) dp[i][j]=true;
+                else if(i==0)
+                    dp[i][j]=(dp[i][j-1] and s2[j-1]==s3[i+j-1]);
+                else if(j==0)
+                    dp[i][j]=(dp[i-1][j] and s1[i-1]==s3[i+j-1]);
+                else 
+                {
+                    dp[i][j]=((dp[i-1][j] and s1[i-1]==s3[i+j-1]) or (dp[i][j-1] and s2[j-1]==s3[i+j-1]));
+                }
+            }
         }
-        
-    return table[s2.length()];
+        return dp[s1.size()][s2.size()];
     }
 };
