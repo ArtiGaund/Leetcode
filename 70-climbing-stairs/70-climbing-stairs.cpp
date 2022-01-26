6cfc6308c0d0b44fc2b35b2a4ed2010aca9f3665
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-26 19:30:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.62%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,11 +3,13 @@ public:
     int climbStairs(int n) {
         if(n<2) return 1;
         if(n==2) return 2;
-        vector<int> dp(n+1);
-        dp[1]=1;
-        dp[2]=2;
+        int n1=1,n2=2,temp;
         for(int i=3;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        {
+            temp=n1+n2;
+            n1=n2;
+            n2=temp;
+        }
+        return n2;
     }
 };
