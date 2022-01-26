6ff2903c2e20a73884e1b871c6f4712b965b2e05
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-26 10:49:20 +0530

    Time: 232 ms (29.41%), Space: 85.4 MB (48.31%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,32 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int> &res)
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> tree1,tree2;
+        inorder(root1,tree1);
+        inorder(root2,tree2);
+        vector<int> res(tree1.size()+tree2.size());
+        merge(tree1,tree2,res);
+        return res;
+    }
+    void inorder(TreeNode* root,vector<int> &v)
     {
         if(root==nullptr) return;
-        inorder(root->left,res);
-        res.push_back(root->val);
-        inorder(root->right,res);
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
     }
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> res;
-        inorder(root1,res);
-        inorder(root2,res);
-        sort(res.begin(),res.end());
-        return res;
+    void merge(vector<int> &v1,vector<int> &v2,vector<int> &res)
+    {
+        int i=0,j=0,k=0;
+        while(i<v1.size() and j<v2.size())
+        {
+            if(v1[i]<v2[j])
+                res[k++]=v1[i++];
+            else
+                res[k++]=v2[j++];
+        }
+        while(i<v1.size()) res[k++]=v1[i++];
+        while(j<v2.size()) res[k++]=v2[j++];
     }
 };
