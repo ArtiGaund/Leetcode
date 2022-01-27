8707d8a9ea9032acef1e5dd52cf32bff59a1324c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-27 23:37:43 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (45.23%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -12,26 +12,13 @@
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
+        return solve(root,0);
+    }
+    int solve(TreeNode* root,int sum)
+    {
         if(root==nullptr) return 0;
-        stack<TreeNode*> s;
-        s.push(root);
-        int res=0;
-        while(!s.empty())
-        {
-            TreeNode *cur=s.top();
-            s.pop();
-            if(cur->left==nullptr and cur->right==nullptr) res+=cur->val;
-            if(cur->right) 
-            {
-                cur->right->val+=cur->val*10;
-                s.push(cur->right);
-            }
-            if(cur->left)
-            {
-                cur->left->val+=cur->val*10;
-                s.push(cur->left);
-            }
-        }
-        return res;
+        sum=sum*10+root->val;
+        if(root->left==nullptr and root->right==nullptr) return sum;
+        return solve(root->left,sum)+solve(root->right,sum);
     }
 };
