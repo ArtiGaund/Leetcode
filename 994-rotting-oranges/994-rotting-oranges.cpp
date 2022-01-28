bad1891967d7b4136d39623e008a668fe0a24664
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-29 01:28:11 +0530

    Time: 19 ms (13.77%), Space: 12.9 MB (94.60%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==2) q.push({i,j});
+                else if(grid[i][j]==1) fresh++;
+            }
+        }
+        int c=0;
+        vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+        while(!q.empty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                auto p=q.front();
+                q.pop();
+                int l=p.first;
+                int r=p.second;
+                for(auto d:dir)
+                {
+                    int x=l+d.first;
+                    int y=r+d.second;
+                    if(x>=0 and x<row and y>=0 and y<col and grid[x][y]==1)
+                    {
+                        grid[x][y]=2;
+                        fresh--;
+                        q.push({x,y});
+                    }
+                }
+            }
+            if(!q.empty())
+                c++;
+        }
+        return (fresh==0?c:-1);
+    }
+};
