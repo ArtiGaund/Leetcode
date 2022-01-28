7a402348f1d4f16b3f44f9c67ef470d1875f4727
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-29 00:02:08 +0530

    Time: 24 ms (8.56%), Space: 14.4 MB (19.40%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -4,33 +4,29 @@ public:
        if(image.size()==0) return image;
         int row=image.size();
         int col=image[0].size();
-        queue<pair<int,int>> q;
-        int oldcolor=image[sr][sc];
+        int old=image[sr][sc];
         image[sr][sc]=newColor;
+        queue<pair<int,int>> q; //to store the boundaries of visited vertix
         q.push({sr-1,sc});
-        q.push({sr+1,sc});
-        q.push({sr,sc-1});
-        q.push({sr,sc+1});
-        vector<vector<bool>> vis(row,vector<bool>(col,false));
+         q.push({sr+1,sc});
+         q.push({sr,sc-1});
+         q.push({sr,sc+1});
+        vector<vector<bool>> vis(row,vector<bool>(col,false)); //for checking visited cell
         vis[sr][sc]=true;
         while(!q.empty())
         {
-            int size=q.size();
-            for(int i=0;i<size;i++)
+            auto p=q.front();
+            q.pop();
+            int l=p.first;
+            int r=p.second;
+            if(l>=0 and l<row and r>=0 and r<col and !vis[l][r] and image[l][r]==old) // check for 
             {
-                auto p=q.front();
-                int l=p.first;
-                int r=p.second;
-                q.pop();
-                if(l>=0 and l<row and r>=0 and r<col and !vis[l][r] and image[l][r]==oldcolor)
-                {
-                    vis[l][r]=true;
-                    image[l][r]=newColor;
-                    q.push({l-1,r});
-                    q.push({l+1,r});
-                    q.push({l,r-1});
-                    q.push({l,r+1});
-                }
+                image[l][r]=newColor;
+                vis[l][r]=true;
+                 q.push({l-1,r});
+                 q.push({l+1,r});
+                 q.push({l,r-1});
+                 q.push({l,r+1});
             }
         }
         return image;
