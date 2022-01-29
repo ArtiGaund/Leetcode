b775b56a5c0b2ca6686166f0d7a37c83a8266e57
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-29 18:16:35 +0530

    Time: 1303 ms (16.02%), Space: 193 MB (39.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int des) {
-        unordered_map<int,vector<int>> graph; //create graph
+    bool validPath(int n, vector<vector<int>>& edges, int s, int dest) {
+        unordered_map<int,vector<int>> mp;
         for(auto e:edges)
         {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+            mp[e[0]].push_back(e[1]);
+            mp[e[1]].push_back(e[0]);
         }
         vector<bool> vis(n,false);
         queue<int> q;
-        q.push(source);
-        vis[source]=true;
+        q.push(s);
+        vis[s]=true;
         while(!q.empty())
         {
             int cur=q.front();
             q.pop();
-            if(cur==des) return true;
-            for(auto e:graph[cur])
+            if(cur==dest) return true;
+            for(auto e:mp[cur])
             {
                 if(!vis[e])
                 {
