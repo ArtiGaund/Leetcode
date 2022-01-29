e0e93c21a3eba0954a878ec6acf241c07fa3c716
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-01-29 11:11:46 +0530

    Time: 68 ms (65.01%), Space: 27.4 MB (20.96%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        int n=grid.size();
+        if(grid.size()==0) return 0;
+        int row=grid.size();
+        int col=grid[0].size();
         queue<pair<int,int>> q,q1;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<row;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<col;j++)
             {
                 if(grid[i][j]==1)
                 {
@@ -26,10 +28,10 @@ public:
                 q.pop();
                 int i=p.first;
                 int j=p.second;
-                if(i>=0 and i<n and j>=0 and j<n and grid[i][j]==0)
+                if(i>=0 and i<row and j>=0 and j<col and grid[i][j]==0)
                 {
                     grid[i][j]=step;
-                    q1.push({i-1,j});
+                   q1.push({i-1,j});
                     q1.push({i+1,j});
                     q1.push({i,j-1});
                     q1.push({i,j+1});
