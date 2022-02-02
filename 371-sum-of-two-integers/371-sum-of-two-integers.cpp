fc27f35d080f9dc68d8fd851c10552e522de9ae1
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-02 16:50:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.64%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-      unsigned int ua = (unsigned int) a; 
-          unsigned int ub = (unsigned int) b;
-
-          while(ub){ // Repeat as long as there is no carry
-            /*Make sure to make carry 64bit wide since left shifting uint32 gives error*/
-
-              uint64_t carry = ua & ub; //bitwise AND gets the carry values
-              ua = ua ^ ub; //XOR gives the ADDITION values without taking the carry values into account
-              ub = carry << 1; // We left shift the carry value to be able to use it
-          }
-
-          return (int)ua; //Converting the unsigned back to signed does the trick
+        unsigned int ua=(unsigned int)a;
+        unsigned int ub=(unsigned int)b;
+        while(ub)
+        {
+            uint64_t carry=ua&ub; //generate carry
+            ua=ua^ub; //add value without generating carry
+            ub=carry<<1;
+        }
+        return (int)ua;
     }
 };
