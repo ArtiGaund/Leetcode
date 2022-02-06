b6a61866c44cc0f26b51170f7a9ee145397b2d75
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-06 11:08:23 +0530

    Time: 587 ms (66.67%), Space: 139.6 MB (100.00%) - LeetHub

--- 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     long long minimumDifference(vector<int>& nums) {
-        priority_queue<int> L; //store smallest N digits in the first part
-        priority_queue<int,vector<int>,greater<>> R; //store greatest N digit in sec part
+        priority_queue<int> L;
+        priority_queue<int,vector<int>,greater<>> R;
         long n=nums.size()/3,left=0,right=0,ans=LLONG_MAX;
-        vector<long> temp(nums.size()); 
+        vector<long> temp(nums.size());
         for(int i=nums.size()-1;i>=n;i--)
         {
             R.push(nums[i]);
@@ -14,12 +14,12 @@ public:
                 right-=R.top();
                 R.pop();
             }
-            if(R.size()==n) temp[i]=right; //max sum
+            if(R.size()==n) temp[i]=right;
         }
         for(int i=0;i<nums.size()-n;i++)
         {
-                L.push(nums[i]);
-                left+=nums[i];
+            L.push(nums[i]);
+            left+=nums[i];
             if(L.size()>n)
             {
                 left-=L.top();
