b0b9728d505d23d65e6284d2c28ecfe530f6cee5
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-07 18:30:53 +0530

    Time: 188 ms (66.93%), Space: 47.3 MB (71.22%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        //BFS
+        //DFS
         int n=graph.size();
-        vector<int> adj[n];
+        vector<int> res,vis(n,0);
         for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<graph[i].size();j++)
-                adj[graph[i][j]].push_back(i);
-        }
-        vector<int> in(n,0);
-        for(int i=0;i<n;i++)
-        {
-            for(auto it:adj[i])
-                in[it]++;
-        }
-        queue<int> q;
-        for(int i=0;i<n;i++)
-            if(in[i]==0) q.push(i);
-        vector<int> res;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            res.push_back(node);
-            for(auto it:adj[node])
-            {
-                in[it]--;
-                if(in[it]==0) q.push(it);
-            }
-        }
-        sort(res.begin(),res.end());
+            if(dfs(i,vis,graph))
+                res.push_back(i);
         return res;
+    }
+    bool dfs(int node,vector<int> &vis,vector<vector<int>> &graph)
+    {
+        if(vis[node]==1) return false;
+        if(vis[node]==2) return true;
+        vis[node]=1;
+        for(auto it:graph[node])
+        {
+            if(dfs(it,vis,graph)==false) return false;
+        }
+        vis[node]=2;
+        return true;
     }
 };
