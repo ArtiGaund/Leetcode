0c48d355367262094eada3500484ab2004a53633
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-07 16:00:26 +0530

    Time: 266 ms (13.78%), Space: 313.2 MB (5.11%) - LeetHub

---
+++ 851-loud-and-rich/851-loud-and-rich.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        int n=quiet.size();
+        vector<int> adj[n];
+        for(auto e:richer)
+            adj[e[1]].push_back(e[0]);
+        vector<int> res(n,-1);
+        for(int node=0;node<n;node++)
+            dfs(node,adj,res,quiet);
+        return res;
+    }
+    int dfs(int node,vector<int> adj[],vector<int> &res,vector<int> q)
+    {
+        if(res[node]==-1)
+        {
+            res[node]=node;
+            for(auto it:adj[node])
+            {
+                int cad=dfs(it,adj,res,q);
+                if(q[cad]<q[res[node]])
+                    res[node]=cad;
+            }
+        }
+        return res[node];
+    }
+};
