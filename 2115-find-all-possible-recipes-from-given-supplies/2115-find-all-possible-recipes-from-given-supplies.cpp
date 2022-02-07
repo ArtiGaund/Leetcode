2c9e0c62db3e8455e4c1df5509d334e1cbc1a8c9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-07 23:05:11 +0530

    Time: 556 ms (55.67%), Space: 160 MB (17.53%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     //BFS
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        int n=ingredients.size();
         unordered_map<string,vector<string>> adj;
-        map<string,int> in;
-        for(int i=0;i<n;i++)
+        map<string,int> indegree;
+        for(int i=0;i<ingredients.size();i++)
         {
             for(string s:ingredients[i])
             {
                 adj[s].push_back(recipes[i]);
-                in[recipes[i]]++;
+                indegree[recipes[i]]++;
             }
         }
         queue<string> q;
-        for(string items:supplies)
-            if(in[items]==0) q.push(items);
+        for(string item:supplies)
+            if(indegree[item]==0) q.push(item);
         while(!q.empty())
         {
             string cur=q.front();
             q.pop();
-            for(string recipe:adj[cur])
+            for(auto it:adj[cur])
             {
-                in[recipe]--;
-                if(in[recipe]==0) q.push(recipe);
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
         vector<string> res;
-        for(string recipe:recipes)
-            if(in[recipe]==0)
-            res.push_back(recipe);
+        for(string s:recipes)
+            if(indegree[s]==0) res.push_back(s);
         return res;
     }
 };
