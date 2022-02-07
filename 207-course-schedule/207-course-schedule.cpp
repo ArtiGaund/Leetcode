03007818477c601221e88366560cf367ff69bf9b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-07 11:46:58 +0530

    Time: 23 ms (68.52%), Space: 13.6 MB (68.20%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
-        for(int i=0;i<pre.size();i++) 
+        for(int i=0;i<pre.size();i++)
             adj[pre[i][1]].push_back(pre[i][0]);
-        vector<int> in(n,0);
+        vector<int> vis(n,0); //vis[0]=not visited
         for(int i=0;i<n;i++)
-            for(auto it:adj[i])
-                in[it]++;
-        queue<int> q;
-        for(int i=0;i<n;i++)
-            if(in[i]==0) q.push(i);
-        while(!q.empty())
         {
-            int node=q.front();
-            q.pop();
-            n--;
+            if(isCycle(i,adj,vis))
+                return false;
+        }
+        return true;
+    }
+    bool isCycle(int node,vector<int> adj[],vector<int> &vis)
+    {
+        if(vis[node]==1) return true;
+        if(vis[node]==0)
+        {
+             vis[node]=1; //visited but not done with it children
             for(auto it:adj[node])
             {
-                in[it]--;
-                if(in[it]==0) q.push(it);
+                if(isCycle(it,adj,vis)) return true;
             }
         }
-        return n==0;
+       
+        vis[node]=2; //done with it children
+        return false;
     }
 };
