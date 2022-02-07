9793cd453824a248db87dbefcaf0972e3620a136
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-07 11:38:23 +0530

    Time: 24 ms (67.19%), Space: 13 MB (96.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
-        for(int i=0;i<pre.size();i++)
+        for(int i=0;i<pre.size();i++) 
             adj[pre[i][1]].push_back(pre[i][0]);
-        vector<int> indegree(n,0);
+        vector<int> in(n,0);
         for(int i=0;i<n;i++)
-        {
-            for(auto it:adj[i]) indegree[it]++;
-        }
+            for(auto it:adj[i])
+                in[it]++;
         queue<int> q;
         for(int i=0;i<n;i++)
-            if(indegree[i]==0) q.push(i);
+            if(in[i]==0) q.push(i);
         while(!q.empty())
         {
             int node=q.front();
@@ -19,10 +18,10 @@ public:
             n--;
             for(auto it:adj[node])
             {
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
+                in[it]--;
+                if(in[it]==0) q.push(it);
             }
         }
-       return n==0;
+        return n==0;
     }
 };
