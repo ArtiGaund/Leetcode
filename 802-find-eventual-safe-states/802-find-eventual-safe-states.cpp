11e0984d71c9bc6180970fa89a8e30ebbaeb82ff
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-07 18:18:39 +0530

    Time: 182 ms (69.11%), Space: 47.1 MB (89.09%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
+    //DFS
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        //BFS
-        int n=graph.size();
-        vector<int> adj[n];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<graph[i].size();j++)
-            {
-                adj[graph[i][j]].push_back(i);
-            }
+        int n = graph.size();
+        if (n == 0) return {};
+        vector<int> res, visited(n, 0); // 0: not visited; 1: in a dfs path; 2: visited
+        for (int i = 0; i < n; i++) {
+            if (dfs(graph, i, visited)) res.push_back(i);
         }
-        vector<int> in(n,0);
-        for(int i=0;i<n;i++)
-        {
-            for(auto it:adj[i]) in[it]++;
-        }
-        queue<int> q;
-        for(int i=0;i<n;i++)
-            if(in[i]==0)
-                q.push(i);
-        vector<int> res;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            res.push_back(node);
-            for(auto it:adj[node])
-            {
-                in[it]--;
-                if(in[it]==0) q.push(it);
-            }
-        }
-        sort(res.begin(),res.end());
         return res;
+    }
+private:
+    bool dfs(vector<vector<int>>& graph, int i, vector<int>& visited) {
+        if (visited[i] == 2) return true;
+        if (visited[i] == 1) {
+            return false;
+        }
+        visited[i] = 1;
+        for (int j : graph[i]) {
+            if (dfs(graph, j, visited) == false) return false;
+        }
+        visited[i] = 2;
+        return true;
     }
 };
