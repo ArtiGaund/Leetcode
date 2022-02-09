c13c123e3eed904088c669f7ab04dc08b2d749cb
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-09 17:50:23 +0530

    Time: 14 ms (42.68%), Space: 10.2 MB (24.01%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,16 +3,25 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,dp,grid);
+        vector<vector<int>> dp(n,vector<int>(m));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 and j==0) dp[i][j]=grid[i][j];
+                else
+                {
+                    int up=grid[i][j];
+                    if(i>0) up+=dp[i-1][j];
+                    else up+=1e9;
+                    int left=grid[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left+=1e9;
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
-    int solve(int i,int j,vector<vector<int>> &dp,vector<vector<int>> &mat)
-    {
-        if(i==0 and j==0) return mat[0][0];
-        if(i<0 or j<0) return 1e9;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=mat[i][j]+solve(i-1,j,dp,mat);
-        int left=mat[i][j]+solve(i,j-1,dp,mat);
-        return dp[i][j]=min(up,left);
-    }
+    
 };
