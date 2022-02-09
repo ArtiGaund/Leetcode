d83d10d7d4e936ee316e76794c624f1940d52b23
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-09 19:30:09 +0530

    Time: 34 ms (9.88%), Space: 10.3 MB (37.39%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -3,23 +3,26 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        for(int j=0;j<m;j++) dp[0][j]=matrix[0][j];
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                int up=matrix[i][j]+dp[i-1][j];
+                int left=matrix[i][j];
+                if(j-1>=0) left+=dp[i-1][j-1];
+                else left+=1e9;
+                int right=matrix[i][j];
+                if(j+1<m) right+=dp[i-1][j+1];
+                else right+=1e9;
+                dp[i][j]=min({up,left,right});
+            }
+        }
         int mini=INT_MAX;
         for(int j=0;j<m;j++)
-        {
-            int ans=solve(n-1,j,dp,matrix,m);
-            mini=min(mini,ans);
-        }
+            mini=min(mini,dp[n-1][j]);
         return mini;
     }
-    int solve(int i,int j,vector<vector<int>> &dp,vector<vector<int>> &mat,int m)
-    {
-        if(j<0 or j>=m) return 1e9;
-        if(i==0) return mat[0][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=mat[i][j]+solve(i-1,j,dp,mat,m);
-        int left=mat[i][j]+solve(i-1,j-1,dp,mat,m);
-        int right=mat[i][j]+solve(i-1,j+1,dp,mat,m);
-        return dp[i][j]=min({up,left,right});
-    }
+    
 };
