a5443062bb67fc25e193c7d7b3ff2eb52f8e77d9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-09 19:53:22 +0530

    Time: 71 ms (68.99%), Space: 15.9 MB (31.49%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& arr,int i,int j,vector<vector<int>> &dp) {
-        if(dp[i+1][j]!=INT_MAX)
-            return dp[i+1][j];
-        int s=INT_MAX;
-        if(j==arr.size())
-            return 0;
-        for(int p=0;p<arr[j].size();p++){
-            if(p==i)
-                continue;
-            else
-                s=min(s,arr[j][p]+dfs(arr,p,j+1,dp));
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int res=INT_MAX;
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        for(int j=0;j<n;j++)
+            dp[0][j]=grid[0][j];
+        for(int i=1;i<n;i++)
+        {
+            //case 1
+            int mi=INT_MAX-200;
+            for(int j=0;j<n;j++) //came from above from above row to cur
+            {
+                dp[i][j]=min(dp[i][j],mi+grid[i][j]);
+                mi=min(dp[i-1][j],mi);
+            }
+            //case 2
+            mi=INT_MAX-200;
+            for(int j=n-1;j>=0;j--) //get min while coming from back side from above row to cu
+            {
+                dp[i][j]=min(dp[i][j],mi+grid[i][j]);
+                mi=min(dp[i-1][j],mi);
+            }
         }
-        return dp[i+1][j]=s;
-    }
-    int minFallingPathSum(vector<vector<int>>& arr) {
-        vector<vector<int>>dp(202, vector<int>(202,INT_MAX));
-        return dfs(arr,-1,0,dp);
+        for(int j=0;j<n;j++)
+            res=min(res,dp[n-1][j]);
+        return res;
     }
 };
