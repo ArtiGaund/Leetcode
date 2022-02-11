4185766b3a920d0e4f1e5b2381b72e442bee6a72
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-11 16:56:17 +0530

    Time: 36 ms (52.34%), Space: 13 MB (19.94%) - LeetHub

--- 358-rearrange-string-k-distance-apart/358-rearrange-string-k-distance-apart.cpp
+++ 358-rearrange-string-k-distance-apart/358-rearrange-string-k-distance-apart.cpp
@@ -7,17 +7,16 @@ public:
         priority_queue<pair<int,char>> pq;
         for(auto it:mp)
             pq.push({it.second,it.first});
-        string res="";
-       
         int len=s.size();
+        string res="";
         while(!pq.empty())
         {
-             vector<pair<int,char>> cache;
+            vector<pair<int,char>> cache;
             int count=min(k,len);
             for(int i=0;i<count;i++)
             {
                 if(pq.empty()) return "";
-                pair<int,char> temp=pq.top();
+                auto temp=pq.top();
                 pq.pop();
                 res+=temp.second;
                 if(--temp.first>0) cache.push_back(temp);
