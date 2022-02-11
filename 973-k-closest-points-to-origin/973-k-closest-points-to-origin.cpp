536325e3cd40630006a75a6f2ed71c503c6538e9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-11 15:12:48 +0530

    Time: 1599 ms (5.12%), Space: 204.1 MB (5.04%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,35 @@
+typedef pair<vector<int>,double> pii;
+class Solution {
+public:
+struct compare
+{
+    bool operator()(pii p1,pii p2)
+    {
+    return p1.second<p2.second;
+    }
+};
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pii,vector<pii>,compare> pq;
+        for(int i=0;i<k;i++)
+        {
+            double dis=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            pq.push({points[i],dis});
+        }
+        for(int i=k;i<points.size();i++)
+        {
+            double dis=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            if(dis<pq.top().second)
+            {
+            pq.pop();
+            pq.push({points[i],dis});
+            }
+        }
+        vector<vector<int>> res;
+        while(!pq.empty())
+        {
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        return res;
+    }
+};
