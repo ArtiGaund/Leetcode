2865a606d77dbdde49ce976c3b2ffaa4fb11249a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-11 23:40:44 +0530

    Time: 398 ms (22.64%), Space: 72.7 MB (21.85%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,33 +1,33 @@
-typedef pair<vector<int>,double> pii;
+typedef pair<double,vector<int>> pii;
 class Solution {
 public:
-    struct compare
-    {
-      bool operator()(pii p1,pii p2)
-      {
-          return p1.second<p2.second;
-      }
-    };
+// struct compare
+// {
+//     bool operator()(pii p1,pii p2)
+//     {
+//     return p1.second<p2.second;
+//     }
+// };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pii,vector<pii>,compare> pq;
+        priority_queue<pii> pq;
         for(int i=0;i<k;i++)
         {
             double dis=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
-            pq.push({points[i],dis});
+            pq.push({dis,points[i]});
         }
         for(int i=k;i<points.size();i++)
         {
             double dis=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
-            if(dis<pq.top().second)
+            if(dis<pq.top().first)
             {
-                pq.pop();
-                pq.push({points[i],dis});
+            pq.pop();
+            pq.push({dis,points[i]});
             }
         }
         vector<vector<int>> res;
         while(!pq.empty())
         {
-            res.push_back(pq.top().first);
+            res.push_back(pq.top().second);
             pq.pop();
         }
         return res;
