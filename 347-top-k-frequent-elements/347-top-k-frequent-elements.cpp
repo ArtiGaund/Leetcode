e0852f333f20fabca2d6c9240293a55741eaee2a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-11 15:02:40 +0530

    Time: 19 ms (61.56%), Space: 13.5 MB (97.71%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,29 +1,30 @@
 typedef pair<int,int> pii;
 class Solution {
 public:
-    struct compare{
+    struct compare
+    {
         bool operator()(pii p1,pii p2)
         {
-            return p1.second>p2.second;
+        return p1.second>p2.second;
         }
     };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for(int a:nums) mp[a]++;
-        priority_queue<pii,vector<pii>,compare> pq;
-        int count=0;
-        for(auto it:mp)
-        {
-            pq.push({it.first,it.second});
-            count++;
-            if(count>k) pq.pop();
-        }
-        vector<int> res;
-        while(!pq.empty())
-        {
-            res.push_back({pq.top().first});
-            pq.pop();
-        }
-        return res;
+      unordered_map<int,int> mp;
+      for(int a:nums) mp[a]++;
+      priority_queue<pii,vector<pii>,compare> pq;
+      int count=0;
+      for(auto it:mp)
+      {
+        pq.push({it.first,it.second});
+        count++;
+        if(count>k) pq.pop();
+      }
+      vector<int> res;
+      while(!pq.empty())
+      {
+        res.push_back(pq.top().first);
+        pq.pop();
+      }
+      return res;
     }
 };
