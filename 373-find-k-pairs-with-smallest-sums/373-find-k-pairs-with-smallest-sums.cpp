dc368296490ffbfaa43a1a62aa32d7e249fccb44
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-11 18:59:41 +0530

    Time: 355 ms (16.58%), Space: 119.3 MB (7.08%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -3,17 +3,17 @@ public:
     typedef pair<int,pair<int,int>> pii;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         priority_queue<pii,vector<pii>,greater<pii>> pq;
-        for(int i=0;i<nums1.size();i++) //pairing all elements of nums1 with 0th element ofnums2
+        for(int i=0;i<nums1.size();i++)
             pq.push({nums1[i]+nums2[0],{i,0}});
         vector<vector<int>> res;
-        while(!pq.empty() and k--) //finding pair with remaining elements of nums2
+        while(!pq.empty() and k--)
         {
             auto temp=pq.top();
             pq.pop();
             int i=temp.second.first;
             int j=temp.second.second;
             res.push_back({nums1[i],nums2[j]});
-            if(j+1==nums2.size()) continue; //reach end of nums2
+            if(j+1==nums2.size()) continue;
             pq.push({nums1[i]+nums2[j+1],{i,j+1}});
         }
         return res;
