735ee1c4066a362dd5c03826a191f7a1f5814f18
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-12 19:12:08 +0530

    Time: 50 ms (45.27%), Space: 13.1 MB (77.96%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -7,18 +7,17 @@ public:
         for(int i=0;i<m;i++)
             pq.push({matrix[i][0],{i,0}});
         int n=matrix[0].size();
-        vector<int> ans;
+        int val=-1;
         while(!pq.empty() and k--)
         {
             auto temp=pq.top();
             pq.pop();
-            int val=temp.first;
+            val=temp.first;
             int i=temp.second.first;
             int j=temp.second.second;
-            ans.push_back(val);
             if(j+1==n) continue;
             pq.push({matrix[i][j+1],{i,j+1}});
         }
-        return ans.back();
+        return val;
     }
 };
