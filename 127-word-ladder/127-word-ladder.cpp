629edd06c0ae9a57a0d580feb527e06401c0e0d8
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-12 17:25:50 +0530

    Time: 164 ms (63.44%), Space: 13.9 MB (77.35%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int ladderLength(string begin, string end, vector<string>& word) {
         unordered_set<string> s;
-        bool present=false;
+        bool present=false; //for checking end word is present or not
         for(string w:word)
         {
             if(w==end) present=true;
             s.insert(w);
         }
-        if(present==false) return 0;
+        if(present==false) return 0; //if end word is not present its impossible
         queue<string> q;
         q.push(begin);
         int depth=0;
@@ -20,15 +20,15 @@ public:
             {
                 string cur=q.front();
                 q.pop();
-                for(int i=0;i<cur.size();i++)
+                for(int i=0;i<cur.size();i++) //for changing char in a cur word
                 {
                     string temp=cur;
                     for(char ch='a';ch<='z';ch++)
                     {
                         temp[i]=ch;
                         if(cur==temp) continue; //skip same word
-                        if(temp==end) return depth+1;
-                        if(s.find(temp)!=s.end())
+                        if(temp==end) return depth+1; // found end word
+                        if(s.find(temp)!=s.end()) //found word in set then insert in queue and erase from set
                         {
                             q.push(temp);
                             s.erase(temp);
