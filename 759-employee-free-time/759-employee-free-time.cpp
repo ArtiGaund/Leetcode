fbde168a3a974da1ec04f45214d1be4af70cb602
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-13 15:37:23 +0530

    Time: 41 ms (58.25%), Space: 9.7 MB (96.31%) - LeetHub

---
+++ 759-employee-free-time/759-employee-free-time.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    typedef pair<int,pair<int,int>> pii;  
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        //priority_queue Node is pair<start, <employeeIndex, intervalIndex>>
+        //pair.second is used to get to next interval
+        using PQ_Node = pair<int, pair<int, int>>;
+        vector<Interval> ans;
+        if (schedule.empty()) return ans;
+        priority_queue<PQ_Node, vector<PQ_Node>, greater<>> pq;
+        for(int i=0; i<schedule.size(); ++i) {
+            if (schedule[i].size()) {
+                pq.push({schedule[i][0].start, {i, 0}});
+            }
+        }
+        int end = schedule[pq.top().second.first][0].end;
+        while(pq.size()) {
+            auto p = pq.top();
+            pq.pop();
+            int intervalIndex = p.second.second;
+            int employeeIndex = p.second.first;
+            if (p.first > end) {
+                ans.push_back(Interval{end, p.first});
+                end = schedule[employeeIndex][intervalIndex].end;
+            } else {
+                end = max(end, schedule[employeeIndex][intervalIndex].end);
+            }
+            ++intervalIndex;
+            if (intervalIndex < schedule[employeeIndex].size()) {
+            
+                pq.push({schedule[employeeIndex][intervalIndex].start,
+                         {employeeIndex, intervalIndex}});
+            }
+            
+        }
+        return ans;
+    }
+};
