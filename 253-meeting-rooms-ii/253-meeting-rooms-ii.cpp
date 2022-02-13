dc37f170c80420eab452ed5a01c06c0b24076983
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-13 15:26:02 +0530

    Time: 35 ms (32.72%), Space: 11.7 MB (87.45%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -5,6 +5,7 @@ public:
         return a[0]<b[0];
     }
     int minMeetingRooms(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return 0;
         sort(intervals.begin(),intervals.end(),compare);
         priority_queue<int,vector<int>,greater<int>> minH;
         for(int i=0;i<intervals.size();i++)
