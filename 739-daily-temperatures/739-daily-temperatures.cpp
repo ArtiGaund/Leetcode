9a1f7508c15c0d55b9e232e3235b87cac3468f43
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-13 18:28:18 +0530

    Time: 211 ms (56.39%), Space: 85.2 MB (71.95%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n=temp.size();
-        vector<int> dis(n); //will keep the dis between cur elem and next greater element;
-        stack<pair<int,int>> mq; //monotonic queue for non increasing sequence
+        vector<int> dis(n);
+        stack<pair<int,int>> mq;
         for(int i=n-1;i>=0;i--)
         {
             int val=temp[i];
-            while(!mq.empty() and val>=mq.top().first)
-                mq.pop();
+            while(!mq.empty() and val>=mq.top().first) mq.pop();
             if(!mq.empty())
                 dis[i]=mq.top().second-i;
             else
                 dis[i]=0;
-            mq.push({temp[i],i});
+            mq.push({val,i});
         }
         return dis;
-        
     }
 };
