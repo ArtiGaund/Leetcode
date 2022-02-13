033ae7f3604203edce941824f6a0559d8f9b97a8
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-13 12:36:17 +0530

    Time: 16 ms (91.86%), Space: 11.9 MB (71.56%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    static bool compare(vector<int> &a,vector<int> &b)
+    {
+        return a[0]<b[0];
+    }
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),compare);
+        priority_queue<int,vector<int>,greater<int>> minH;
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(!minH.empty() and minH.top()<=intervals[i][0]) minH.pop();
+            minH.push(intervals[i][1]);
+        }
+        return minH.size();
+    }
+};
