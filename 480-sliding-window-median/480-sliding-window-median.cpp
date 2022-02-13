8b96b3f91da20aba6c51cb643fa170f1f33dcf0e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-13 11:36:48 +0530

    Time: 70 ms (52.23%), Space: 14.6 MB (83.15%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.cpp
+++ 480-sliding-window-median/480-sliding-window-median.cpp
@@ -2,58 +2,61 @@ class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
         vector<double> medians;
-        unordered_map<int,int> mp; //keep track of invalid no. It holds the count of occurrence of all such no that have been invalidated and yet remain in heaps
-        priority_queue<int> low; //max heap
-        priority_queue<int,vector<int>,greater<int>> high; //min heap
+        priority_queue<int> maxH;
+        priority_queue<int,vector<int>,greater<int>> minH;
+        unordered_map<int,int> mp;
         int i=0;
-        while(i<k) //initialize the heap
-            low.push(nums[i++]);
-        for(int j=0;j<k/2;j++)
+        while(i<k) //adding in maxH
+            maxH.push(nums[i++]);
+        for(int j=0;j<k/2;j++) //putting half element from maxh to minh to balance both 
         {
-            high.push(low.top());
-            low.pop();
+            minH.push(maxH.top());
+            maxH.pop();
         }
         int balance=0;
-        while(true)
+        while(true) //for remaining elements
         {
-            medians.push_back(k&1?low.top():((double)low.top()+(double)high.top())*0.5); //get median
-            if(i>=nums.size()) break; //if all elements are processed
-            int out_num=nums[i-k]; //outgoing element
-            int in_num=nums[i++]; //incoming element
-            balance=0; //balance factor
-            balance+=(out_num<=low.top()?-1:1); //out_num exits window
+            if(k&1) //k is odd
+                medians.push_back(maxH.top());
+            else
+                medians.push_back(((double)maxH.top()+(double)minH.top())*0.5);
+            if(i>=nums.size()) break; //processed all elements;
+            int out_num=nums[i-k]; //outgoing number
+            int in_num=nums[i++]; //incoming number
             mp[out_num]++;
-            if(!low.empty() and in_num<=low.top()) //in_num enters window
+            balance=0;
+            balance+=(out_num<=maxH.top()?-1:1); //outgoing number exit
+            if(!maxH.empty() and in_num<=maxH.top())
             {
                 balance++;
-                low.push(in_num);
+                maxH.push(in_num);
             }
             else
             {
                 balance--;
-                high.push(in_num);
+                minH.push(in_num);
             }
-            if(balance<0) //rebalance heaps low needs more valid elements
+            if(balance<0) //maxh need more valid no
             {
-                low.push(high.top());
-                high.pop();
+                maxH.push(minH.top());
+                minH.pop();
                 balance++;
             }
-            if(balance>0) //high needs more valid elements
+            if(balance>0) //minh need more valid no
             {
-                high.push(low.top());
-                low.pop();
+                minH.push(maxH.top());
+                maxH.pop();
                 balance--;
             }
-            while(mp[low.top()]) //remove invalid no that should be discarded from heap tops
+            while(mp[maxH.top()]) //invalid number in maxh, outgoing no reach top of maxh
             {
-                mp[low.top()]--;
-                low.pop();
+                mp[maxH.top()]--;
+                maxH.pop();
             }
-            while(!high.empty() and mp[high.top()])
+            while(!minH.empty() and mp[minH.top()])
             {
-                mp[high.top()]--;
-                high.pop();
+                mp[minH.top()]--;
+                minH.pop();
             }
         }
         return medians;
