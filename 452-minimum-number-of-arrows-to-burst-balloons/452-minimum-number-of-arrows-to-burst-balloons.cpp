c45fe62d7ac17ea67fa7ad52dbb8b5210d7d5e20
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-15 19:57:04 +0530

    Time: 786 ms (37.40%), Space: 102.4 MB (9.42%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -8,12 +8,12 @@ public:
         int n=points.size();
         if(n==0) return 0;
         sort(points.begin(),points.end(),compare);
-       vector<int> prev=points[0];
+        vector<int> prev=points[0];
         int arrow=1;
         for(int i=1;i<n;i++)
         {
             vector<int> cur=points[i];
-            if(cur[0]>prev[1]) 
+            if(cur[0]>prev[1])
             {
                 arrow++;
                 prev=cur;
