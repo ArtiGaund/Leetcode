f4894938ef755b3e225697d1a3d5f92791f7c147
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-15 15:02:22 +0530

    Time: 54 ms (38.36%), Space: 20.1 MB (19.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-       sort(intervals.begin(), intervals.end());
-
-        vector<vector<int>> merged;
-        for (auto interval : intervals) {
-            // if the list of merged intervals is empty or if the current
-            // interval does not overlap with the previous, simply append it.
-            if (merged.empty() || merged.back()[1] < interval[0]) {
-                merged.push_back(interval);
-            }
-            // otherwise, there is overlap, so we merge the current and previous
-            // intervals.
-            else {
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+        if(intervals.size()==0) return intervals;
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> merge;
+        vector<int> prev=intervals[0];
+        for(int i=1;i<intervals.size();i++)
+        {
+            vector<int> cur=intervals[i];
+            if(intervalOverlap(prev,cur))
+                prev[1]=max(prev[1],cur[1]);
+            else
+            {
+                merge.push_back(prev);
+                prev=cur;
             }
         }
-        return merged;
+        merge.push_back(prev);
+        return merge;
+    }
+    bool intervalOverlap(vector<int> &a,vector<int> &b)
+    {
+        if(b[0]>a[1]) return false;
+        return true;
     }
 };
