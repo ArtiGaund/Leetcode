abd5b9bc596f33c4ed12d2406bf32eb12ff2d67a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-15 16:44:20 +0530

    Time: 21 ms (56.13%), Space: 17.1 MB (76.15%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> merge;
-        if(intervals.size()==0) 
+        vector<vector<int>> res;
+        int n=intervals.size();
+        if(n==0)
         {
-            merge.push_back(newInterval);
-            return merge;
+            res.push_back(newInterval);
+            return res;
         }
         int i=0;
-        int n=intervals.size();
-        while(i<n and intervals[i][1]<newInterval[0]) //skip all intervals which comes before the new       
-           merge.push_back(intervals[i++]);                // interval
+        while(i<n and intervals[i][1]<newInterval[0])
+            res.push_back(intervals[i++]);
         while(i<n and intervals[i][0]<=newInterval[1])
         {
             newInterval[0]=min(intervals[i][0],newInterval[0]);
             newInterval[1]=max(intervals[i][1],newInterval[1]);
             i++;
         }
-        merge.push_back(newInterval);
+        res.push_back(newInterval);
         while(i<n)
-            merge.push_back(intervals[i++]);
-        return merge;
+            res.push_back(intervals[i++]);
+        return res;
     }
 };
