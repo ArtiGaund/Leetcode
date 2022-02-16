7ba7bed5fec0cdc93c620719c66b043cf019d13c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-16 16:26:35 +0530

    Time: 103 ms, Memory: 16.1 MB - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int m,n;
-    int maxPath;
-    vector<vector<int>> dir={{0,-1},{-1,0},{1,0},{0,1}};
+    int n,m,maxPath;
+    vector<vector<int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        m=matrix.size();
-        n=matrix[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        maxPath=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
+        n=matrix.size();
+        m=matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
                 maxPath=max(maxPath,dfs(matrix,dp,i,j));
-        }
         return maxPath;
     }
     int dfs(vector<vector<int>> &mat,vector<vector<int>> &dp,int i,int j)
@@ -23,7 +19,7 @@ public:
         {
             int new_i=i+dir[d][0];
             int new_j=j+dir[d][1];
-            if(new_i<0 or new_i>=m or new_j<0 or new_j>=n or mat[new_i][new_j]<=mat[i][j]) continue;
+            if(new_i<0 or new_i>=n or new_j<0 or new_j>=m or mat[new_i][new_j]<=mat[i][j]) continue;
             dp[i][j]=max(dp[i][j],1+dfs(mat,dp,new_i,new_j));
         }
         return dp[i][j];
