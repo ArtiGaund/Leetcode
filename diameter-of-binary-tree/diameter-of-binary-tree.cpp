22ba855a6a70b8f3342f4e4ce7814061583a8816
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-16 16:43:38 +0530

    Time: 4 ms, Memory: 20.2 MB - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,42 +11,19 @@
  */
 class Solution {
 public:
-    int max_val=INT_MIN;
+    int height(TreeNode* root,int &res)
+    {
+        if(root==nullptr) return 0;
+        int l=height(root->left,res);
+        int r=height(root->right,res);
+        int dia=l+r;
+        res=max(dia,res);
+        return max(l,r)+1;
+    }
     int diameterOfBinaryTree(TreeNode* root) {
-        // base case
-        if(root==nullptr or (root->left==nullptr and root->right==nullptr)) return 0;
-        unordered_map<TreeNode*,int> mp;
-        stack<TreeNode*> s;
-        mp[root]=0;
-        s.push(root);
-        while(!s.empty())
-        {
-            TreeNode *cur=s.top();
-            s.pop();
-            if(mp.find(cur)!=mp.end() and mp[cur]==0)
-            {
-                s.push(cur);
-                mp[cur]=1;  // indicate we will process children of this node
-                if(cur->left)
-                {
-                    s.push(cur->left);
-                    mp[cur->left]=0;
-                }
-                if(cur->right)
-                {
-                    s.push(cur->right);
-                    mp[cur->right]=0;
-                }
-            }
-            else
-            {
-                //both children of popped node have been processed already
-                int lmax=(cur->left==nullptr?0:mp[cur->left]); //longest path from left subtree
-                int rmax=(cur->right==nullptr?0:mp[cur->right]); //longest path from right substrr
-                max_val=max(max_val,1+lmax+rmax);
-                mp[cur]=max(lmax,rmax)+1; //update the popped value
-            }
-        }
-        return max_val-1; //subtrack 1 since we need edge count, but we stored node count
+        if(root==nullptr) return 0;
+        int res=0;
+        height(root,res);
+        return res;
     }
 };
