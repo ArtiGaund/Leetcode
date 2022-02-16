82adfcd24c68ff101bf6af70cc43ba33c117731e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-16 18:43:07 +0530

    Time: 1724 ms, Memory: 16.2 MB - LeetHub

---
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        if(n==0) return 0;
+        int count=0;
+        set<int> vis;
+        for(int i=0;i<n;i++)
+        {
+            if(vis.find(i)==vis.end())
+            {
+                count++;
+                dfs(stones,vis,i);
+            }
+        }
+        return n-count;
+    }
+    void dfs(vector<vector<int>> &stones,set<int> &vis,int index)
+    {
+        vis.insert(index);
+        for(int i=0;i<stones.size();i++)
+        {
+            if(vis.find(i)!=vis.end()) continue;
+            if(stones[i][0]==stones[index][0] or stones[i][1]==stones[index][1])
+                dfs(stones,vis,i);
+        }
+    }
+};
