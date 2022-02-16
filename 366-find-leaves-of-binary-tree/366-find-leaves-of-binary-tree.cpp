104e0d1c982028d8a8ae04b8836011e5df654434
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-17 00:44:44 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (11.71%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -11,35 +11,22 @@
  */
 class Solution {
 public:
-    //DFS to sorting
-    vector<pair<int,int>> pairs;
+    //DFS without sort
+    vector<vector<int>> res;
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        if(root==nullptr) return res;
+        getheight(root);
+        return res;
+    }
     int getheight(TreeNode* root)
     {
         if(root==nullptr) return -1;
         int left=getheight(root->left);
         int right=getheight(root->right);
         int curH=max(left,right)+1;
-        this->pairs.push_back({curH,root->val});
+        if(curH==res.size())
+            res.push_back({});
+        res[curH].push_back(root->val);
         return curH;
-    }
-    vector<vector<int>> findLeaves(TreeNode* root) {
-        this->pairs.clear();
-        getheight(root);
-        sort(this->pairs.begin(),this->pairs.end());
-        vector<vector<int>> res;
-        int n=this->pairs.size(),height=0,i=0;
-        while(i<n)
-        {
-            vector<int> temp;
-            while(i<n and this->pairs[i].first==height)
-            {
-                temp.push_back(this->pairs[i].second);
-                i++;
-            }
-            res.push_back(temp);
-            height++;
-        }
-        return res;
-        
     }
 };
