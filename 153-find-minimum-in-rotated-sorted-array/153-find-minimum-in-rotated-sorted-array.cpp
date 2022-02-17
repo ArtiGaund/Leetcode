4054ccfc997442ec623b9aaf1f0b88bc505c34cc
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-17 16:25:26 +0530

    Time: 7 ms (33.67%), Space: 10 MB (96.06%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
         {
             int mid=low+(high-low)/2;
             if(nums[mid]<nums[high]) high=mid;
-            else if(nums[mid]>nums[high]) low=mid+1;
+            else low=mid+1;
         }
         return nums[low];
     }
