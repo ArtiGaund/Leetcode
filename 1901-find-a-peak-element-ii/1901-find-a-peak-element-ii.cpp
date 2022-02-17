e065f1f874e0e6a1cbd5a4213f7c12f1de0150b6
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-17 19:11:07 +0530

    Time: 203 ms (62.90%), Space: 46.2 MB (66.38%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<int> findPeakGrid(vector<vector<int>>& matrix) {
-       int n = matrix.size(), m = matrix[0].size(), lo = 0, hi = m - 1, mid; 
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            int max_row = 0;
-            for (int i = 0; i < n; ++i) {
-                if (matrix[max_row][mid] < matrix[i][mid])
-                    max_row = i;
-            }
-            if ((mid == 0 || matrix[max_row][mid] > matrix[max_row][mid - 1]) && 
-                (mid == m - 1 || matrix[max_row][mid] > matrix[max_row][mid + 1]))
-                return {max_row, mid};
-            else if (mid > 0 && matrix[max_row][mid - 1] > matrix[max_row][mid])
-                hi = mid - 1;
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        int low=0,high=m-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int max_row=0;
+            for(int i=0;i<n;i++)
+                if(mat[max_row][mid]<mat[i][mid])
+                    max_row=i;
+            if((mid==0 or mat[max_row][mid]>mat[max_row][mid-1]) and (mid==m-1 or mat[max_row][mid]>mat[max_row][mid+1]))
+                return {max_row,mid};
+            else if(mid>0 and mat[max_row][mid-1]>mat[max_row][mid])
+                high=mid-1;
             else
-                lo = mid + 1;
+                low=mid+1;
         }
-        return {-1, -1};
+        return {-1,-1};
     }
 };
