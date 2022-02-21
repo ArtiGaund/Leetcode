5de376cf3ccd823a4b262f72ad1532ce55f34281
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-21 16:56:58 +0530

    Time: 10 ms (69.41%), Space: 14.5 MB (13.78%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    // BFS
+    // DFS
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         if(image[sr][sc]==newColor) return image;
         int row=image.size();
         int col=image[0].size();
         int old=image[sr][sc];
         image[sr][sc]=newColor;
-        queue<pair<int,int>> q;
-        q.push({sr-1,sc});
-        q.push({sr+1,sc});
-        q.push({sr,sc-1});
-        q.push({sr,sc+1});
-        vector<vector<bool>> vis(row,vector<bool>(col,false));
-        vis[sr][sc]=true;
-        while(!q.empty())
+        vector<int> dir={0,-1,0,1,0};
+        for(int i=0;i<4;i++)
         {
-            auto p=q.front();
-            q.pop();
-            int l=p.first;
-            int r=p.second;
-            if(l>=0 and l<row and r>=0 and r<col and !vis[l][r] and image[l][r]==old)
-            {
-                image[l][r]=newColor;
-                vis[l][r]=true;
-                q.push({l-1,r});
-                q.push({l+1,r});
-                q.push({l,r-1});
-                q.push({l,r+1});
-            }
+            int nr=sr+dir[i];
+            int nc=sc+dir[i+1];
+            if(nr<0 or nr>=row or nc<0 or nc>=col or image[nr][nc]!=old) continue;
+            floodFill(image,nr,nc,newColor);
         }
         return image;
     }
