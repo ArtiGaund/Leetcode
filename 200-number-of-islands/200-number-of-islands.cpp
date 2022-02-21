4569e44895ed0bba6f91b1ff2a01da4b322b7813
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-21 18:47:55 +0530

    Time: 40 ms (70.15%), Space: 17.9 MB (22.12%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,52 @@
 class Solution {
 public:
-    //DFS
-    int n,m,count=0;
+    //BFS
     int numIslands(vector<vector<char>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
-        if(n==0) return 0;
-        for(int i=0;i<n;i++)
+        int row=grid.size();
+        int col=grid[0].size();
+        if(row==0) return 0;
+        int count=0;
+        for(int i=0;i<row;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<col;j++)
             {
                 if(grid[i][j]=='1')
                 {
                     count++;
-                    dfs(grid,i,j);
+                    grid[i][j]='0'; //mark as visited
+                    queue<int> q; //to check neighbors
+                    q.push(i*col+j);
+                    while(!q.empty())
+                    {
+                        int index=q.front();
+                        q.pop();
+                        int r=index/col;
+                        int c=index%col;
+                        if(r-1>=0 and grid[r-1][c]=='1')
+                        {
+                            q.push((r-1)*col+c);
+                            grid[r-1][c]='0';
+                        }
+                        if(r+1<row and grid[r+1][c]=='1')
+                        {
+                            q.push((r+1)*col+c);
+                            grid[r+1][c]='0';
+                        }
+                        if(c-1>=0 and grid[r][c-1]=='1')
+                        {
+                            q.push(r*col+(c-1));
+                            grid[r][c-1]='0';
+                        }
+                        if(c+1<col and grid[r][c+1]=='1')
+                        {
+                            q.push(r*col+(c+1));
+                            grid[r][c+1]='0';
+                        }
+                    }
                 }
             }
         }
         return count;
     }
-    void dfs(vector<vector<char>> &grid,int i,int j)
-    {
-        if(i<0 or i>=n or j<0 or j>=m or grid[i][j]!='1') return;
-        grid[i][j]='*';
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
-    }
+    
 };
