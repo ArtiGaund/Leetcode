b1b142c8a8600ced5663872810c098bdaaed2bc9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-22 10:44:19 +0530

    Time: 234 ms (67.74%), Space: 75.1 MB (44.09%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // same as largest rectangle in histogram
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int> height(n,0);
+        int max_area=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==1)
+                    height[j]+=1;
+                else height[j]=0;
+            }
+            vector<int> temp=height;
+            sort(temp.begin(),temp.end());
+            for(int j=0;j<n;j++)
+                max_area=max(max_area,temp[j]*(n-j));
+        }
+       
+        return max_area;
+    }
+};
