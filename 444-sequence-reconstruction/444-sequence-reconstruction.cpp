5eb774e87a83e5acde3c3398ccd1b9fb9b5ae204
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-22 11:53:19 +0530

    Time: 109 ms (75.48%), Space: 42.4 MB (91.35%) - LeetHub

---
+++ 444-sequence-reconstruction/444-sequence-reconstruction.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool sequenceReconstruction(vector<int>& nums, vector<vector<int>>& sequences) {
+        int n=nums.size();
+        vector<int> adj[n+1];
+         vector<int> indegree(n+1,0);
+        for(auto s:sequences)
+        {
+            for(int i=0;i<s.size()-1;i++)
+            {
+                adj[s[i]].push_back(s[i+1]);
+                indegree[s[i+1]]++;
+            }
+                
+        }
+       
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(auto it:adj[i])
+        //         indegree[it]++;
+        // }
+        queue<int> q;
+        for(int i=1;i<=n;i++)
+            if(indegree[i]==0) 
+                q.push(i);
+        if(q.size()!=1) return false;
+        int index=0;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            if(node!=nums[index++]) return false;
+            for(auto it:adj[node])
+            {
+                if(--indegree[it]==0)
+                    q.push(it);
+            }
+            if(q.size()>1) return false;
+        }
+        return index==n;
+    }
+};
