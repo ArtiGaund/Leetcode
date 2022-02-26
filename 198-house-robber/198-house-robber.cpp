531fe529759c258824ce0805c847363c00f5ce9d
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-26 10:47:25 +0530

    Time: 4 ms (35.99%), Space: 7.7 MB (47.00%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-       int n1=0,n2=nums[0],temp;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        int n1=0,n2=nums[0],temp;
         for(int i=1;i<n;i++)
         {
             temp=max(n1+nums[i],n2);
@@ -11,5 +14,4 @@ public:
         }
         return n2;
     }
-   
 };
