19202d0ddf83bcb1d4ea40c050f5b122ee76bc65
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-26 10:50:07 +0530

    Time: 4 ms (39.49%), Space: 8 MB (17.13%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==0) return 0;
         if(n==1) return nums[0];
         if(n==2) return max(nums[0],nums[1]);
         int res1=solve(nums,0,n-2);
         int res2=solve(nums,1,n-1);
         return max(res1,res2);
     }
-    int solve(vector<int> &nums,int start,int end)
+    int solve(vector<int> nums,int s,int e)
     {
-        if(start==end) return nums[start];
         int prev=0,cur=0;
-        for(int i=start;i<=end;i++)
+        for(int i=s;i<=e;i++)
         {
             int temp=max(prev+nums[i],cur);
             prev=cur;
