db5d4db25b589a846f05e19a250d3847f50c2c8e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-27 15:29:40 +0530

    Time: 129 ms (67.24%), Space: 53.9 MB (41.45%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     //min heap
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        priority_queue<int,vector<int>,greater<int>> minH;
+        priority_queue<int> maxH;
         for(int i=0;i<heights.size()-1;i++)
         {
             int climb=heights[i+1]-heights[i]; 
-            if(climb<=0) continue; //jumping down
-            minH.push(climb); //add ladder to min heap
-            if(minH.size()<=ladders) continue; //all ladders are not used
-            bricks-=minH.top(); //otherwise use bricks
-            minH.pop();
-            if(bricks<0) //no bricks left cnt move forward
-                return i;
+            if(climb<=0) continue; //going down
+            maxH.push(climb); // adding ladder
+            bricks-=climb;
+            if(bricks<0 and ladders==0) return i; // used all bricks and no ladders remaining
+            if(bricks<0) // bricks used then replace largest brick allocation with ladder
+            {
+                bricks+=maxH.top();
+                maxH.pop();
+                ladders--;
+            }
         }
         return heights.size()-1; //reached end 
     }
