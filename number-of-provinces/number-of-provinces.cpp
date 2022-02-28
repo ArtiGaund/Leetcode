93d6eaad7e8b495e738ae2a074a8d25db1526bc2
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-28 10:17:59 +0530

    Time: 26 ms, Memory: 13.7 MB - LeetHub

--- number-of-provinces/number-of-provinces.cpp
+++ number-of-provinces/number-of-provinces.cpp
@@ -1,26 +1,59 @@
-class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& v) {
-        int n=v.size();
-        vector<bool> vis(n,false);
-        stack<int> s;
-        int res=0;
+class UnionFind
+{
+    public:
+    vector<int> parent;
+    vector<int> rank;
+    int count;
+    UnionFind(int n):parent(n),rank(n),count(n)
+    {
         for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            parent[i]=i;
+            rank[i]=1;
+        }
+    }
+    int find(int a)
+    {
+        if(parent[a]!=a)
+            parent[a]=find(parent[a]);
+        return parent[a];
+    }
+    void unionSet(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
+        {
+            if(rank[x]>rank[y])
+                parent[y]=x;
+            else
             {
-                res++;
-                s.push(i);
-                while(!s.empty())
-                {
-                    int cur=s.top();
-                    s.pop();
-                    vis[cur]=true;
-                    for(int j=0;j<v[cur].size();j++)
-                        if(!vis[j] and v[cur][j]==1) s.push(j);
-                }
+                parent[x]=y;
+                if(rank[x]==rank[y])
+                    rank[y]++;
+            }
+            count--;
+        }
+    }
+    int getCount()
+    {
+        return count;
+    }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        if(n==0) return 0;
+        UnionFind uf(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                    uf.unionSet(i,j);
             }
         }
-        return res;
+        return uf.getCount();
     }
 };
