fa69e124664a7f4973ad868a0d5d00177ff41fc8
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-02-28 10:08:17 +0530

    Time: 26 ms (56.98%), Space: 12.1 MB (74.64%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,28 +1,52 @@
+class UnionFind
+{
+    public:
+        vector<int> parent;
+        vector<int> rank;
+        int count;
+    UnionFind(int n)
+    {
+        parent=vector<int>(n);
+        rank=vector<int>(n);
+        count=n;
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=1;
+        }
+    }
+    int find(int x)
+    {
+        if(parent[x]!=x)
+            parent[x]=find(parent[x]);
+        return parent[x];
+    }
+    bool unionFind(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x==y) return false;
+        if(rank[x]>rank[y])
+            parent[y]=x;
+        else
+        {
+            parent[x]=y;
+            if(rank[x]==rank[y]) rank[y]++;
+        }
+        count--;
+        return true;
+    }
+};
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
         if(edges.size()!=n-1) return false;
-        vector<int> adj[n+1];
-        for(auto e:edges)
+        UnionFind uf(n);
+        for(auto v:edges)
         {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+            if(!uf.unionFind(v[0],v[1]))
+                return false;
         }
-        queue<int> q;
-        set<int> seen;
-        q.push(0);
-        seen.insert(0);
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(int neighbor:adj[node])
-            {
-                if(seen.find(neighbor)!=seen.end()) continue;
-                seen.insert(neighbor);
-                q.push(neighbor);
-            }
-        }
-        return seen.size()==n;
+        return true;
     }
 };
