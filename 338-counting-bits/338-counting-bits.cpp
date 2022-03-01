538aaafbac5c4be7fce31ee88fbb8b490df52922
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-03-01 11:06:25 +0530

    Time: 5 ms (68.32%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    vector<int> countBits(int num) {
-        vector<int> res(num+1);
-        for(int i=1;i<=num;i++)
+    vector<int> countBits(int n) {
+        vector<int> res(n+1);
+        for(int i=1;i<=n;i++)
             res[i]=res[i&(i-1)]+1;
         return res;
     }
