e4533e5ca5a89849d01086afa9087a4a17911d4e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-03-01 11:55:05 +0530

    Time: 211 ms (36.96%), Space: 54.4 MB (12.40%) - LeetHub

--- 305-number-of-islands-ii/305-number-of-islands-ii.cpp
+++ 305-number-of-islands-ii/305-number-of-islands-ii.cpp
@@ -1,79 +1,87 @@
+class UnionFind
+{
+    public: 
+    vector<int> parent;
+    vector<int> rank;
+    int count;
+    UnionFind(int n):parent(n),rank(n),count(0)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=-1;
+            rank[i]=1;
+        }
+    }
+    void setParent(int x)
+    {
+        parent[x]=x;
+        count++;
+    }
+    int find(int x)
+    {
+        if(x==-1) return x;
+        if(parent[x]!=x)
+            parent[x]=find(parent[x]);
+        return parent[x];
+    }
+    void unionset(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
+        {
+            if(rank[x]>rank[y]) parent[y]=x;
+            else if(rank[x]<rank[y]) parent[x]=y;
+            else
+            {
+                parent[y]=x;
+                rank[x]++;
+            }
+            count--;
+        }
+    }
+    int getCount()
+    {
+        return count;
+    }
+};
 class Solution {
 public:
-    void dsfInfectIslandId(int newId, int r, int c, vector<vector<int>> &grid) {
-        if ((r > -1) && (c > -1) && (r < grid.size()) && (c < grid[0].size()) && (grid[r][c] != 0) && grid[r][c] != newId) {
-            grid[r][c] = newId;
-            dsfInfectIslandId(newId, r-1, c, grid);
-            dsfInfectIslandId(newId, r+1, c, grid);
-            dsfInfectIslandId(newId, r, c-1, grid);
-            dsfInfectIslandId(newId, r, c+1, grid);
-        }
-        return;
-    }
-public:
     vector<int> numIslands2(int m, int n, vector<vector<int>>& positions) {
-        vector<vector<int>>grid(m,vector<int>(n,0));
-        int numOfIslands = 0;
-        vector<int> result;
-        int currentIslandId = 0;
-        int retrievedIslandId = 0;
-        int numOfBridged = 0;
-
-        for (auto position : positions) {
-            if (grid[position[0]][position[1]] != 0) {
-                result.push_back(numOfIslands);
-                continue;
+        vector<int> res;
+        if(m==0)
+            return {};
+        UnionFind uf(m*n);
+        vector<vector<int>> v(m,vector<int>(n,0));
+        int k=0;
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                v[i][j]=k++;
+        vector<bool> vis(m*n,false);
+        vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        for(auto pos:positions)
+        {
+            int r=pos[0];
+            int c=pos[1];
+           int index=v[r][c];
+            if(!vis[index])
+            {
+                uf.setParent(index);
+                vis[index]=true;
             }
-            if (position[0] - 1 > -1 && grid[position[0] - 1][position[1]] != 0) {
-                ++numOfBridged;
-                retrievedIslandId = grid[position[0] - 1][position[1]];
-            }
-            if (position[0] + 1 < grid.size() && grid[position[0] + 1][position[1]] != 0) {
-                if (retrievedIslandId != grid[position[0] + 1][position[1]]) {
-                    ++numOfBridged;
-                    if (retrievedIslandId == 0) {
-                        retrievedIslandId = grid[position[0] + 1][position[1]];
-                    }
-                    else {
-                        dsfInfectIslandId(retrievedIslandId, position[0] + 1, position[1], grid);
-                    }
+            for(auto it:dir)
+            {
+                int x=r+it[0];
+                int y=c+it[1];
+                if(x>=0 and x<m and y>=0 and y<n)
+                {
+                    int index1=v[x][y];
+                    if(vis[index1]==true)
+                        uf.unionset(index,index1);
                 }
             }
-            if (position[1] - 1 > -1 && grid[position[0]][position[1] - 1] != 0) {
-                if (retrievedIslandId != grid[position[0]][position[1] - 1]) {
-                    ++numOfBridged;
-                    if (retrievedIslandId == 0) {
-                        retrievedIslandId = grid[position[0]][position[1] - 1];
-                    }
-                    else {
-                        dsfInfectIslandId(retrievedIslandId, position[0], position[1] - 1, grid);
-                    }
-                }                
-            }
-            if (position[1] + 1 < grid[0].size() && grid[position[0]][position[1] + 1] != 0) {
-                if (retrievedIslandId != grid[position[0]][position[1] + 1]) {
-                    ++numOfBridged;
-                    if (retrievedIslandId == 0) {
-                        retrievedIslandId = grid[position[0]][position[1] + 1];
-                    }
-                    else {
-                        dsfInfectIslandId(retrievedIslandId, position[0], position[1] + 1, grid);
-                    }
-                }
-            }
-            if (retrievedIslandId != 0) {
-                grid[position[0]][position[1]] = retrievedIslandId;
-                retrievedIslandId = 0;
-                numOfIslands -= (numOfBridged - 1);
-                numOfBridged = 0;
-            }
-            else {
-                ++currentIslandId;
-                ++numOfIslands;
-                grid[position[0]][position[1]] = currentIslandId;
-            }
-            result.push_back(numOfIslands);
+            res.push_back(uf.getCount());
         }
-        return result;
+        return res;
     }
 };
