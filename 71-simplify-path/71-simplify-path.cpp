249ba65201b83c79a244aa700fad0082b9e1d334
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-03-14 10:04:09 +0530

    Time: 8 ms (62.74%), Space: 10.7 MB (18.17%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string res="/";
+        stack<string> s;
+        for(int i=0;i<path.size();i++)
+        {
+            if(path[i]!='/')
+            {
+                string temp="";
+                while(path[i]!='/' and i<path.size())
+                {
+                    temp+=path[i];
+                    i++;
+                }
+                if(temp=="..")
+                {
+                    if(!s.empty()) s.pop();
+                }
+                else
+                {
+                    string t=".";
+                    if(temp!=t) s.push(temp);
+                }
+            }
+        }
+        if(s.empty()) return res;
+        while(!s.empty())
+        {
+            res=s.top()+res;
+            s.pop();
+            res="/"+res;
+        }
+        res.erase(res.size()-1);
+        return res;
+            
+    }
+};
