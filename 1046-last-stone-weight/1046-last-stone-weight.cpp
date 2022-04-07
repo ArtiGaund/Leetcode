da1ec3f017bca42fa9a963f82bd4d3150f6c5fd1
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-04-07 19:21:45 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> maxH;
-        for(int a:stones) maxH.push(a);
-        while(maxH.size()>1)
+        priority_queue<int> max_h;
+        for(auto a:stones) max_h.push(a);
+        while(max_h.size()>1)
         {
-            int first=maxH.top();
-            maxH.pop();
-            int second=maxH.top();
-            maxH.pop();
-            if(first!=second)
-            {
-                maxH.push(abs(first-second));
-            }
+            int first=max_h.top();
+            max_h.pop();
+            int sec=max_h.top();
+            max_h.pop();
+            if(first!=sec)
+                max_h.push(abs(first-sec));
         }
-        int res=maxH.empty()?0:maxH.top();
+        int res=max_h.empty()?0:max_h.top();
         return res;
     }
 };
