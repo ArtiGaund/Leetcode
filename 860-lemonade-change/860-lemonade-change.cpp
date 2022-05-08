60b758cc0296d83fdcfe6e70a4c317c6a3340256
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-05-08 10:38:28 +0530

    Time: 140 ms (47.39%), Space: 83.5 MB (50.11%) - LeetHub

---
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n5=0,n10=0;
+        int n=bills.size();
+        for(int i=0;i<n;i++)
+        {
+            if(bills[i]==5) n5++;
+            else if(bills[i]==10)
+            {
+                if(n5)
+                {
+                    n5--;
+                    n10++;
+                }
+                else return false;
+            }
+            else
+            {
+                if(n5 and n10)
+                {
+                    n5--;
+                    n10--;
+                }
+                else
+                {
+                    if(n5>=3) n5-=3;
+                    else return false;
+                }
+            }
+        }
+        return true;
+    }
+};
