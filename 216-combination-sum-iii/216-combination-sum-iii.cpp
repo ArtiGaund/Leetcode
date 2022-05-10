5a961e68a721b6fcd91fe9ab90e69ea16b7b211a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-05-10 18:25:12 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (58.80%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> cur;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtrack(k,n,1);
+        return res;
+    }
+    void backtrack(int k,int target,int index)
+    {
+        if(target==0)
+        {
+            if(cur.size()==k) res.push_back(cur);
+            return;
+        }
+        for(int i=index;i<=9;i++)
+        {
+            cur.push_back(i);
+            backtrack(k,target-i,i+1);
+            cur.pop_back();
+        }
+    }
+};
