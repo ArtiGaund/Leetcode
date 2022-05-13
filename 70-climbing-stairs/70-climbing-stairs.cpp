ae68c35692b424fd3c99c326bf56d11fdb350d56
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-05-13 11:19:14 +0530

    Time: 6 ms (8.94%), Space: 6.3 MB (16.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return topDown(n,dp);
-    }
-    int topDown(int n,vector<int> &dp)
-    {
-        if(n<=1) return dp[n]=1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=topDown(n-1,dp)+topDown(n-2,dp);
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
