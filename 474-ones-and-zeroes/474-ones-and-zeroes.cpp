bac9578912bc73038fd43f2bafce6624bfe73496
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-05-23 14:43:12 +0530

    Time: 340 ms (58.03%), Space: 9.8 MB (83.54%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    pair<int,int> countZeroOne(string s)
+    pair<int,int> countOneZero(string s)
     {
-        int zero=0,one=0;
+        int one=0,zero=0;
         for(char c:s)
         {
             if(c=='0') zero++;
@@ -14,13 +14,15 @@ public:
         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
         for(string s:strs)
         {
-           pair<int,int> temp=countZeroOne(s);
+            pair<int,int> temp=countOneZero(s);
             int zero=temp.first;
             int one=temp.second;
             for(int i=m;i>=zero;i--)
             {
                 for(int j=n;j>=one;j--)
+                {
                     dp[i][j]=max(dp[i][j],1+dp[i-zero][j-one]);
+                }
             }
         }
         return dp[m][n];
