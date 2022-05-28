d2eb561e69df7cf71be480746a4db25738b1c4be
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-05-28 15:23:22 +0530

    Time: 8 ms (24.68%), Space: 7.6 MB (90.25%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    int mod=(int)(1e9+7);
-   
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        for(int i=0;i<r;i++)
+        if(grid[0][0]==1) return 0;
+        grid[0][0]=1;
+        for(int i=1;i<r;i++)
+            grid[i][0]=(grid[i][0]==0 and grid[i-1][0]==1)?1:0;
+        for(int j=1;j<c;j++)
+            grid[0][j]=(grid[0][j]==0 and grid[0][j-1]==1)?1:0;
+        for(int i=1;i<r;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=1;j<c;j++)
             {
-                if(grid[i][j]==1)
-                    grid[i][j]=-1;
+                if(grid[i][j]==0) grid[i][j]=grid[i-1][j]+grid[i][j-1];
+                else grid[i][j]=0;
             }
         }
-       vector<int> prev(c,0);
-        for(int i=0;i<r;i++)
-        {
-            vector<int> temp(c,0);
-            for(int j=0;j<c;j++)
-            {
-                temp[j]=0;
-                if(grid[i][j]==-1) temp[j]=0;
-                else if(i==0 and j==0) temp[j]=1;
-                else
-                {
-                    int up=0,left=0;
-                    if(i>0) up=prev[j];
-                    if(j>0) left=temp[j-1];
-                    temp[j]=up+left;
-                }
-            }
-            prev=temp;
-        }
-        return prev[c-1];
+        return grid[r-1][c-1];
     }
 };
