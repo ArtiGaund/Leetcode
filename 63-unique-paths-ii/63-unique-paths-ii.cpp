2d0eb5ff8666fae9d2472338acc20ddaabf72d3e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-05-28 15:20:36 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (63.20%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -13,22 +13,25 @@ public:
                     grid[i][j]=-1;
             }
         }
-        vector<vector<int>> dp(r,vector<int>(c,-1));
-       for(int i=0;i<r;i++)
-       {
-           for(int j=0;j<c;j++)
-           {
-               if(grid[i][j]==-1) dp[i][j]=0;
-               else if(i==0 and j==0) dp[i][j]=1;
-               else 
-               {
-                   int up=0,left=0;
-                   if(i>0) up=dp[i-1][j];
-                   if(j>0) left=dp[i][j-1];
-                   dp[i][j]=(up+left);
-               }
-           }
-       }
-        return dp[r-1][c-1];
+       vector<int> prev(c,0);
+        for(int i=0;i<r;i++)
+        {
+            vector<int> temp(c,0);
+            for(int j=0;j<c;j++)
+            {
+                temp[j]=0;
+                if(grid[i][j]==-1) temp[j]=0;
+                else if(i==0 and j==0) temp[j]=1;
+                else
+                {
+                    int up=0,left=0;
+                    if(i>0) up=prev[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=up+left;
+                }
+            }
+            prev=temp;
+        }
+        return prev[c-1];
     }
 };
