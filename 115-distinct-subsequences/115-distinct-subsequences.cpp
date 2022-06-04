433a37dad113144ed0cdbd96c4b2b45a44626733
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-04 11:38:04 +0530

    Time: 64 ms (32.66%), Space: 6.4 MB (88.75%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,18 +3,16 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<double> prev(m+1,0),cur(m+1,0);
-        prev[0]=cur[0]=1;
+        vector<double> dp(m+1,0);
+        dp[0]=1;
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=m;j++)
+            for(int j=m;j>=1;j--)
             {
                 if(s[i-1]==t[j-1])
-                    cur[j]=prev[j-1]+prev[j];
-                else cur[j]=prev[j];
+                    dp[j]=dp[j-1]+dp[j];
             }
-            prev=cur;
         }
-        return (int)(prev[m]);
+        return (int)(dp[m]);
     }
 };
