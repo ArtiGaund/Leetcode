10e7c6e64d014f0418da3cc300c6b41afa2edee7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-04 18:26:16 +0530

    Time: 95 ms (42.20%), Space: 9.7 MB (67.17%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -23,8 +23,7 @@ public:
         {
             for(int j=1;j<=m;j++)
             {
-                if(p[i-1]==s[j-1] or p[i-1]=='?')
-                    dp[i][j]=dp[i-1][j-1];
+                if(p[i-1]==s[j-1] or p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
                 else if(p[i-1]=='*')
                     dp[i][j]=dp[i-1][j] or dp[i][j-1];
                 else dp[i][j]=false;
