cd7526828e7de5ffcd02adf78557ac6bdffb81e0
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-04 12:18:58 +0530

    Time: 39 ms (24.74%), Space: 9.1 MB (37.84%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(string s1,string s2,vector<vector<int>> &dp,int i,int j)
-    {
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j])
-            return dp[i][j]=solve(s1,s2,dp,i-1,j-1);
-        int in=1+solve(s1,s2,dp,i,j-1);
-        int d=1+solve(s1,s2,dp,i-1,j);
-        int r=1+solve(s1,s2,dp,i-1,j-1);
-        return dp[i][j]=min({in,d,r});
-    }
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(s1,s2,dp,n-1,m-1);
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else 
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+            }
+        }
+        return dp[n][m];
     }
 };
