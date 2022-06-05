7462c17537abdeb6e761d650cb57f93de6d5bfc5
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-05 16:47:20 +0530

    Time: 333 ms (39.41%), Space: 75.3 MB (74.73%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int> &p,vector<vector<vector<int>>> &dp,int n,int index,int buy,int cap)
-    {
-        if(cap==0) return 0;
-        if(index==n) return 0;
-        if(dp[index][buy][cap]!=-1) return dp[index][buy][cap];
-        int profit=0;
-        if(buy)
-        {
-            int take=-p[index]+solve(p,dp,n,index+1,0,cap);
-            int nottake=0+solve(p,dp,n,index+1,1,cap);
-            profit=max(take,nottake);
-        }
-        else
-        {
-            int take=p[index]+solve(p,dp,n,index+1,1,cap-1);
-            int nottake=0+solve(p,dp,n,index+1,0,cap);
-            profit=max(take,nottake);
-        }
-        return dp[index][buy][cap]=profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(prices,dp,n,0,1,2);
+        vector<vector<int>> prev(2,vector<int>(3,0)),cur(2,vector<int>(3,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=2;cap++)
+                {
+                    int profit=0;
+                    if(buy)
+                    {
+                        int take=-prices[i]+prev[0][cap];
+                        int nottake=0+prev[1][cap];
+                        profit=max(take,nottake);
+                    }
+                    else
+                    {
+                        int take=prices[i]+prev[1][cap-1];
+                        int nottake=0+prev[0][cap];
+                        profit=max(take,nottake);
+                    }
+                    cur[buy][cap]=profit;
+                }
+            }
+            prev=cur;
+        }
+        return prev[1][2];
     }
 };
