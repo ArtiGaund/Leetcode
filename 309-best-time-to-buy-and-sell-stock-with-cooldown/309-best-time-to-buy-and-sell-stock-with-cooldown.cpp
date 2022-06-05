978c97df0d3a1bc3e5fb1fdb275d180de24815f0
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-05 18:20:25 +0530

    Time: 3 ms (88.19%), Space: 11.3 MB (55.59%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int> &prices,vector<vector<int>> &dp,int n,int index,int buy)
-    {
-        if(index>=n) return 0;
-        if(dp[index][buy]!=-1) return dp[index][buy];
-        if(buy)
-        {
-            int take=-prices[index]+solve(prices,dp,n,index+1,0);
-            int nottake=0+solve(prices,dp,n,index+1,1);
-            return dp[index][buy]=max(take,nottake);
-        }
-        else
-        {
-            int take=prices[index]+solve(prices,dp,n,index+2,1);
-            int nottake=0+solve(prices,dp,n,index+1,0);
-            return dp[index][buy]=max(take,nottake);
-        }
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+2,vector<int>(2,0));
-        for(int i=n-1;i>=0;i--)
+        vector<int> f1(2,0),f2(2,0),cur(2,0);
+        for(int index=n-1;index>=0;index--)
         {
-            for(int buy=0;buy<=1;buy++)
-            {
-                if(buy)
-                {
-                    int take=-prices[i]+dp[i+1][0];
-                    int nottake=0+dp[i+1][1];
-                    dp[i][buy]=max(take,nottake);
-                }
-                else
-                {
-                    int take=prices[i]+dp[i+2][1];
-                    int nottake=0+dp[i+1][0];
-                    dp[i][buy]=max(take,nottake);
-                }
-            }
+            cur[1]=max(-prices[index]+f1[0],0+f1[1]);
+            cur[0]=max(prices[index]+f2[1],0+f1[0]);
+            f2=f1;
+            f1=cur;
         }
-        return dp[0][1];
+        return cur[1];
     }
 };
