68b39ce9436d49b8e7db6e992aff5e46da4ef016
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-05 17:42:43 +0530

    Time: 11 ms (71.01%), Space: 12.8 MB (32.99%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
+    int solve(vector<int> &prices,vector<vector<int>> &dp,int n,int k,int index,int trans)
+    {
+        if(index==n or trans==2*k) return 0;
+        if(dp[index][trans]!=-1) return dp[index][trans];
+        int profit=0;
+        if(trans%2==0) //buy
+        {
+            int take=-prices[index]+solve(prices,dp,n,k,index+1,trans+1);
+            int nottake=0+solve(prices,dp,n,k,index+1,trans);
+            profit=max(take,nottake);
+        }
+        else
+        {
+            int take=prices[index]+solve(prices,dp,n,k,index+1,trans+1);
+            int nottake=0+solve(prices,dp,n,k,index+1,trans);
+            profit=max(take,nottake);
+        }
+        return dp[index][trans]=profit;
+    }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> prev(2,vector<int>(k+1,0));
-        vector<vector<int>> cur(2,vector<int>(k+1,0));
-        for(int index=n-1;index>=0;index--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                for(int cap=1;cap<=k;cap++)
-                {
-                    if(buy==1)
-                    {
-                        int take=-prices[index]+prev[0][cap];
-                        int nottake=0+prev[1][cap];
-                        cur[buy][cap]=max(take,nottake);
-                    }
-                    else
-                    {
-                        int take=prices[index]+prev[1][cap-1];
-                        int nottake=0+prev[0][cap];
-                        cur[buy][cap]=max(take,nottake);
-                    }
-                }
-                prev=cur;
-            }
-        }
-        return prev[1][k];
+        vector<vector<int>> dp(n,vector<int>(2*k,-1));
+        return solve(prices,dp,n,k,0,0);
     }
 };
