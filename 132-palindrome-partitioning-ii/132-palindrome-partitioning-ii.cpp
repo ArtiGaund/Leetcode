ffa3bdd7d61dd8323ba023fdf56d10a8e09b338d
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-07 11:14:01 +0530

    Time: 383 ms (46.11%), Space: 6.4 MB (91.89%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -10,24 +10,23 @@ public:
         }
         return true;
     }
-    int solve(int i,int n,string &s,vector<int> &dp)
-    {
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mincost=INT_MAX;
-        for(int j=i;j<n;j++)
-        {
-            if(isPalindrome(i,j,s))
-            {
-                int cost=1+solve(j+1,n,s,dp);
-                mincost=min(cost,mincost);
-            }
-        }
-        return dp[i]=mincost;
-    }
+   
     int minCut(string s) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return solve(0,n,s,dp)-1;
+        vector<int> dp(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            int mincost=INT_MAX;
+            for(int j=i;j<n;j++)
+            {
+                if(isPalindrome(i,j,s))
+                {
+                    int cost=1+dp[j+1];
+                    mincost=min(cost,mincost);
+                }
+            }
+            dp[i]=mincost;
+        }
+        return dp[0]-1;
     }
 };
