0b878a04306e3f1d9a5250dfba6e0e7e2cc2142c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-07 12:08:42 +0530

    Time: 48 ms (83.56%), Space: 13.1 MB (82.15%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    int maxAreaFunction(vector<int> &dp)
-    {
-        int m=dp.size();
-        stack<int> s;
-        s.push(-1);
-        int maxArea=0;
-        for(int i=0;i<m;i++)
-        {
-            while(s.top()!=-1 and dp[s.top()]>=dp[i])
-            {
-                int cur_h=dp[s.top()];
-                s.pop();
-                int cur_width=i-s.top()-1;
-                maxArea=max(maxArea,cur_h*cur_width);
-            }
-            s.push(i);
+   int largestRectangleArea(vector < int > & histo) {
+      stack < int > st;
+      int maxA = 0;
+      int n = histo.size();
+      for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+          int height = histo[st.top()];
+          st.pop();
+          int width;
+          if (st.empty())
+            width = i;
+          else
+            width = i - st.top() - 1;
+          maxA = max(maxA, width * height);
         }
-        while(s.top()!=-1)
-        {
-            int cur_h=dp[s.top()];
-            s.pop();
-            int cur_width=m-s.top()-1;
-            maxArea=max(maxArea,cur_h*cur_width);
-        }
-        return maxArea;
+        st.push(i);
+      }
+      return maxA;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
+        int maxArea=0;
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int> dp(m,0);
-        int maxArea=0;
+        vector<int> height(m,0);
+        // O(n *(m+n))
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                dp[j]=(matrix[i][j]=='1'?dp[j]+1:0);
+                if(matrix[i][j]=='1') height[j]++;
+                else height[j]=0;
             }
-            maxArea=max(maxArea,maxAreaFunction(dp));
+            // O(n)
+            int area=largestRectangleArea(height);
+            maxArea=max(maxArea,area);
         }
         return maxArea;
     }
