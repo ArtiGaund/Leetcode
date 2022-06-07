fabed25e34bd60a9849b7380ae0a3857f428561b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-07 11:51:53 +0530

    Time: 16 ms (83.04%), Space: 8.4 MB (80.97%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int solve(int index,vector<int> &num,int k,vector<int> &dp)
-{
-    int n=num.size();
-    if(index==n) return 0;
-    if(dp[index]!=-1) return dp[index];
-    int len=0;
-    int maxi=INT_MIN;
-    int res=INT_MIN;
-    for(int j=index;j<min(index+k,n);j++)
-    {
-        len++;
-        maxi=max(maxi,num[j]);
-        int sum=len*maxi+solve(j+1,num,k,dp);
-        res=max(res,sum);
-    }
-    return dp[index]=res;
-}
     int maxSumAfterPartitioning(vector<int>& nums, int k) {
        int n=nums.size();
-    vector<int> dp(n,-1);
-    return solve(0,nums,k,dp);
+    vector<int> dp(n+1,0);
+    for(int index=n-1;index>=0;index--)
+    {
+        int len=0;
+        int maxi=INT_MIN;
+        int res=INT_MIN;
+        for(int j=index;j<min(index+k,n);j++)
+        {
+            len++;
+            maxi=max(maxi,nums[j]);
+            int sum=len*maxi+dp[j+1];
+            res=max(res,sum);
+        }
+        dp[index]=res;
+    }
+    return dp[0];
     }
 };
