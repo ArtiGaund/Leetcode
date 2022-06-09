50763f6a857e328034d8f69ed2b0392977c05222
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-09 17:44:38 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (67.59%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<=n;i++)
         {
             for(int j=0;j<=m;j++)
-            { 
+            {
                 if(i==0 and j==0) dp[i][j]=1;
                 else if(i==0) dp[i][j]=0;
                 else if(j==0)
@@ -24,8 +24,7 @@ public:
                     {
                         dp[i][j]=dp[i-2][j];
                         char sec_p=p[i-2];
-                        if(sec_p=='.' or sec_p==sc) 
-                            dp[i][j]=dp[i][j] or dp[i][j-1];
+                        if(sec_p=='.' or sec_p==sc) dp[i][j]=dp[i][j] or dp[i][j-1];
                     }
                     else if(pc=='.' or pc==sc) dp[i][j]=dp[i-1][j-1];
                     else dp[i][j]=0;
