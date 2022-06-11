2dba6935029e9ae9dec997e685aed61738b0d495
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-11 18:52:13 +0530

    Time: 188 ms (41.76%), Space: 16.6 MB (60.94%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m=grid.size(),n=grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         if(m==1 and n==1) return 0;
         int di[]={-1,0,1,0};
         int dj[]={0,-1,0,1};
         queue<tuple<int,int,int>> q;
         q.push({0,0,k});
-        vector<vector<vector<bool>>> seen(m,vector<vector<bool>>(n,vector<bool>(k+1,0)));
-        seen[0][0][k]=1;
+        vector<vector<vector<bool>>> vis(m,vector<vector<bool>>(n,vector<bool>(k+1,0)));
+        vis[0][0][k]=1;
         int ans=0;
-        while (!q.empty()) {
-          ++ans;
-          for (int sz = q.size(); sz > 0; --sz) {
-            const auto [i, j, eliminate] = q.front();
-            q.pop();
-            for (int l = 0; l < 4; ++l) {
-              const int x = i + di[l];
-              const int y = j + dj[l];
-              if (x < 0 || x == m || y < 0 || y == n)
-                continue;
-              if (x == m - 1 && y == n - 1)
-                return ans;
-              if (grid[x][y] == 1 && eliminate == 0)
-                continue;
-              const int newEliminate = eliminate - grid[x][y];
-              if (seen[x][y][newEliminate])
-                continue;
-              q.push({x, y, newEliminate});
-              seen[x][y][newEliminate] = true;
+        while(!q.empty())
+        {
+            ans++;
+            for(int size=q.size();size>0;size--)
+            {
+                const auto [x,y,eliminate]=q.front();
+                q.pop();
+                for(int i=0;i<4;i++)
+                {
+                    int adjx=x+di[i];
+                    int adjy=y+dj[i];
+                    if(adjx<0 or adjx>=m or adjy<0 or adjy>=n) continue;
+                    if(adjx==m-1 and adjy==n-1) return  ans;
+                    if(grid[adjx][adjy]==1 and eliminate==0) continue;
+                    const int newEliminate=eliminate-grid[adjx][adjy];
+                    if(vis[adjx][adjy][newEliminate]) continue;
+                    q.push({adjx,adjy,newEliminate});
+                    vis[adjx][adjy][newEliminate]=1;
+                }
             }
-          }
         }
         return -1;
     }
