248c4420c55f7aab6914d78c90cfaf01e70b29df
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-12 15:30:39 +0530

    Time: 161 ms (44.27%), Space: 16.5 MB (61.41%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -4,31 +4,30 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         if(m==1 and n==1) return 0;
-        int di[]={-1,0,1,0};
-        int dj[]={0,-1,0,1};
         queue<tuple<int,int,int>> q;
         q.push({0,0,k});
         vector<vector<vector<bool>>> vis(m,vector<vector<bool>>(n,vector<bool>(k+1,0)));
         vis[0][0][k]=1;
-        int ans=0;
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
+        int steps=0;
         while(!q.empty())
         {
-            ans++;
-            for(int size=q.size();size>0;size--)
+            steps++;
+            for(int i=q.size();i>0;i--)
             {
-                const auto [x,y,eliminate]=q.front();
+                auto [x,y,eliminate]=q.front();
                 q.pop();
-                for(int i=0;i<4;i++)
+                for(auto d:dir)
                 {
-                    int adjx=x+di[i];
-                    int adjy=y+dj[i];
-                    if(adjx<0 or adjx>=m or adjy<0 or adjy>=n) continue;
-                    if(adjx==m-1 and adjy==n-1) return  ans;
-                    if(grid[adjx][adjy]==1 and eliminate==0) continue;
-                    const int newEliminate=eliminate-grid[adjx][adjy];
-                    if(vis[adjx][adjy][newEliminate]) continue;
-                    q.push({adjx,adjy,newEliminate});
-                    vis[adjx][adjy][newEliminate]=1;
+                    int new_x=x+d.first;
+                    int new_y=y+d.second;
+                    if(new_x==m-1 and new_y==n-1) return steps;
+                    if(new_x<0 or new_x>=m or new_y<0 or new_y>=n) continue;
+                    if(grid[new_x][new_y]==1 and eliminate==0) continue;
+                    int new_eliminate=eliminate-grid[new_x][new_y];
+                    if(vis[new_x][new_y][new_eliminate]) continue;
+                    q.push({new_x,new_y,new_eliminate});
+                    vis[new_x][new_y][new_eliminate]=1;
                 }
             }
         }
