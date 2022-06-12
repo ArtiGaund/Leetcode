1136b8facc7375803f1556e7ccfd39cc1c1a6700
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-12 18:30:51 +0530

    Time: 130 ms (20.01%), Space: 24.8 MB (18.95%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    int n,m,maxpath;
-    vector<vector<int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        n=matrix.size();
-        m=matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                maxpath=max(maxpath,dfs(matrix,dp,i,j));
-        return maxpath;
-    }
-    int dfs(vector<vector<int>> &v,vector<vector<int>> &dp,int i,int j)
+    int dfs(vector<vector<int>> &matrix,vector<vector<int>> &dp,int m,int n,int i,int j)
     {
-        if(dp[i][j]!=-1) return dp[i][j];
-        dp[i][j]=1;
-        for(int d=0;d<4;d++)
+        if(dp[i][j]>0) return dp[i][j];
+        int maxlen=0;
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
+        for(auto d:dir)
         {
-            int new_i=i+dir[d][0];
-            int new_j=j+dir[d][1];
-            if(new_i<0 or new_i>=n or new_j<0 or new_j>=m or v[new_i][new_j]<=v[i][j]) continue;
-            dp[i][j]=max(dp[i][j],1+dfs(v,dp,new_i,new_j));
+            int x=i+d.first;
+            int y=j+d.second;
+            if(x>=0 and x<m and y>=0 and y<n and matrix[x][y]>matrix[i][j])
+            {
+                int longest=dfs(matrix,dp,m,n,x,y);
+                maxlen=max(maxlen,longest);
+            }
         }
+        dp[i][j]=maxlen+1;
         return dp[i][j];
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        if(m==0 or n==0) return 0;
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        int maxlen=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int longest=dfs(matrix,dp,m,n,i,j);
+                maxlen=max(maxlen,longest);
+            }
+        }
+        return maxlen;
     }
 };
