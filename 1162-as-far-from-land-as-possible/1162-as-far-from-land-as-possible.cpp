9592bf2c5bbcf804cb932295eae47b81a0b3d71a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-13 17:27:34 +0530

    Time: 230 ms (12.60%), Space: 35.7 MB (13.23%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        if(grid.size()==0) return 0;
-        int row=grid.size();
-        int col=grid[0].size();
-        queue<pair<int,int>> q,q1;
-        for(int i=0;i<row;i++)
+        int m=grid.size();
+        int n=grid[0].size();
+        queue<pair<int,int>> q;
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<col;j++)
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
-                {
-                    q.push({i-1,j});
-                    q.push({i+1,j});
-                    q.push({i,j-1});
-                    q.push({i,j+1});
-                }
+                    q.push({i,j});
             }
         }
-        int step=0;
+        int maxlen=0;
+        
         while(!q.empty())
         {
-            step++;
-            while(!q.empty())
+            auto cur=q.front();
+            q.pop();
+            int x=cur.first;
+            int y=cur.second;
+            vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
+            for(auto d:dir)
             {
-                auto p=q.front();
-                q.pop();
-                int i=p.first;
-                int j=p.second;
-                if(i>=0 and i<row and j>=0 and j<col and grid[i][j]==0)
+                int new_x=x+d.first;
+                int new_y=y+d.second;
+                if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and grid[new_x][new_y]==0)
                 {
-                    grid[i][j]=step;
-                   q1.push({i-1,j});
-                    q1.push({i+1,j});
-                    q1.push({i,j-1});
-                    q1.push({i,j+1});
+                    q.push({new_x,new_y});
+                    grid[new_x][new_y]=grid[x][y]+1;
+                    maxlen=max(maxlen,grid[new_x][new_y]);
                 }
             }
-            swap(q,q1);
         }
-        return step==1?-1:step-1;
+        return (maxlen>0?maxlen-1:-1);
     }
 };
