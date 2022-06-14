8b4e7b3224ae38abf57aa23bdb7e3418c2b392f7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-14 15:49:13 +0530

    Time: 257 ms (10.88%), Space: 40 MB (13.10%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int row=mat.size();
-        int col=mat[0].size();
+        int m=mat.size(),n=mat[0].size();
+        if(m==0 or n==0) return mat;
         queue<pair<int,int>> q;
-        for(int i=0;i<row;i++)
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<col;j++)
+            for(int j=0;j<n;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    q.push({i-1,j});
-                    q.push({i+1,j});
-                    q.push({i,j-1});
-                    q.push({i,j+1});
+                    for(auto d:dir)
+                    {
+                        int x=i+d.first;
+                        int y=j+d.second;
+                        q.push({x,y});
+                    }
                 }
             }
         }
-        vector<vector<bool>> vis(row,vector<bool>(col,false));
+        vector<vector<bool>> vis(m,vector<bool>(n,0));
         int step=0;
         while(!q.empty())
         {
@@ -25,18 +28,19 @@ public:
             int size=q.size();
             for(int i=0;i<size;i++)
             {
-                auto p=q.front();
+                auto cur=q.front();
                 q.pop();
-                int x=p.first;
-                int y=p.second;
-                if(x>=0 and x<row and y>=0 and y<col and !vis[x][y] and mat[x][y]==1)
+                int x=cur.first;
+                int y=cur.second;
+                if(x>=0 and x<m and y>=0 and y<n and !vis[x][y] and mat[x][y]==1)
                 {
-                    vis[x][y]=true;
+                    vis[x][y]=1;
                     mat[x][y]=step;
-                    q.push({x-1,y});
-                    q.push({x+1,y});
-                    q.push({x,y-1});
-                    q.push({x,y+1});
+                    for(auto d:dir)
+                    {
+                        int new_x=x+d.first,new_y=y+d.second;
+                        q.push({new_x,new_y});
+                    }
                 }
             }
         }
