0ab1712bfb7d98d469847d660393d869ed24ee76
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-14 19:24:00 +0530

    Time: 324 ms (33.06%), Space: 14.4 MB (69.14%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &stones,int pos,vector<bool> &vis)
-    {
-        int x=stones[pos][0],y=stones[pos][1];
-        vis[pos]=1;
-        for(int i=0;i<stones.size();i++)
-        {
-            if((stones[i][0]==x or stones[i][1]==y) and !vis[i])
-                dfs(stones,i,vis);
+    struct subset{
+        int parent;
+        int rank;
+    };
+    int find(vector<subset> &Parent, int x){
+        if(Parent[x].parent==-1){
+            return x;
         }
+        return find(Parent, Parent[x].parent);
+    }
+    void Union(vector<subset> &Parent, int x, int y){
+        if(Parent[x].rank<Parent[y].rank){
+            Parent[x].parent = y;
+        }else if(Parent[x].rank>Parent[y].rank){
+            Parent[y].parent = x;
+        }else{
+            Parent[y].parent = x;
+            Parent[x].rank++;
+        }
+        return;
+    }
+    bool isSameRow(vector<vector<int>> &stones, int i, int j){
+        return stones[i][0]==stones[j][0];
+    }
+    bool isSameColumn(vector<vector<int>> &stones, int i, int j){
+        return stones[i][1]==stones[j][1];
     }
     int removeStones(vector<vector<int>>& stones) {
-        int n=stones.size();
-        vector<bool> vis(n,0);
-        int group=0;
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                group++;
-                dfs(stones,i,vis);
+        int n = stones.size();
+        // vector<vector<int>> edges;
+        vector<subset> Parent(n,{-1,0});
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(isSameRow(stones, i, j) or isSameColumn(stones,i, j)){
+                    int x = find(Parent, i);
+                    int y = find(Parent, j);
+                    if(x!=y){
+                        Union(Parent,x,y);
+                    }
+                }
             }
         }
-        return n-group;
+       
+    
+        int co = 0;
+        for(int i =0;i<n;i++){
+            if(Parent[i].parent==-1){
+                co++;
+            }
+        }
+        return n-co;
     }
 };
