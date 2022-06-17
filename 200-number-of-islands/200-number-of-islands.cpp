9be9ed18896b028aaf1b5322011ee0278fb2a864
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-17 16:46:13 +0530

    Time: 42 ms (74.42%), Space: 12.2 MB (84.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,81 +1,30 @@
-class UnionFind
-{
-    public:vector<int> parent;
-    vector<int> rank;
-    int count;
-    UnionFind(vector<vector<char>> &grid)
-    {
-        count=0;
-        int m=grid.size();
-        int n=grid[0].size();
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    parent.push_back(i*n+j);
-                    count++;
-                }
-                else parent.push_back(-1);
-                rank.push_back(0);
-                    
-            }
-        }
-    }
-    int find(int x)
-    {
-        if(x!=parent[x])
-            parent[x]=find(parent[x]);
-        return parent[x];
-    }
-    void unionset(int a,int b)
-    {
-        int x=find(a);
-        int y=find(b);
-        if(x!=y)
-        {
-            if(rank[x]>rank[y]) parent[y]=x;
-            else if(rank[x]<rank[y]) parent[x]=y;
-            else
-            {
-                parent[y]=x;
-                rank[x]++;
-            }
-            count--;
-        }
-    }
-    int getCount()
-    {
-        return count;
-    }
-};
 class Solution {
 public:
+    void dfs(vector<vector<char>> &grid,int i,int j)
+    {
+        if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j]!='1') return;
+        grid[i][j]='2';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        if(m==0) return 0;
-        UnionFind uf(grid);
-        int num_of_island=0;
-        vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        // vector<vector<bool>> vis(m,vector<bool>(n,0));
+        int ans=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]=='1')
                 {
-                    grid[i][j]='0';
-                    for(auto it:dir)
-                    {
-                        int x=i+it[0];
-                        int y=j+it[1];
-                        if(x>=0 and x<m and y>=0 and y<n and grid[x][y]=='1')
-                            uf.unionset(i*n+j,x*n+y);
-                    }
+                    dfs(grid,i,j);
+                    ans++;
                 }
             }
         }
-        return uf.getCount();
+        return ans;
     }
 };
