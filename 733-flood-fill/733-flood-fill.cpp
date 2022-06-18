1bc3c0c91e683991915f2d08e7252a38ff72ed64
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-18 17:08:06 +0530

    Time: 17 ms (34.05%), Space: 14 MB (80.49%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    // DFS
+    void dfs(vector<vector<int>> &image,int i,int j,int color,int newColor)
+    {
+        if(i<0 or i>=image.size() or j<0 or j>=image[0].size() or image[i][j]!=color) return;
+        image[i][j]=newColor;
+        dfs(image,i-1,j,color,newColor);
+        dfs(image,i+1,j,color,newColor);
+        dfs(image,i,j-1,color,newColor);
+        dfs(image,i,j+1,color,newColor);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc]==newColor) return image;
-        int row=image.size();
-        int col=image[0].size();
-        int old=image[sr][sc];
-        image[sr][sc]=newColor;
-        vector<int> dir={0,-1,0,1,0};
-        for(int i=0;i<4;i++)
-        {
-            int nr=sr+dir[i];
-            int nc=sc+dir[i+1];
-            if(nr<0 or nr>=row or nc<0 or nc>=col or image[nr][nc]!=old) continue;
-            floodFill(image,nr,nc,newColor);
-        }
+        int color=image[sr][sc];
+        if(color!=newColor) dfs(image,sr,sc,color,newColor);
         return image;
     }
 };
