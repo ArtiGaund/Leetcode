621253087bcb7daf04249c570112e47238ac99c3
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-19 16:50:05 +0530

    Time: 120 ms (53.78%), Space: 13.1 MB (81.88%) - LeetHub

---
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
@@ -0,0 +1,48 @@
+#define tu tuple<int,int,int>
+class Solution {
+public:
+    
+    int trapRainWater(vector<vector<int>>& height) {
+        int m=height.size();
+        int n=height[0].size();
+        priority_queue<tu,vector<tu>,greater<tu>> pq;
+        vector<vector<bool>> vis(m,vector<bool>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 or j==0 or i==m-1 or j==n-1)
+                {
+                    pq.push({height[i][j],i,j});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
+        int water=0;
+        while(!pq.empty())
+        {
+            int h=get<0>(pq.top());
+            int x=get<1>(pq.top());
+            int y=get<2>(pq.top());
+            pq.pop();
+            for(auto d:dir)
+            {
+                int new_x=x+d.first;
+                int new_y=y+d.second;
+                if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and vis[new_x][new_y]==0)
+                {
+                    water+=max(0,h-height[new_x][new_y]);
+                    if(height[new_x][new_y]>=h)
+                    {
+                        pq.push({height[new_x][new_y],new_x,new_y});
+                    }
+                    else
+                        pq.push({h,new_x,new_y});
+                    vis[new_x][new_y]=1;
+                }
+            }
+        }
+        return water;
+    }
+};
