b4dc0c1ed4b5d48dd3c5ab98edd24a104908c5fa
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-19 12:04:39 +0530

    Time: 29 ms (51.72%), Space: 23.3 MB (49.35%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int getIslandSize(vector<vector<int>> &grid,int i,int j,int islandId)
+    int getIslandSize(vector<vector<int>> &grid,int i,int j,int m,int n,int island)
     {
-        if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j]!=1) return 0;
-        grid[i][j]=islandId;
-        int left=getIslandSize(grid,i,j-1,islandId);
-        int right=getIslandSize(grid,i,j+1,islandId);
-        int up=getIslandSize(grid,i-1,j,islandId);
-        int down=getIslandSize(grid,i+1,j,islandId);
+        if(i<0 or i>=m or j<0 or j>=n or grid[i][j]!=1) return 0;
+        grid[i][j]=island;
+        int left=getIslandSize(grid,i,j-1,m,n,island);
+        int right=getIslandSize(grid,i,j+1,m,n,island);
+        int up=getIslandSize(grid,i-1,j,m,n,island);
+        int down=getIslandSize(grid,i+1,j,m,n,island);
         return left+right+up+down+1;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         if(m==0 or n==0) return 0;
-        int islandId=2;
-        int maxArea=0;
+        int maxarea=0,island=2;
+        // unordered_map<int,int> mp;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    int size=getIslandSize(grid,i,j,islandId);
-                    maxArea=max(maxArea,size);
-                    islandId++;
+                    int size=getIslandSize(grid,i,j,m,n,island);
+                    maxarea=max(size,maxarea);
+                    // mp[island]=size;
+                    island++;
                 }
             }
         }
-        return maxArea;
+        return maxarea;
     }
 };
