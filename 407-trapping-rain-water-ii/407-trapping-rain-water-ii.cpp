c3e9d1af71e2355d745762cf83a5b6c1f226150b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-19 17:11:08 +0530

    Time: 149 ms (40.80%), Space: 13 MB (88.59%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
@@ -1,12 +1,10 @@
 #define tu tuple<int,int,int>
 class Solution {
 public:
-    
     int trapRainWater(vector<vector<int>>& height) {
-        int m=height.size();
-        int n=height[0].size();
-        priority_queue<tu,vector<tu>,greater<tu>> pq;
+        int m=height.size(),n=height[0].size();
         vector<vector<bool>> vis(m,vector<bool>(n,0));
+        priority_queue<tu,vector<tu>,greater<tu>> pq;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -18,8 +16,8 @@ public:
                 }
             }
         }
-        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
         int water=0;
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
         while(!pq.empty())
         {
             int h=get<0>(pq.top());
@@ -30,15 +28,12 @@ public:
             {
                 int new_x=x+d.first;
                 int new_y=y+d.second;
-                if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and vis[new_x][new_y]==0)
+                if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and !vis[new_x][new_y])
                 {
                     water+=max(0,h-height[new_x][new_y]);
-                    if(height[new_x][new_y]>=h)
-                    {
+                    if(height[new_x][new_y]>=h) 
                         pq.push({height[new_x][new_y],new_x,new_y});
-                    }
-                    else
-                        pq.push({h,new_x,new_y});
+                    else pq.push({h,new_x,new_y});
                     vis[new_x][new_y]=1;
                 }
             }
