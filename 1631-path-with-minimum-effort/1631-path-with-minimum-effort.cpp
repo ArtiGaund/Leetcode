aa8f39b7b3f62708f65927b5cc28b93a2d38e6e2
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-19 10:38:04 +0530

    Time: 252 ms (62.05%), Space: 20.3 MB (68.21%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -4,12 +4,12 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m=heights.size();
         int n=heights[0].size();
+        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
         vector<vector<bool>> vis(m,vector<bool>(n,0));
-        vector<vector<int>> dis(m,vector<int>(n,INT_MAX));
         priority_queue<tu,vector<tu>,greater<tu>> pq;
         pq.push({0,0,0});
-        dis[0][0]=0;
-        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
+        dist[0][0]=0;
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
         while(!pq.empty())
         {
             int curDis=get<0>(pq.top());
@@ -18,22 +18,21 @@ public:
             pq.pop();
             if(vis[x][y]) continue;
             vis[x][y]=1;
-            int diff=0;
             for(auto d:dir)
             {
                 int new_x=x+d.first;
                 int new_y=y+d.second;
                 if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and !vis[new_x][new_y])
                 {
-                    diff=abs(heights[x][y]-heights[new_x][new_y]);
-                    if(dis[new_x][new_y]>diff)
+                    int diff=abs(heights[x][y]-heights[new_x][new_y]);
+                    if(dist[new_x][new_y]>diff)
                     {
-                        dis[new_x][new_y]=max(diff,dis[x][y]);
-                        pq.push({dis[new_x][new_y],new_x,new_y});
+                        dist[new_x][new_y]=max(dist[x][y],diff);
+                        pq.push({dist[new_x][new_y],new_x,new_y});
                     }
                 }
             }
         }
-        return dis[m-1][n-1];
+        return dist[m-1][n-1];
     }
 };
