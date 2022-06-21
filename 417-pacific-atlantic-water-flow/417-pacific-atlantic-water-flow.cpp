4a0ef492aa210e8b75044ec98c5f441f659dfb7c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-21 16:04:14 +0530

    Time: 41 ms (94.80%), Space: 22 MB (28.84%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>> &heights,queue<pair<int,int>> q,int m,int n,vector<vector<int>> &vis)
+    void bfs(vector<vector<int>> heights,queue<pair<int,int>> &q,int m,int n,vector<vector<int>> &vis)
     {
-        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
         while(!q.empty())
         {
             auto cur=q.front();
@@ -11,12 +11,13 @@ public:
             int y=cur.second;
             for(auto d:dir)
             {
-                int new_x=x+d.first,new_y=y+d.second;
+                int new_x=x+d.first;
+                int new_y=y+d.second;
                 if(new_x<0 or new_x>=m or new_y<0 or new_y>=n or vis[new_x][new_y]==1) continue;
                 if(heights[new_x][new_y]>=heights[x][y])
                 {
-                    vis[new_x][new_y]=1;
                     q.push({new_x,new_y});
+                    vis[new_x][new_y]=1;
                 }
             }
         }
@@ -25,40 +26,19 @@ public:
         int m=heights.size(),n=heights[0].size();
         if(m==0 or n==0) return {};
         vector<vector<int>> vis1(m,vector<int>(n,0));
-        vector<vector<int>> vis2(m,vector<int>(n,0));
+         vector<vector<int>> vis2(m,vector<int>(n,0));
         queue<pair<int,int>> q;
-        for(int i=0;i<m;i++)
-        {
-            vis1[i][0]=1;
-            q.push({i,0});
-        }
-        for(int j=0;j<n;j++)
-        {
-            vis1[0][j]=1;
-            q.push({0,j});
-        }
+        for(int i=0;i<m;i++) q.push({i,0}), vis1[i][0]=1;
+        for(int j=0;j<n;j++) q.push({0,j}), vis1[0][j]=1;
         bfs(heights,q,m,n,vis1);
         while(!q.empty()) q.pop();
-        for(int i=0;i<m;i++)
-        {
-            vis2[i][n-1]=1;
-            q.push({i,n-1});
-        }
-        for(int j=0;j<n;j++)
-        {
-            vis2[m-1][j]=1;
-            q.push({m-1,j});
-        }
+        for(int i=0;i<m;i++) q.push({i,n-1}), vis2[i][n-1]=1;
+        for(int j=0;j<n;j++) q.push({m-1,j}), vis2[m-1][j]=1;
         bfs(heights,q,m,n,vis2);
         vector<vector<int>> res;
         for(int i=0;i<m;i++)
-        {
             for(int j=0;j<n;j++)
-            {
-                if(vis1[i][j]==vis2[i][j] and vis1[i][j]==1)
-                    res.push_back({i,j});
-            }
-        }
+                if(vis1[i][j]==vis2[i][j] and vis1[i][j]==1) res.push_back({i,j});
         return res;
     }
 };
