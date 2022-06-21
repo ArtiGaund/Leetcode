f3545d5ae83a95fceb42003e1b56f5885600c3b4
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-21 16:11:14 +0530

    Time: 158 ms (20.59%), Space: 35.7 MB (13.34%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        int m=grid.size(),n=grid[0].size();
         queue<pair<int,int>> q;
         for(int i=0;i<m;i++)
-        {
             for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==1)
-                    q.push({i,j});
-            }
-        }
-        int maxlen=0;
-        
+                if(grid[i][j]==1) q.push({i,j});
+        int maxdis=0;
         while(!q.empty())
         {
             auto cur=q.front();
             q.pop();
-            int x=cur.first;
-            int y=cur.second;
+            int x=cur.first,y=cur.second;
             vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
             for(auto d:dir)
             {
-                int new_x=x+d.first;
-                int new_y=y+d.second;
+                int new_x=x+d.first,new_y=y+d.second;
                 if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and grid[new_x][new_y]==0)
                 {
                     q.push({new_x,new_y});
                     grid[new_x][new_y]=grid[x][y]+1;
-                    maxlen=max(maxlen,grid[new_x][new_y]);
+                    maxdis=max(maxdis,grid[new_x][new_y]);
                 }
             }
         }
-        return (maxlen>0?maxlen-1:-1);
+        return (maxdis>0?maxdis-1:-1);
     }
 };
