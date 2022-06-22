fb4153d4ddc0afe5e86b4a92a1256fb60baf91ef
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-22 15:11:31 +0530

    Time: 214 ms (89.96%), Space: 63.6 MB (96.09%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<vector<int>> graph(n+1);
+        vector<int> graph[n+1];
         for(int i=0;i<dislikes.size();i++)
         {
             graph[dislikes[i][0]].push_back(dislikes[i][1]);
@@ -13,13 +13,13 @@ public:
         {
             if(color[i]==-1)
             {
-                color[i]=1;
                 q.push(i);
+                color[i]=1;
                 while(!q.empty())
                 {
                     int cur=q.front();
                     q.pop();
-                    for(auto it:graph[cur])
+                    for(int it:graph[cur])
                     {
                         if(color[it]==-1)
                         {
