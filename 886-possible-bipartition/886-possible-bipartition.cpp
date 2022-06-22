c5af31f02e6a66145b4427bf8fda5d1212a13ec3
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-22 15:13:30 +0530

    Time: 331 ms (36.39%), Space: 63.9 MB (86.41%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    bool dfs(int node,vector<int> &color,vector<int> graph[])
+    {
+        if(color[node]==-1) color[node]=1;
+        for(int it:graph[node])
+        {
+            if(color[it]==-1)
+            {
+                color[it]=1-color[node];
+                if(!dfs(it,color,graph)) return false;
+            }
+            else if(color[it]==color[node]) return false;
+        }
+        return true;
+    }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<int> graph[n+1];
         for(int i=0;i<dislikes.size();i++)
@@ -8,27 +22,11 @@ public:
             graph[dislikes[i][1]].push_back(dislikes[i][0]);
         }
         vector<int> color(n+1,-1);
-        queue<int> q;
         for(int i=1;i<=n;i++)
         {
             if(color[i]==-1)
             {
-                q.push(i);
-                color[i]=1;
-                while(!q.empty())
-                {
-                    int cur=q.front();
-                    q.pop();
-                    for(int it:graph[cur])
-                    {
-                        if(color[it]==-1)
-                        {
-                            color[it]=1-color[cur];
-                            q.push(it);
-                        }
-                        else if(color[it]==color[cur]) return false;
-                    }
-                }
+                if(!dfs(i,color,graph)) return false;
             }
         }
         return true;
