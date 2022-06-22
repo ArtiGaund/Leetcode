8af9d6ed2dfeedcb34efa8c1bd8c4c7a1175013f
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-22 19:10:00 +0530

    Time: 8 ms (15.45%), Space: 8.3 MB (76.16%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(string &s,string &d,unordered_map<string,vector<pair<string,double>>> &mp,unordered_set<string> &vis,double &ans,double temp)
+    void dfs(string &s,string &d,unordered_map<string,vector<pair<string,double>>> &graph,unordered_set<string> &vis,double &ans,double temp)
     {
         if(vis.count(s)>0) return;
         vis.insert(s);
@@ -9,15 +9,15 @@ public:
             ans=temp;
             return;
         }
-        for(auto a:mp[s])
-            dfs(a.first,d,mp,vis,ans,temp*a.second);
+        for(auto a:graph[s])
+            dfs(a.first,d,graph,vis,ans,temp*a.second);
     }
     vector<double> calcEquation(vector<vector<string>>& eq, vector<double>& val, vector<vector<string>>& queries) {
-        unordered_map<string,vector<pair<string,double>>> mp;
+        unordered_map<string,vector<pair<string,double>>> graph;
         for(int i=0;i<eq.size();i++)
         {
-            mp[eq[i][0]].push_back({eq[i][1],val[i]});
-            mp[eq[i][1]].push_back({eq[i][0],1/val[i]});
+            graph[eq[i][0]].push_back({eq[i][1],val[i]});
+            graph[eq[i][1]].push_back({eq[i][0],1/val[i]});
         }
         vector<double> res;
         for(auto q:queries)
@@ -26,8 +26,7 @@ public:
             string d=q[1];
             unordered_set<string> vis;
             double ans=-1.0;
-            if(mp.find(s)!=mp.end())
-                dfs(s,d,mp,vis,ans,1.0);
+            if(graph.find(s)!=graph.end()) dfs(s,d,graph,vis,ans,1.0);
             res.push_back(ans);
         }
         return res;
