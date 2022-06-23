baefb75850a7cbba8240102d68e4aa58f7dcf341
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-23 13:54:29 +0530

    Time: 1157 ms (56.64%), Space: 249 MB (6.74%) - LeetHub

--- 675-cut-off-trees-for-golf-event/675-cut-off-trees-for-golf-event.cpp
+++ 675-cut-off-trees-for-golf-event/675-cut-off-trees-for-golf-event.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    int n,m;
-    vector<vector<int>> mat;
-    int dx[4]={-1,1,0,0};
-    int dy[4]={0,0,-1,1};
-    int bfs(int sr,int sc,int er,int ec){
+    int bfs(int sr,int sc,int er,int ec,vector<vector<int>> &mat){
         if(mat[sr][sc]==mat[er][ec]) return 0;
-        
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
         vector<vector<int>> vis(n+1,vector<int>(m+1,0));
         
         queue<pair<int,int>> qq; qq.push({sr,sc});
@@ -17,9 +15,9 @@ public:
             level++;
             while(sz--){
                 auto pos=qq.front(); qq.pop();
-                for(int i=0;i<4;i++){
-                    int x=pos.first+dx[i];
-                    int y=pos.second+dy[i];
+                for(auto d:dir){
+                    int x=pos.first+d.first;
+                    int y=pos.second+d.second;
                     if(x>=0 and y>=0 and x<n and y<m and !vis[x][y] and mat[x][y]!=0){
                         vis[x][y]=1;
                         qq.push({x,y});
@@ -30,30 +28,28 @@ public:
         }
         return -1;
     }
-    
     int cutOffTree(vector<vector<int>>& forest) {
-        if(forest[0][0]==0) return -1;//always start from (0,0)
-        
-        mat=forest;
-        n=forest.size(); m=forest[0].size();
-        map<int,pair<int,int>> vals;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(forest[i][j]>1){
-                    vals[forest[i][j]]={i,j};
-                }
+        if(forest[0][0]==0) return -1;
+        int m=forest.size();
+        int n=forest[0].size();
+        map<int,pair<int,int>> mp;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(forest[i][j]>1)
+                    mp[forest[i][j]]={i,j};
             }
         }
-        
-        int ans=0;
-        int x1=0,y1=0;
-        for(auto &v: vals){
-            int temp=bfs(x1,y1,v.second.first,v.second.second);
+        int ans=0,x=0,y=0;
+        for(auto &v:mp)
+        {
+            int temp=bfs(x,y,v.second.first,v.second.second,forest);
             if(temp==-1) return -1;
             ans+=temp;
-            x1=v.second.first; y1=v.second.second;
+            x=v.second.first;
+            y=v.second.second;
         }
-        
         return ans;
     }
 };
