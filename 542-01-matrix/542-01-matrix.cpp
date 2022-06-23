a043bbe5a8ed652a592f94d0705456bd2ca8a1e7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-23 14:19:55 +0530

    Time: 107 ms (57.01%), Space: 30.1 MB (70.23%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -3,47 +3,27 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        if(m==0 or n==0) return mat;
-        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
         queue<pair<int,int>> q;
         for(int i=0;i<m;i++)
-        {
+         {   
             for(int j=0;j<n;j++)
-            {
-                if(mat[i][j]==0)
-                {
-                    for(auto d:dir)
-                    {
-                        int x=i+d.first;
-                        int y=j+d.second;
-                        q.push({x,y});
-                    }
-                }
+             {  
+                if(mat[i][j]==0) q.push({i,j});
+                else mat[i][j]=INT_MAX;
             }
         }
-        vector<vector<bool>> vis(m,vector<bool>(n,0));
-        int steps=0;
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
         while(!q.empty())
         {
-            steps++;
-            int size=q.size();
-            for(int i=0;i<size;i++)
+            auto cur=q.front();
+            q.pop();
+            for(auto d:dir)
             {
-                auto cur=q.front();
-                q.pop();
-                int x=cur.first;
-                int y=cur.second;
-                if(x>=0 and x<m and y>=0 and y<n and !vis[x][y] and mat[x][y]==1)
-                {
-                    vis[x][y]=1;
-                    mat[x][y]=steps;
-                    for(auto d:dir)
-                    {
-                        int new_x=x+d.first;
-                        int new_y=y+d.second;
-                        q.push({new_x,new_y});
-                    }
-                }
+                int x=cur.first+d.first;
+                int y=cur.second+d.second;
+                if(x<0 or x>=m or y<0 or y>=n or mat[x][y]<=mat[cur.first][cur.second]+1) continue;
+                q.push({x,y});
+                mat[x][y]=mat[cur.first][cur.second]+1;
             }
         }
         return mat;
