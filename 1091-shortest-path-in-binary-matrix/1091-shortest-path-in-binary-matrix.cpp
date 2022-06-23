d8c321d4e71ca385b3f11840b3266f5108e9f091
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-23 14:44:32 +0530

    Time: 66 ms (91.45%), Space: 19.2 MB (83.88%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         if(grid[0][0]==1 or grid[m-1][n-1]==1) return -1;
-        vector<pair<int,int>> dir={{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1},{1,0},{0,1},{1,1}};
+        vector<pair<int,int>> dir={{-1,-1},{-1,0},{0,-1},{1,1},{1,0},{0,1},{1,-1},{-1,1}};
         queue<pair<int,int>> q;
         q.push({0,0});
-        int steps=0;
         grid[0][0]=1;
+        int ans=0;
         while(!q.empty())
         {
+            ans++;
             int size=q.size();
-            steps++;
             for(int i=0;i<size;i++)
             {
                 auto cur=q.front();
                 q.pop();
-                int x=cur.first;
-                int y=cur.second;
-                if(x==m-1 and y==n-1) return steps;
+                int x=cur.first,y=cur.second;
+                if(x==m-1 and y==n-1) return ans;
                 for(auto d:dir)
                 {
-                    int new_x=x+d.first;
-                    int new_y=y+d.second;
-                    
-                    if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and grid[new_x][new_y]==0)
+                    int nx=x+d.first;
+                    int ny=y+d.second;
+                    if(nx>=0 and nx<m and ny>=0 and ny<n and grid[nx][ny]==0)
                     {
-                        q.push({new_x,new_y});
-                        grid[new_x][new_y]=1;
+                        q.push({nx,ny});
+                        grid[nx][ny]=1;
                     }
                 }
             }
