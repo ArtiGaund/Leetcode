3fcc01820b0363217ad347c6f061e08c5a7f10d7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-26 17:26:17 +0530

    Time: 41 ms (37.31%), Space: 14.1 MB (33.45%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,52 +1,31 @@
-class UnionFind
-{
-    public: vector<int> parent;
-            vector<int> rank;
-            int count;
-        UnionFind(int n):parent(n),rank(n),count(n)
-        {
-            for(int i=0;i<n;i++)
-            {
-                parent[i]=i;
-                rank[i]=1;
-            }
-        }
-        int find(int x)
-        {
-            if(parent[x]!=x) parent[x]=find(parent[x]);
-            return parent[x];
-        }
-       void unionset(int a,int b)
-    {
-        int x=find(a);
-        int y=find(b);
-        if(x!=y)
-        {
-            if(rank[x]>rank[y]) parent[y]=x;
-            else if(rank[x]<rank[y]) parent[x]=y;
-            else
-            {
-                parent[y]=x;
-                rank[x]++;
-            }
-            count--;
-        }
-    }
-    int getCount()
-    {
-        return count;
-    }
-};
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        if(n==0) return 0;
-        UnionFind uf(n);
+        vector<int> vis(n,0);
+        stack<int> st;
+        int count=0;
         for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
-                if(isConnected[i][j]==1)
-                    uf.unionset(i,j);
-        return uf.getCount();
+        {
+            if(!vis[i])
+            {
+                st.push(i);
+                count++;
+                while(!st.empty())
+                {
+                    int cur=st.top();
+                    st.pop();
+                    vis[cur]=1;
+                    for(int j=0;j<isConnected[cur].size();j++)
+                    {
+                        if(!vis[j] and isConnected[cur][j]==1)
+                        {
+                            st.push(j);
+                        }
+                    }
+                }
+            }
+        }
+        return count;
     }
 };
