0a2f4142a08f7f1d662376dbeec363a022ef7595
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-28 11:24:11 +0530

    Time: 285 ms (18.18%), Space: 43.3 MB (43.52%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-    vector<pair<int,int>> adj[101];
+    void dfs(vector<int> &signal,vector<pair<int,int>> adj[],int node,int time,int n)
+    {
+        if(time>=signal[node]) return;
+        signal[node]=time;
+        for(auto e:adj[node])
+        {
+            int t=e.first;
+            int neighbor=e.second;
+            dfs(signal,adj,neighbor,time+t,n);
+        }
+    }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<pair<int,int>> adj[n+1];
         for(auto time:times)
         {
-            int source=time[0];
-            int dest=time[1];
-            int travelTime=time[2];
-            adj[source].push_back({travelTime,dest});
+            adj[time[0]].push_back({time[2],time[1]});
         }
-        vector<int> signalRecievedAt(n+1,INT_MAX);
-        dijkstra(signalRecievedAt,k,n);
+        for(int i=1;i<=n;i++)
+            sort(adj[i].begin(),adj[i].end());
+        vector<int> signal(n+1,INT_MAX);
+        dfs(signal,adj,k,0,n);
         int ans=INT_MIN;
-        for(int i=1;i<=n;i++) ans=max(ans,signalRecievedAt[i]);
+        for(int node=1;node<=n;node++)
+            ans=max(ans,signal[node]);
         return ans==INT_MAX?-1:ans;
-    }
-    void dijkstra(vector<int> &signalRecievedAt,int source,int n)
-    {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,source});
-        signalRecievedAt[source]=0;
-        while(!pq.empty())
-        {
-            int curNodeTime=pq.top().first;
-            int curNode=pq.top().second;
-            pq.pop();
-            if(curNodeTime>signalRecievedAt[curNode]) continue;
-            for(auto edge:adj[curNode])
-            {
-                int time=edge.first;
-                int neighbor=edge.second;
-                if(signalRecievedAt[neighbor]>curNodeTime+time)
-                {
-                    signalRecievedAt[neighbor]=curNodeTime+time;
-                    pq.push({signalRecievedAt[neighbor],neighbor});
-                }
-            }
-        }
     }
 };
