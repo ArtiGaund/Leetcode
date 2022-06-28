f8510463b36e77c76f9351af55dac5365fb142ef
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-28 11:54:25 +0530

    Time: 201 ms (40.33%), Space: 43.4 MB (39.49%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(vector<int> &signal,vector<pair<int,int>> adj[],int node,int time,int n)
+    void dfs(vector<int> &signal,vector<pair<int,int>> adj[],int node,int time)
     {
         if(time>=signal[node]) return;
         signal[node]=time;
@@ -8,22 +8,20 @@ public:
         {
             int t=e.first;
             int neighbor=e.second;
-            dfs(signal,adj,neighbor,time+t,n);
+            dfs(signal,adj,neighbor,time+t);
         }
     }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<pair<int,int>> adj[n+1];
-        for(auto time:times)
+        for(auto t:times)
         {
-            adj[time[0]].push_back({time[2],time[1]});
+            adj[t[0]].push_back({t[2],t[1]});
         }
-        for(int i=1;i<=n;i++)
-            sort(adj[i].begin(),adj[i].end());
+        for(int i=1;i<=n;i++) sort(adj[i].begin(),adj[i].end());
         vector<int> signal(n+1,INT_MAX);
-        dfs(signal,adj,k,0,n);
+        dfs(signal,adj,k,0);
         int ans=INT_MIN;
-        for(int node=1;node<=n;node++)
-            ans=max(ans,signal[node]);
+        for(int node=1;node<=n;node++) ans=max(ans,signal[node]);
         return ans==INT_MAX?-1:ans;
     }
 };
