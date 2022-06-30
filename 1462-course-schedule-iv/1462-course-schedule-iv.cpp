619baeebf11d73e7dcbe17041e87df1803e37796
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-30 15:25:35 +0530

    Time: 237 ms (89.75%), Space: 69.1 MB (31.14%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
-    //dFS
+    void dfs(int src,vector<int> &vis,vector<int> adj[],vector<vector<bool>> &ispre,int node)
+    {
+        vis[node]=true;
+        ispre[src][node]=true;
+        for(auto it:adj[node])
+            if(!vis[it])
+                dfs(src,vis,adj,ispre,it);
+    }
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& queries) {
         vector<int> adj[n];
-        vector<vector<bool>> ispre(n,vector<bool>(n,false));
+        vector<vector<bool>> ispre(n,vector<bool>(n,0));
         for(auto p:pre)
         {
             adj[p[0]].push_back(p[1]);
@@ -11,22 +18,12 @@ public:
         }
         for(int i=0;i<n;i++)
         {
-            vector<bool> vis(n,false);
-            dfs(i,adj,vis,ispre,i);
+            vector<int> vis(n,0);
+            dfs(i,vis,adj,ispre,i);
         }
         vector<bool> res;
         for(auto q:queries)
             res.push_back(ispre[q[0]][q[1]]);
         return res;
-    }
-    void dfs(int src,vector<int> adj[],vector<bool> &vis,vector<vector<bool>> &ispre,int node)
-    {
-        vis[node]=true;
-        ispre[src][node]=true;
-        for(auto it:adj[node])
-        {
-            if(!vis[it])
-                dfs(src,adj,vis,ispre,it);
-        }
     }
 };
