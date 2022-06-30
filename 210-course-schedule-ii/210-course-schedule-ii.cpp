35aeb6722fab803f9c871de45832c35244612ddf
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-30 15:14:26 +0530

    Time: 29 ms (64.41%), Space: 13.2 MB (95.20%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+    // bfs
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
         for(int i=0;i<pre.size();i++)
             adj[pre[i][1]].push_back(pre[i][0]);
         vector<int> indegree(n,0);
         for(int i=0;i<n;i++)
-            for(auto it:adj[i])
-                indegree[it]++;
+            for(auto it:adj[i]) indegree[it]++;
         queue<int> q;
         for(int i=0;i<n;i++)
             if(indegree[i]==0) q.push(i);
@@ -15,13 +15,13 @@ public:
         int i=0;
         while(!q.empty())
         {
-            int node=q.front();
+            auto cur=q.front();
             q.pop();
-            res[i++]=node;
-            for(auto it:adj[node])
+            res[i++]=cur;
+            for(auto it:adj[cur])
             {
-                if(--indegree[it]==0)
-                    q.push(it);
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
         if(i==n) return res;
