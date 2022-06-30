4f7c3f9a0ca77ba97aa216c185550d0c490e04df
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-06-30 15:10:11 +0530

    Time: 30 ms (61.53%), Space: 13.5 MB (72.07%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    // bfs
+    //dfs
+    bool isCycle(int node,vector<int> adj[],vector<int> &vis)
+    {
+        if(vis[node]==1) return true;
+        if(vis[node]==0)
+        {
+            vis[node]=1;
+            for(auto it:adj[node])
+            {
+                if(isCycle(it,adj,vis))
+                    return true;
+            }
+        }
+        vis[node]=2;
+        return false;
+    }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
         for(int i=0;i<pre.size();i++)
             adj[pre[i][1]].push_back(pre[i][0]);
-        vector<int> indegree(n,0);
+        vector<int> vis(n,0);
         for(int i=0;i<n;i++)
-            for(auto it:adj[i]) indegree[it]++;
-        queue<int> q;
-        for(int i=0;i<n;i++)
-            if(indegree[i]==0) q.push(i);
-        while(!q.empty())
         {
-            auto cur=q.front();
-            q.pop();
-            n--;
-            for(auto it:adj[cur])
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.push(it);
-            }
+            if(isCycle(i,adj,vis)) return false;
         }
-        return (n==0);
+        return true;
     }
 };
