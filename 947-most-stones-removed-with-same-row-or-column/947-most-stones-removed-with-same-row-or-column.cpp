8d46962cb15202e5ff32ab05dd97853b649ee279
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-01 16:31:09 +0530

    Time: 329 ms (35.36%), Space: 14.4 MB (73.50%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,27 +1,54 @@
 class Solution {
 public:
-    void dfs(int node,vector<int> &vis,vector<vector<int>> &stones)
+    struct subset
     {
-        vis[node]=1;
-        int x=stones[node][0],y=stones[node][1];
-        for(int i=0;i<stones.size();i++)
+        int parent,rank;
+    };
+    int find(vector<subset> &Parent,int x)
+    {
+        if(Parent[x].parent==-1) return x;
+        return find(Parent,Parent[x].parent);
+    }
+    void unionset(vector<subset> &Parent,int a,int b)
+    {
+        int x=find(Parent,a);
+        int y=find(Parent,b);
+        if(x!=y)
         {
-            if((stones[i][0]==x or stones[i][1]==y) and !vis[i])
-                dfs(i,vis,stones);
+            if(Parent[x].rank>Parent[y].rank) Parent[y].parent=x;
+            else if(Parent[x].rank<Parent[y].rank) Parent[x].parent=y;
+            else
+            {
+                Parent[y].parent=x;
+                Parent[x].rank++;
+            }
         }
+        return;
+    }
+    bool isSameRow(vector<vector<int>> &stones,int i,int j)
+    {
+        return stones[i][0]==stones[j][0];
+    }
+    bool isSameCol(vector<vector<int>> &stones,int i,int j)
+    {
+        return stones[i][1]==stones[j][1];
     }
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        int group=0;
-        vector<int> vis(n,0);
+        vector<subset> Parent(n,{-1,0});
         for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            for(int j=0;j<i;j++)
             {
-                group++;
-                dfs(i,vis,stones);
+                if(isSameRow(stones,i,j) or isSameCol(stones,i,j))
+                {
+                    unionset(Parent,i,j);
+                }
             }
         }
+        int group=0;
+        for(int i=0;i<n;i++)
+            if(Parent[i].parent==-1) group++;
         return n-group;
     }
 };
