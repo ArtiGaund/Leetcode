083f528daa9a245c45c9cc51803cfa4fcba125b2
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-01 12:15:43 +0530

    Time: 31 ms (68.39%), Space: 13.7 MB (87.80%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,31 +1,59 @@
+class UnionFind
+{
+    public:
+    vector<int> parent,rank;
+    int count;
+    UnionFind(int n):parent(n),rank(n),count(n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=1;
+        }
+    }
+    int find(int x)
+    {
+        if(x!=parent[x])
+            x=find(parent[x]);
+        return x;
+    }
+    void unionset(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
+        {
+            if(rank[x]>rank[y]) parent[y]=x;
+            else if(rank[x]<rank[y]) parent[x]=y;
+            else
+            {
+                parent[y]=x;
+                rank[x]++;
+            }
+            count--;
+        }
+    }
+    int getCount()
+    {
+        return count;
+    }
+};
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<int> vis(n,0);
-        stack<int> st;
-        int count=0;
+        UnionFind uf(n);
         for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            for(int j=0;j<isConnected[i].size();j++)
             {
-                st.push(i);
-                count++;
-                while(!st.empty())
+                if(isConnected[i][j]==1)
                 {
-                    int cur=st.top();
-                    st.pop();
-                    vis[cur]=1;
-                    for(int j=0;j<isConnected[cur].size();j++)
-                    {
-                        if(!vis[j] and isConnected[cur][j]==1)
-                        {
-                            st.push(j);
-                        }
-                    }
+                    if(uf.find(i)!=uf.find(j))
+                        uf.unionset(i,j);
                 }
             }
         }
-        return count;
+        return uf.getCount();
     }
 };
