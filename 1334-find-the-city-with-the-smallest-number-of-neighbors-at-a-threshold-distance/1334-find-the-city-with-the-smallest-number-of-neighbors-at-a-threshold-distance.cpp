76cec695dd954f10e34270b124628c4148ef56f8
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-01 10:53:31 +0530

    Time: 173 ms (29.16%), Space: 12.5 MB (61.88%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int d) {
+        vector<vector<int>> dist(n+1,vector<int>(n+1,INT_MAX));
+        for(int i=0;i<n;i++)
+            dist[i][i]=0;
+        for(auto e:edges)
+        {
+            dist[e[0]][e[1]]=e[2];
+            dist[e[1]][e[0]]=e[2];
+        }
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(dist[i][k]!=INT_MAX)
+                {
+                    for(int j=0;j<n;j++)
+                    {
+                        if(i!=j and dist[k][j]!=INT_MAX)
+                            dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                    }
+                }
+            }
+        }
+        int res=0;
+        int m=n;
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            for(int j=0;j<n;j++)
+                if(dist[i][j]<=d and dist[i][j]!=INT_MAX)
+                    count++;
+            if(count<=m)
+            {
+                m=count;
+                res=i;
+            }
+        }
+        return res;
+    }
+};
