f738ef3be9871dbeff24f182852c4f03daf7510b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-01 18:18:23 +0530

    Time: 12 ms (37.47%), Space: 11.4 MB (74.23%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,49 +1,52 @@
+class UnionFind
+{
+    public:
+    vector<int> parent,rank;
+    UnionFind(int n):parent(n),rank(n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=1;
+        }
+    }
+    int find(int x)
+    {
+        if(x!=parent[x])
+            x=find(parent[x]);
+        return x;
+    }
+    void unionset(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
+        {
+            if(rank[x]>rank[y]) parent[y]=x;
+            else if(rank[x]<rank[y]) parent[x]=y;
+            else
+            {
+                parent[y]=x;
+                rank[x]++;
+            }
+            // return true;
+        }
+        // return false;
+    }
+};
 class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
-        vector<int> graph[26];
+        UnionFind uf(26);
         for(string &s:equations)
         {
             if(s[1]=='=')
-            {
-                int x=s[0]-'a';
-                int y=s[3]-'a';
-                graph[x].push_back(y);
-                graph[y].push_back(x);
-            }
-        }
-        vector<int> color(26,0);
-        int t=0;
-        for(int start=0;start<26;start++)
-        {
-            if(color[start]==0)
-            {
-                t++;
-                stack<int> st;
-                st.push(start);
-                while(!st.empty())
-                {
-                    int node=st.top();
-                    st.pop();
-                    for(int nei:graph[node])
-                    {
-                        if(color[nei]==0)
-                        {
-                            color[nei]=t;
-                            st.push(nei);
-                        }
-                    }
-                }
-            }
+                uf.unionset(s[0]-'a',s[3]-'a');
         }
         for(string &s:equations)
         {
             if(s[1]=='!')
-            {
-                int x=s[0]-'a';
-                int y=s[3]-'a';
-                if(x==y or (color[x]!=0 and color[x]==color[y])) return false;
-            }
+                if(uf.find(s[0]-'a')==uf.find(s[3]-'a')) return false;
         }
         return true;
     }
