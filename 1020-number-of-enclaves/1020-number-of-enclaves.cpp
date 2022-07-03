4d57a7dc531c45eeed529579ed71cf8f93df4bfd
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-03 10:47:54 +0530

    Time: 60 ms (95.85%), Space: 21.7 MB (56.06%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int cnt;
+    void dfs(vector<vector<int>> &grid,int i,int j,int m,int n)
+    {
+        if(i<0 or i>=m or j<0 or j>=n or grid[i][j]==0) return;
+        grid[i][j]=0;
+        cnt++;
+        dfs(grid,i-1,j,m,n);
+        dfs(grid,i+1,j,m,n);
+        dfs(grid,i,j-1,m,n);
+        dfs(grid,i,j+1,m,n);
+    }
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
@@ -15,24 +25,14 @@ public:
             if(grid[m-1][j]==1) dfs(grid,m-1,j,m,n);
         }
         cnt=0;
-        for(int i=0;i<m;i++)
+        for(int i=1;i<m-1;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=1;j<n-1;j++)
             {
                 if(grid[i][j]==1)
                     dfs(grid,i,j,m,n);
             }
         }
         return cnt;
-    }
-    void dfs(vector<vector<int>> &grid,int i,int j,int m,int n)
-    {
-        if(i<0 or i>=m or j<0 or j>=n or grid[i][j]==0) return;
-        cnt++;
-        grid[i][j]=0;
-        dfs(grid,i-1,j,m,n);
-        dfs(grid,i+1,j,m,n);
-        dfs(grid,i,j-1,m,n);
-        dfs(grid,i,j+1,m,n);
     }
 };
