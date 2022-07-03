b57190fbfc6bf13972bbd4f28513beea74ab37ed
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-03 11:39:13 +0530

    Time: 29 ms (7.91%), Space: 11.1 MB (10.61%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size();
+        vector<int> graph[n+1];
+        for(int i=0;i<rooms.size();i++)
+        {
+            for(int j=0;j<rooms[i].size();j++)
+                graph[i].push_back(rooms[i][j]);
+        }
+        vector<int> vis(n,0);
+        vis[0]=1;
+        queue<int> q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int cur=q.front();
+            q.pop();
+            if(!graph[cur].size()) continue;
+            for(auto it:graph[cur])
+            {
+                if(!vis[it])
+                {
+                    q.push(it);
+                    vis[it]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+            if(!vis[i]) return false;
+        return true;
+    }
+};
