4ea45098a2d869cc233eeb4daaeb61d3b45f5cf8
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-03 15:35:33 +0530

    Time: 28 ms (97.06%), Space: 14.1 MB (54.61%) - LeetHub

--- 690-employee-importance/690-employee-importance.cpp
+++ 690-employee-importance/690-employee-importance.cpp
@@ -10,16 +10,16 @@ public:
 
 class Solution {
 public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int,Employee*> mp;
+        for(Employee* e:employees) mp[e->id]=e;
+        return dfs(id,mp);
+    }
     int dfs(int id,unordered_map<int,Employee*> &mp)
     {
         Employee *e=mp[id];
         int ans=e->importance;
         for(int subid:e->subordinates) ans+=dfs(subid,mp);
         return ans;
-    }
-    int getImportance(vector<Employee*> employees, int id) {
-        unordered_map<int,Employee*> mp;
-        for(Employee *e:employees) mp[e->id]=e;
-        return dfs(id,mp);
     }
 };
