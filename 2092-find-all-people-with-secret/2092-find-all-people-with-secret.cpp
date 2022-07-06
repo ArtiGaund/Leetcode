9d3da272af01be6c0ae9b002a41c94eaf26ab8a7
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-06 18:49:39 +0530

    Time: 1657 ms (14.00%), Space: 194.8 MB (28.23%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,31 +1,75 @@
+class UnionFind
+{
+    public:
+    vector<int> parent,rank;
+    UnionFind(int n):parent(n),rank(n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+    int find(int x)
+    {
+        if(x!=parent[x])
+            x=find(parent[x]);
+        return x;
+    }
+    void unionset(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
+        {
+            if(rank[x]>rank[y]) parent[y]=x;
+            else if(rank[x]<rank[y]) parent[x]=y;
+            else
+            {
+                parent[y]=x;
+                rank[x]++;
+            }
+        }
+    }
+    bool connected(int a,int b)
+    {
+        return find(a)==find(b);
+    }
+    void reset(int a)
+    {
+        parent[a]=a;
+    }
+};
 class Solution {
 public:
+    static bool comp(vector<int> &a,vector<int> &b)
+    {
+        return a[2]<b[2];
+    }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        vector<pair<int,int>> adj[n];
-        for(int i=0;i<meetings.size();i++)
+        sort(meetings.begin(),meetings.end(),comp);
+        UnionFind uf(n);
+        uf.unionset(0,firstPerson);
+        for(int i=0;i<meetings.size();)
         {
-            adj[meetings[i][0]].push_back({meetings[i][1],meetings[i][2]});
-            adj[meetings[i][1]].push_back({meetings[i][0],meetings[i][2]});
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0}); //time, person
-        pq.push({0,firstPerson});
-        vector<bool> knows(n,0);
-        while(!pq.empty())
-        {
-            auto [curTime,curPerson]=pq.top();
-            pq.pop();
-            if(knows[curPerson]) continue;
-            knows[curPerson]=1;
-            for(auto it:adj[curPerson])
+            unordered_set<int> s;
+            int time=meetings[i][2];
+            while(i<meetings.size() and time==meetings[i][2])
             {
-                if(!knows[it.first] and curTime<=it.second)
-                    pq.push({it.second,it.first});
+                s.insert(meetings[i][0]);
+                s.insert(meetings[i][1]);
+                uf.unionset(meetings[i][0],meetings[i][1]);
+                i++;
+            }
+            for(int person:s)
+            {
+                if(uf.connected(0,person)==false)
+                    uf.reset(person);
             }
         }
         vector<int> res;
         for(int i=0;i<n;i++)
-            if(knows[i]) res.push_back(i);
+            if(uf.connected(0,i)) res.push_back(i);
         return res;
     }
 };
