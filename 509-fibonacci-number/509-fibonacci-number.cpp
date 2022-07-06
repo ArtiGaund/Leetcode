7a359e9f97b1f57456cc5ce9b64676f7303c5216
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-07-06 14:13:36 +0530

    Time: 13 ms (36.95%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 or n==1) return n;
-        int a=0,b=1;
-        for(int i=2;i<=n;i++)
-        {
-            int c=a+b;
-            a=b;
-            b=c;
-        }
-        return b;
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
