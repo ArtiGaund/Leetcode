4b7f0d7d3a40463368f4ee81dcdecad2ddd81d82
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-08-04 12:05:35 +0530

    Time: 199 ms (97.88%), Space: 70.7 MB (90.45%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
-        if(arr.size()<=1) return 0;
-        unordered_map<int,vector<int>> mp;
-        int n = arr.size();
-        vector<bool> vis(n);
+        int n=arr.size();
+        if(n<=1) return 0;
+        unordered_map<int,vector<int>> graph;
+        for(int i=0;i<n;i++) graph[arr[i]].push_back(i);
         queue<int> q;
+        vector<bool> vis(n);
         q.push(0);
-        for(int i=0;i<n;i++) mp[arr[i]].push_back(i);
-        int k = 1;
-        vis[0]=true;
-        while(!q.empty()){
-            int sz=q.size();
-            while(sz--){
-                int node = q.front();
+        vis[0]=1;
+        int step=1;
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size--)
+            {
+                int node=q.front();
                 q.pop();
-
-                if(node+1<n and !vis[node+1]){
-                    if(node+1==n-1) return k;
+               if(node+1<n and !vis[node+1]){
+                    if(node+1==n-1) return step;
                     q.push(node+1);
                     vis[node+1]=true;
                 }
-
                 if(node-1>=0 and !vis[node-1]){
-                    if(node-1==n-1) return k;
+                    if(node-1==n-1) return step;
                     q.push(node-1);
                     vis[node-1]=true;
                 }
 
-                for(auto i:mp[arr[node]]){
+                for(auto i:graph[arr[node]])
+                {
                     if(i==node or vis[i]) continue;
-                    if(i==n-1) return k;
+                    if(i==n-1) return step;
                     q.push(i);
-                    vis[i]=true;
+                    vis[i]=1;
                 }
-                
-                mp[arr[node]].clear();
+                graph[arr[node]].clear();
             }
-            k++;
+            step++;
         }
-        
         return -1;
     }
 };
