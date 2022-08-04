c98a865f5ac5a4ca4a9f777563836cd9051f66f9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-08-04 11:37:43 +0530

    Time: 393 ms (47.13%), Space: 70.6 MB (90.45%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
-        int n=arr.size();
-        if(n<=1) return 0;
-        map<int,vector<int>> mp;
-        for(int i=0;i<n;i++) mp[arr[i]].push_back(i);
-       set<int> vis;
-        vis.insert(0);
+        if(arr.size()<=1) return 0;
+        unordered_map<int,vector<int>> mp;
+        int n = arr.size();
+        vector<bool> vis(n);
         queue<int> q;
         q.push(0);
-        for(int level=0;!q.empty();level++)
-        {
-            int size=q.size();
-            while(size--)
-            {
-                int cur=q.front();
+        for(int i=0;i<n;i++) mp[arr[i]].push_back(i);
+        int k = 1;
+        vis[0]=true;
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                int node = q.front();
                 q.pop();
-                if(cur==n-1) return level;
-                int i=cur;
-                if(i-1>=0 and !vis.count(i-1))
-                {
-                    q.push(i-1);
-                    vis.insert(i-1);
+
+                if(node+1<n and !vis[node+1]){
+                    if(node+1==n-1) return k;
+                    q.push(node+1);
+                    vis[node+1]=true;
                 }
-                if(i+1<n and !vis.count(i+1))
-                {
-                    q.push(i+1);
-                    vis.insert(i+1);
+
+                if(node-1>=0 and !vis[node-1]){
+                    if(node-1==n-1) return k;
+                    q.push(node-1);
+                    vis[node-1]=true;
                 }
-                for(int j=0;j<mp[arr[cur]].size();j++)
-                {
-                    if(!vis.count(mp[arr[cur]][j]))
-                    {
-                        q.push(mp[arr[cur]][j]);
-                        vis.insert(mp[arr[cur]][j]);
-                    }
+
+                for(auto i:mp[arr[node]]){
+                    if(i==node or vis[i]) continue;
+                    if(i==n-1) return k;
+                    q.push(i);
+                    vis[i]=true;
                 }
-                if(mp.count(arr[cur])) mp.erase(arr[cur]);
+                
+                mp[arr[node]].clear();
             }
+            k++;
         }
-        return -1;
         
+        return -1;
     }
 };
