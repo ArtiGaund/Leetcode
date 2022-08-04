1bd372401a3f8047f44df5d341b81a01b59d3673
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-08-04 10:52:34 +0530

    Time: 145 ms (7.17%), Space: 35.4 MB (37.78%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+       int n=arr.size();
+        queue<int> q;
+        q.push(start);
+        set<int> vis;
+        vis.insert(start);
+        while(!q.empty())
+        {
+            int cur=q.front();
+            q.pop();
+            if(arr[cur]==0) return true;
+            if(cur+arr[cur]<n and !vis.count(cur+arr[cur]))
+            {
+                q.push(cur+arr[cur]);
+                vis.insert(cur+arr[cur]);
+            }
+            if(cur-arr[cur]>=0 and !vis.count(cur-arr[cur]))
+            {
+                q.push(cur-arr[cur]);
+                vis.insert(cur-arr[cur]);
+            }
+        }
+        return false;
+    }
+};
