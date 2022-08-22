1f2a23e01e863c91130b70e8de5c0a990231c5a1
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-08-22 19:06:19 +0530

    Time: 28 ms (32.37%), Space: 7.2 MB (90.64%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -3,8 +3,7 @@ public:
     bool checkInclusion(string s1, string s2) {
         unordered_map<char,int> mp;
         for(char c:s1) mp[c]++;
-        int start=0,end=0;
-        int count=mp.size();
+        int start=0,end=0,count=mp.size();
         while(end<s2.size())
         {
             char ch=s2[end];
