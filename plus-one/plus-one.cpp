cdacf513b63038372f78216e09dcc7f44331f732
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-09-08 15:36:19 +0530

    Time: 6 ms, Memory: 8.7 MB - LeetHub

---
+++ plus-one/plus-one.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int c=0;
+        for(int i=digits.size()-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]+=1;
+                c++;
+                break;
+            }
+            else
+                digits[i]=0;
+        }
+        if(digits[0]==0&&c==0)
+            digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
