c409d9d36202d6caebcd22e1e5d7aa0b828490b4
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-09-16 17:33:09 +0530

    Time: 158 ms (34.80%), Space: 13.1 MB (82.35%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-3;i++)
+        {
+            if(i>0 and nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++)
+            {
+                if(j>i+1 and nums[j]==nums[j-1]) continue;
+                searchPair(nums,target,i,j);
+            }
+        }
+        return res;
+    }
+    void searchPair(vector<int> &nums,int target,int first,int second)
+    {
+        int left=second+1;
+        int right=nums.size()-1;
+        while(left<right)
+        {
+            long long int cursum=(long)nums[first]+(long)nums[second]+(long)nums[left]+(long)nums[right];
+            if(cursum==target)
+            {
+                res.push_back({nums[first],nums[second],nums[left],nums[right]});
+                left++;
+                right--;
+                while(left<right and nums[left]==nums[left-1])
+                    left++;
+                while(left<right and nums[right]==nums[right+1])
+                    right--;
+            }
+            else if(cursum<target)
+                left++;
+            else right--;
+        }
+    }
+};
