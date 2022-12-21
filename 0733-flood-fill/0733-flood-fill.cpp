145cef5d49fc2c0b6b86448b2711dc99d59df7f2
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-21 10:18:10 +0530

    Time: 18 ms (50.68%), Space: 14.5 MB (12.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m=image.size(),n=image[0].size();
         if(image[sr][sc]==color) return image;
-        int m=image.size();
-        int n=image[0].size();
         int old=image[sr][sc];
         image[sr][sc]=color;
         queue<pair<int,int>> q;
@@ -17,12 +16,11 @@ public:
         {
             auto cur=q.front();
             q.pop();
-            int r=cur.first;
-            int c=cur.second;
-            if(r>=0 and r<m and c>=0 and c<n and !vis[r][c] and image[r][c]==old)
+            int r=cur.first,c=cur.second;
+            if(r>=0 and r<m and c>=0 and c<n and image[r][c]==old and !vis[r][c])
             {
-                image[r][c]=color;
                 vis[r][c]=1;
+                image[r][c]=color;
                 q.push({r-1,c});
                 q.push({r+1,c});
                 q.push({r,c-1});
