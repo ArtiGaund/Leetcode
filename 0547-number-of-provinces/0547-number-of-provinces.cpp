07951611c5d7401c6f72adaa6c5027193f925598
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-21 09:36:43 +0530

    Time: 50 ms (55.21%), Space: 15.1 MB (25.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,59 +1,38 @@
-class UnionFind
-{
-    public:
-    vector<int> parent,rank;
-    int count;
-    UnionFind(int n):parent(n),rank(n),count(n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            parent[i]=i;
-            rank[i]=1;
-        }
-    }
-    int find(int x)
-    {
-        if(x!=parent[x])
-            x=find(parent[x]);
-        return x;
-    }
-    void unionset(int a,int b)
-    {
-        int x=find(a);
-        int y=find(b);
-        if(x!=y)
-        {
-            if(rank[x]>rank[y]) parent[y]=x;
-            else if(rank[x]<rank[y]) parent[x]=y;
-            else
-            {
-                parent[y]=x;
-                rank[x]++;
-            }
-            count--;
-        }
-    }
-    int getCount()
-    {
-        return count;
-    }
-};
 class Solution {
 public:
+    void dfs(int node,vector<int> graph[],vector<int> &vis)
+    {
+        vis[node]=1;
+        for(auto it:graph[node])
+        {
+            if(!vis[it])
+                dfs(it,graph,vis);
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        UnionFind uf(n);
+        int n=isConnected.size(),m=isConnected[0].size();
+        vector<int> graph[n];
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<isConnected[i].size();j++)
+            for(int j=0;j<m;j++)
             {
-                if(isConnected[i][j]==1)
+                if(isConnected[i][j]==1 and i!=j)
                 {
-                    if(uf.find(i)!=uf.find(j))
-                        uf.unionset(i,j);
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
                 }
             }
         }
-        return uf.getCount();
+        vector<int> vis(n,0);
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                count++;
+                dfs(i,graph,vis);
+            }
+        }
+        return count;
     }
 };
