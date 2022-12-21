09435092aacd601bc3459ea5ed6cddc3859d7429
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-21 09:44:21 +0530

    Time: 36 ms (69.78%), Space: 13.7 MB (72.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,48 +1,57 @@
-class Solution {
-public:
-    void bfs(int node,vector<int> graph[],vector<int> &vis)
+class UnionFind
+{
+    public:
+    vector<int> parent,rank;
+    int count;
+    UnionFind(int n):parent(n),rank(n),count(n)
     {
-        vis[node]=1;
-        queue<int> q;
-        q.push(node);
-        while(!q.empty())
+        for(int i=0;i<n;i++)
         {
-            auto cur=q.front();
-            q.pop();
-            for(auto it:graph[cur])
-            {
-                if(!vis[it])
-                {
-                    q.push(it);
-                    vis[it]=1;
-                }
-            }
+            parent[i]=i;
+            rank[i]=1;
         }
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size(),m=isConnected[0].size();
-        vector<int> graph[n];
-        for(int i=0;i<n;i++)
+    int find(int x)
+    {
+        if(x!=parent[x])
+            x=find(parent[x]);
+        return x;
+    }
+    void unionset(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
         {
-            for(int j=0;j<m;j++)
+            if(rank[x]>rank[y]) parent[y]=x;
+            else if(rank[x]<rank[y]) parent[x]=y;
+            else
             {
-                if(isConnected[i][j]==1 and i!=j)
-                {
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
+                parent[y]=x;
+                rank[x]++;
             }
+            count--;
         }
-        vector<int> vis(n,0);
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                count++;
-                bfs(i,graph,vis);
-            }
-        }
+    }
+    int getCount()
+    {
         return count;
+    }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        UnionFind uf(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<isConnected[i].size();j++)
+            {
+                if(isConnected[i][j]==1)
+                if(uf.find(i)!=uf.find(j))
+                    uf.unionset(i,j);
+            }
+        }
+        return uf.getCount();
     }
 };
