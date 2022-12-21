b9dc480d48c444f55903d3f7e412947470ecff12
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-21 10:21:58 +0530

    Time: 18 ms (50.68%), Space: 13.8 MB (91.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
+    vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
+    int m,n;
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m=image.size(),n=image[0].size();
-        if(image[sr][sc]==color) return image;
         int old=image[sr][sc];
-        image[sr][sc]=color;
-        queue<pair<int,int>> q;
-        q.push({sr-1,sc});
-        q.push({sr+1,sc});
-        q.push({sr,sc-1});
-        q.push({sr,sc+1});
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        vis[sr][sc]=1;
-        while(!q.empty())
+        m=image.size();
+        n=image[0].size();
+        vector<vector<int>> res=image;
+        dfs(image,res,sr,sc,color,old);
+        return res;
+    }
+    void dfs(vector<vector<int>> &image,vector<vector<int>> &res,int r,int c,int color,int old)
+    {
+        res[r][c]=color;
+        for(auto d:dir)
         {
-            auto cur=q.front();
-            q.pop();
-            int r=cur.first,c=cur.second;
-            if(r>=0 and r<m and c>=0 and c<n and image[r][c]==old and !vis[r][c])
-            {
-                vis[r][c]=1;
-                image[r][c]=color;
-                q.push({r-1,c});
-                q.push({r+1,c});
-                q.push({r,c-1});
-                q.push({r,c+1});
-            }
+            int row=r+d.first;
+            int col=c+d.second;
+            if(row>=0 and row<m and col>=0 and col<n and image[row][col]==old and res[row][col]!=color)
+                dfs(image,res,row,col,color,old);
         }
-        return image;
     }
 };
