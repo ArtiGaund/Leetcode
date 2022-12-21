33a082e597bd8cf5a65dca8d4dfbbace66107280
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-21 10:07:57 +0530

    Time: 93 ms (42.92%), Space: 15.8 MB (30.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,79 @@
-class Solution {
-public:
-    int n,m;
-    void dfs(vector<vector<char>> &grid,int row,int col)
+class UnionFind
+{
+    public:
+    vector<int> parent,rank;
+    int count;
+    UnionFind(vector<vector<char>> &grid)
     {
-        if(row<0 or row>=n or col<0 or col>=m or grid[row][col]!='1') return;
-        grid[row][col]='*';
-        dfs(grid,row-1,col);
-        dfs(grid,row+1,col);
-        dfs(grid,row,col-1);
-        dfs(grid,row,col+1);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
-        int count=0;
-        for(int i=0;i<n;i++)
+        count=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]=='1')
                 {
+                    parent.push_back(i*n+j);
                     count++;
-                    dfs(grid,i,j);
+                }
+                else parent.push_back(-1);
+                rank.push_back(0);
+            }
+        }
+    }
+    int find(int x)
+    {
+        if(x!=parent[x])
+            x=find(parent[x]);
+        return x;
+    }
+    void unionset(int a,int b)
+    {
+        int x=find(a);
+        int y=find(b);
+        if(x!=y)
+        {
+            if(rank[x]>rank[y]) parent[y]=x;
+            else if(rank[x]<rank[y]) parent[x]=y;
+            else
+            {
+                parent[y]=x;
+                rank[x]++;
+            }
+            count--;
+        }
+    }
+    int getCount()
+    {
+        return count;
+    }
+};
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        if(m==0) return 0;
+        UnionFind uf(grid);
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    grid[i][j]=='0';
+                    for(auto d:dir)
+                    {
+                        int r=i+d.first;
+                        int c=j+d.second;
+                        if(r>=0 and r<m and c>=0 and c<n and grid[r][c]=='1')
+                            uf.unionset(i*n+j,r*n+c);
+                    }
                 }
             }
         }
-        return count;
+        return uf.getCount();
     }
 };
