c0fc40ef183a451f5dc35dd1e939d74381a5298e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-22 10:49:43 +0530

    Time: 83 ms (83.95%), Space: 27.7 MB (73.74%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -4,25 +4,26 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<pair<int,int>> dir={{-1,0},{0,-1},{0,1},{1,0}};
         queue<pair<int,int>> q;
+        //first and last col
         for(int i=0;i<n;i++)
         {
-            if(!vis[i][0] and grid[i][0]==1) 
+            if(!vis[i][0] and grid[i][0]==1)
             {
                 q.push({i,0});
                 vis[i][0]=1;
             }
-            if(!vis[i][m-1] and grid[i][m-1]==1) 
+            if(!vis[i][m-1] and grid[i][m-1]==1)
             {
                 q.push({i,m-1});
                 vis[i][m-1]=1;
             }
         }
+        //first and last row
         for(int j=0;j<m;j++)
         {
             if(!vis[0][j] and grid[0][j]==1)
-            { 
+            {
                 q.push({0,j});
                 vis[0][j]=1;
             }
@@ -32,18 +33,21 @@ public:
                 vis[n-1][j]=1;
             }
         }
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
         while(!q.empty())
         {
-            int row=q.front().first;
-            int col=q.front().second;
+            auto cur=q.front();
             q.pop();
+            int row=cur.first;
+            int col=cur.second;
             for(auto d:dir)
             {
-                int r=row+d.first,c=col+d.second;
+                int r=row+d.first;
+                int c=col+d.second;
                 if(r>=0 and r<n and c>=0 and c<m and !vis[r][c] and grid[r][c]==1)
                 {
-                    vis[r][c]=1;
                     q.push({r,c});
+                    vis[r][c]=1;
                 }
             }
         }
@@ -52,9 +56,10 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] and grid[i][j]==1) count++;
+                if(!vis[i][j] and grid[i][j]==1)
+                    count++;
             }
         }
-        return count;
+        return  count;
     }
 };
