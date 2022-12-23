f4fdb8b2d0b590be9a3cf092a8169b665964e435
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-23 16:36:15 +0530

    Time: 51 ms (38.46%), Space: 13.9 MB (57.45%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> graph[n];
+        for(auto p:pre)
+            graph[p[1]].push_back(p[0]);
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++)
+            for(auto it:graph[i])
+                indegree[it]++;
+        queue<int> q;
+        for(int i=0;i<n;i++)
+            if(indegree[i]==0)
+                q.push(i);
+        vector<int> topo;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:graph[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                    q.push(it);
+            }
+        }
+        if(topo.size()==n)
+            return topo;
+        return {};
+    }
+};
