655a9cd8bd796775ca640cb09fd7ecaa3eaf5cb6
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-28 11:12:21 +0530

    Time: 108 ms (77.16%), Space: 12.6 MB (49.00%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int d) {
+    int findTheCity(int n, vector<vector<int>>& edges, int Threshold) {
         vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
-        for(int i=0;i<n;i++) dist[i][i]=0;
         for(auto e:edges)
         {
             dist[e[0]][e[1]]=e[2];
             dist[e[1]][e[0]]=e[2];
         }
+        for(int i=0;i<n;i++)
+            dist[i][i]=0;
         for(int k=0;k<n;k++)
         {
             for(int i=0;i<n;i++)
@@ -16,26 +17,28 @@ public:
                 {
                     for(int j=0;j<n;j++)
                     {
-                        if(i!=j and dist[k][j]!=INT_MAX)
+                        if(dist[k][j]!=INT_MAX and i!=j)
                             dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
                     }
                 }
             }
         }
-        int res=0;
-        int m=n;
-        for(int i=0;i<n;i++)
+        int cntCity=n;
+        int cityno=-1;
+        for(int city=0;city<n;city++)
         {
-            int count=0;
-            for(int j=0;j<n;j++)
-                if(dist[i][j]<=d and dist[i][j]!=INT_MAX)
-                    count++;
-            if(count<=m)
+            int cnt=0;
+            for(int neigh=0;neigh<n;neigh++)
             {
-                m=count;
-                res=i;
+                if(dist[city][neigh]<=Threshold and dist[city][neigh]!=INT_MAX)
+                    cnt++;
+            }
+            if(cnt<=cntCity)
+            {
+                cntCity=cnt;
+                cityno=city;
             }
         }
-        return res;
+        return cityno;
     }
 };
