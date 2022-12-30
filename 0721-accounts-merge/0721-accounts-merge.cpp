44f52dcefb987b1c2c3bae701574688b544de277
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2022-12-30 10:09:55 +0530

    Time: 88 ms (93.71%), Space: 34.2 MB (72.84%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,71 @@
+class DisjointSet
+{
+    public:
+    vector<int> rank,parent;
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+    }
+    int findParent(int u)
+    {
+        if(u==parent[u]) return u;
+        return parent[u]=findParent(parent[u]);
+    }
+    void unionset(int u,int v)
+    {
+        int ulp_u=findParent(u);
+        int ulp_v=findParent(v);
+        if(rank[ulp_u]<rank[ulp_v])
+            parent[ulp_u]=ulp_v;
+        else if(rank[ulp_v]<rank[ulp_u])
+            parent[ulp_v]=ulp_u;
+        else
+        {
+            parent[ulp_u]=ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+};
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n=accounts.size();
+        DisjointSet ds(n);
+        unordered_map<string,int> mapMailNode;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<accounts[i].size();j++)
+            {
+                string mail=accounts[i][j];
+                if(mapMailNode.find(mail)==mapMailNode.end())
+                    mapMailNode[mail]=i;
+                else
+                {
+                    ds.unionset(i,mapMailNode[mail]);
+                }
+            }
+        }
+        vector<string> mergeMail[n];
+        for(auto it:mapMailNode)
+        {
+            string mail=it.first;
+            int node=ds.findParent(it.second);
+            mergeMail[node].push_back(mail);
+        }
+        vector<vector<string>> res;
+        for(int i=0;i<n;i++)
+        {
+            if(mergeMail[i].size()==0) continue;
+            sort(mergeMail[i].begin(),mergeMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it:mergeMail[i])
+                temp.push_back(it);
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
