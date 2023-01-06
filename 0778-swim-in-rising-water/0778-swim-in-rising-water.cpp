b68718efa7c76167b9815ae3cfb46b308d4efcea
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-01-06 17:33:40 +0530

    Time: 33 ms (62.77%), Space: 8.9 MB (77.19%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    // pair<int,pair<int,int>>
+    int n;
+    vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
+    bool solve(vector<vector<int>> &grid,bool vis[][50],int i,int j,int w_lvl)
+    {
+        if(i<0 or i>=n or j<0 or j>=n or vis[i][j] or grid[i][j]>w_lvl) return false;
+        if(i==n-1 and j==n-1) return true;
+        vis[i][j]=1;
+        for(auto d:dir)
+            if(solve(grid,vis,i+d.first,j+d.second,w_lvl)) return true;
+        return false;
+    }
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<int>> vis(n,vector<int>(n,0));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({grid[0][0],{0,0}});
-        vis[0][0]=1;
-        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
-        while(!pq.empty())
+        n=grid.size();
+        int l=max({2*(n-1),grid[0][0],grid[n-1][n-1]});
+        int r=n*n-1;
+        int mid;
+        bool vis[50][50]={{0}};
+        while(l<=r)
         {
-            auto cur=pq.top();
-            pq.pop();
-            int t=cur.first;
-            int x=cur.second.first;
-            int y=cur.second.second;
-            if(x==n-1 and y==n-1) return t;
-            for(auto d:dir)
-            {
-                int new_x=x+d.first;
-                int new_y=y+d.second;
-                if(new_x>=0 and new_x<n and new_y>=0 and new_y<n and vis[new_x][new_y]==0)
-                {
-                    vis[new_x][new_y]=1;
-                    pq.push({max(grid[new_x][new_y],t),{new_x,new_y}});
-                }
-            }
+            mid=(l+r)/2;
+            if(solve(grid,vis,0,0,mid))
+                r=mid-1;
+            else l=mid+1;
+            memset(vis,false,sizeof(vis));
         }
-        return -1;
+        return l;
     }
 };
