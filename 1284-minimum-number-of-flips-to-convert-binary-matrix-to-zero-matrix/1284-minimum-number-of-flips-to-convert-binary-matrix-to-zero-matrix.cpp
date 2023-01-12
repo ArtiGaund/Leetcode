25cbdc46086d624eef3035b2f7288c73cc34cab4
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-01-12 11:59:54 +0530

    Time: 28 ms (27.64%), Space: 9.9 MB (34.96%) - LeetHub

---
+++ 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void flip(int i,int j,vector<vector<int>> &mat)
+    {
+        int m=mat.size(),n=mat[0].size();
+        mat[i][j]^=1;
+        if(i-1>=0) mat[i-1][j]^=1;
+        if(i+1<m) mat[i+1][j]^=1;
+        if(j-1>=0) mat[i][j-1]^=1;
+        if(j+1<n) mat[i][j+1]^=1;
+    }
+    bool all_zero(vector<vector<int>> &mat)
+    {
+        int sum=0;
+        for(int i=0;i<mat.size();i++)
+            for(int j=0;j<mat[0].size();j++)
+                sum+=mat[i][j];
+        return sum==0;
+    }
+    int minFlips(vector<vector<int>>& mat) {
+        if(all_zero(mat)) return 0;
+        map<vector<vector<int>>,int> mp;
+        queue<vector<vector<int>>> q;
+        set<vector<vector<int>>> vis;
+        q.push(mat);
+        vis.insert(mat);
+        mp[mat]=0;
+        while(!q.empty())
+        {
+            auto cur=q.front();
+            q.pop();
+            auto adj=cur;
+            for(int i=0;i<adj.size();i++)
+            {
+                for(int j=0;j<adj[0].size();j++)
+                {
+                    flip(i,j,adj);
+                    if(all_zero(adj)) return mp[cur]+1;
+                    if(vis.find(adj)==vis.end())
+                    {
+                        vis.insert(adj);
+                        mp[adj]=mp[cur]+1;
+                        q.push(adj);
+                    }
+                    flip(i,j,adj);
+                }
+            }
+        }
+        return -1;
+    }
+};
