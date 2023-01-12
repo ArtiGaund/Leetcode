63497a729a0b894e1cc47b7e5e7d223a62048f7d
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-01-12 11:13:14 +0530

    Time: 67 ms (88.90%), Space: 19.2 MB (87.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        if(grid[0][0]==1 or grid[n-1][m-1]==1) return -1;
+        int row=grid.size(),col=grid[0].size();
+        if(grid[0][0]==1 or grid[row-1][col-1]==1) return -1;
         queue<pair<int,int>> q;
         q.push({0,0});
         grid[0][0]=1;
-        vector<pair<int,int>> dir={{-1,-1},{-1,0},{0,-1},{1,1},{1,0},{0,1},{-1,1},{1,-1}};
-        int ans=0;
+        int res=0;
+        vector<pair<int,int>> dir={{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1},{1,0},{0,1},{1,1}};
         while(!q.empty())
         {
+            res++;
             int size=q.size();
-            ans++;
-            for(int i=0;i<size;i++)
+            while(size--)
             {
-                int row=q.front().first;
-                int col=q.front().second;
+                int r=q.front().first;
+                int c=q.front().second;
                 q.pop();
-                if(row==n-1 and col==m-1) return ans;
+                if(r==row-1 and c==col-1) return res;
                 for(auto d:dir)
                 {
-                    int r=row+d.first;
-                    int c=col+d.second;
-                    if(r>=0 and r<n and c>=0 and c<m and grid[r][c]==0)
+                    int new_r=r+d.first;
+                    int new_c=c+d.second;
+                    if(new_r>=0 and new_r<row and new_c>=0 and new_c<col and 
+                       grid[new_r][new_c]==0)
                     {
-                        q.push({r,c});
-                        grid[r][c]=1;
+                        q.push({new_r,new_c});
+                        grid[new_r][new_c]=1;
                     }
                 }
             }
