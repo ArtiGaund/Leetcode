349dbaeb5195e6f78387c7ba10105e667909b9bb
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-01-24 18:03:19 +0530

    Time: 71 ms (32.11%), Space: 18.7 MB (80.11%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -5,7 +5,7 @@ public:
         int i=0,j=0;
         while(i<first.size() and j<second.size())
         {
-            if((first[i][0]>=second[j][0] and first[i][0]<=second[j][1]) or    (second[j][0]>=first[i][0] and second[j][0]<=first[i][1]))
+            if((first[i][0]>=second[j][0] and first[i][0]<=second[j][1]) or           (second[j][0]>=first[i][0] and second[j][0]<=first[i][1]))
             {
                 int start=max(first[i][0],second[j][0]);
                 int end=min(first[i][1],second[j][1]);
