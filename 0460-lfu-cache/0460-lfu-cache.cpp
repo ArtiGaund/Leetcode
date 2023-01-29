d12a8e59fd34bb7c47215243cdbd5c3d98b1b86e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-01-29 18:03:13 +0530

    Time: 541 ms (72.63%), Space: 186.9 MB (38.80%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -0,0 +1,48 @@
+class LFUCache {
+public:
+    int capacity;
+    unordered_map<int,pair<int,int>> m; //key->{val,freq}
+    unordered_map<int,list<int>> freq; //freq->list of keys
+    unordered_map<int,list<int>::iterator> pos; //key->pos in freq list
+    int minFreq=INT_MAX;
+    LFUCache(int capacity):capacity(capacity) {
+        
+    }
+    
+    int get(int key) {
+        if(m.count(key)==0) return -1;
+        freq[m[key].second].erase(pos[key]);
+        m[key].second++;
+        freq[m[key].second].push_back(key);
+        pos[key]=--freq[m[key].second].end();
+        if(freq[minFreq].size()==0) minFreq++;
+        return m[key].first;
+    }
+    
+    void put(int key, int value) {
+        if(capacity<=0) return;
+        if(m.count(key) and get(key)!=-1)
+        {
+            m[key].first=value;
+            return;
+        }
+        if(m.size()>=capacity)
+        {
+            int k=freq[minFreq].front();
+            freq[minFreq].pop_front();
+            pos.erase(k);
+            m.erase(k);
+        }
+        m[key]={value,1};
+        freq[1].push_back(key);
+        pos[key]=--freq[1].end();
+        minFreq=1;
+    }
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
