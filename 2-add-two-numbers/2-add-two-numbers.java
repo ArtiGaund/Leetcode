c8250a73b7f3e7770c080710f6e21c16b07e1f43
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-08 20:56:17 +0700

    revision

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -1,36 +1,36 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let current = dummy;
-    let carry = 0;
-    
-    while (l1 !== null || l2 !== null || carry !== 0) {
-        let sum = carry;
-        if (l1 !== null) {
-            sum += l1.val;
-            l1 = l1.next;
-        }
-        if (l2 !== null) {
-            sum += l2.val;
-            l2 = l2.next;
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            current.next = new ListNode(sum);
+            current = current.next;
         }
         
-        carry = Math.floor(sum / 10);
-        sum = sum % 10;
-        current.next = new ListNode(sum);
-        current = current.next;
+        return dummy.next;
     }
-    
-    return dummy.next;
-};
+}
