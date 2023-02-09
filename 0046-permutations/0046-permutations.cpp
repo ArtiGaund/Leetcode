8c3f04acf45b977ea1860688e1f5e6b62ca6bc22
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-02-09 15:22:01 +0530

    Time: 9 ms (15.05%), Space: 8.4 MB (13.41%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> res;
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        queue<vector<int>> q;
-        q.push(vector<int>());
-        for(auto cur:nums)
+        vector<int> cur;
+        solve(nums,0,cur);
+        return res;
+    }
+    void solve(vector<int> &nums,int index,vector<int> &cur)
+    {
+        if(index==nums.size()) res.push_back(cur);
+        else
         {
-            int n=q.size();
-            for(int i=0;i<n;i++)
+            for(int i=0;i<=cur.size();i++)
             {
-                vector<int> old=q.front();
-                q.pop();
-                for(int j=0;j<=old.size();j++)
-                {
-                    vector<int> newPer(old);
-                    newPer.insert(newPer.begin()+j,cur);
-                    if(newPer.size()==nums.size()) res.push_back(newPer);
-                    else q.push(newPer);
-                }
+                vector<int> newPer(cur);
+                newPer.insert(newPer.begin()+i,nums[index]);
+                solve(nums,index+1,newPer);
             }
         }
-        return res;
     }
 };
