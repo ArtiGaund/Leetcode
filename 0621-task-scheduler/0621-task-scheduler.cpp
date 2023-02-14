cac80c0c2702e8bda871f4d43fa12331f584e5f5
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-02-14 12:55:41 +0530

    Time: 139 ms (30.48%), Space: 49.1 MB (6.62%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    struct valComp
+    {
+        bool operator()(const pair<char,int> &x,const pair<char,int> &y)
+        {
+            return y.second>x.second;
+        }
+    };
+    int leastInterval(vector<char>& tasks, int k) {
+      unordered_map<char,int> mp;
+        for(char ch:tasks) mp[ch]++;
+        priority_queue<pair<char,int>,vector<pair<char,int>>,valComp> maxHeap;
+        for(auto it:mp) maxHeap.push(it);
+        int intervalCount=0;
+        while(!maxHeap.empty())
+        {
+            vector<pair<char,int>> waitList;
+            int n=k+1;
+            for(;n>0 and !maxHeap.empty();n--)
+            {
+                intervalCount++;
+                auto cur=maxHeap.top();
+                maxHeap.pop();
+                if(cur.second>1)
+                {
+                    cur.second--;
+                    waitList.push_back(cur);
+                }
+            }
+            for(auto it:waitList)
+            {
+                maxHeap.push(it);
+            }
+            if(!maxHeap.empty()) intervalCount+=n;
+        }
+        return intervalCount;
+    }
+};
