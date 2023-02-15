c64257b91fbde3b8ac9dafc8049c4328ed3a6a09
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-02-15 17:56:32 +0530

    Time: 321 ms (74.76%), Space: 108.9 MB (86.90%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -4,37 +4,36 @@ public:
     {
         bool operator()(const pair<int,int> &x,const pair<int,int> &y)
         {
-            return x.first+x.second<y.first+y.second;
+            return y.first+y.second>x.first+x.second;
         }
     };
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int,int>> minHeap;
+      vector<pair<int,int>> maxHeap;
         for(int i=0;i<nums1.size() and i<k;i++)
         {
             for(int j=0;j<nums2.size() and j<k;j++)
             {
-                if(minHeap.size()<k)
+                if(maxHeap.size()<k)
                 {
-                    minHeap.push_back({nums1[i],nums2[j]});
-                    push_heap(minHeap.begin(),minHeap.end(),sumComp());
+                    maxHeap.push_back({nums1[i],nums2[j]});
+                    push_heap(maxHeap.begin(),maxHeap.end(),sumComp());
                 }
                 else
                 {
-                    if(nums1[i]+nums2[j]>minHeap.front().first+minHeap.front().second)
-                    break;
-                    else
+                    if(nums1[i]+nums2[j]>maxHeap.front().first+maxHeap.front().second) break;
+                    else 
                     {
-                        pop_heap(minHeap.begin(),minHeap.end(),sumComp());
-                        minHeap.pop_back();
-                        minHeap.push_back({nums1[i],nums2[j]});
-                        push_heap(minHeap.begin(),minHeap.end(),sumComp());
+                        pop_heap(maxHeap.begin(),maxHeap.end(),sumComp());
+                        maxHeap.pop_back();
+                        maxHeap.push_back({nums1[i],nums2[j]});
+                        push_heap(maxHeap.begin(),maxHeap.end(),sumComp());
                     }
                 }
             }
         }
         vector<vector<int>> res;
-        for(int i=0;i<minHeap.size();i++)
-            res.push_back({minHeap[i].first,minHeap[i].second});
+        for(int i=0;i<maxHeap.size();i++)
+            res.push_back({maxHeap[i].first,maxHeap[i].second});
         return res;
     }
 };
