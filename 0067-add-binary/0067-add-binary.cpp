722a77995b64e95a370134acb10deea5b63fc85b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-03-06 17:18:48 +0530

    Time: 4 ms (46.79%), Space: 8.3 MB (6.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string res="";
-        int s=0;
         int i=a.size()-1,j=b.size()-1;
-        while(i>=0||j>=0||s==1)
+        int s=0;
+        while(i>=0 or j>=0 or s==1)
         {
             s+=(i>=0?a[i]-'0':0);
             s+=(j>=0?b[j]-'0':0);
