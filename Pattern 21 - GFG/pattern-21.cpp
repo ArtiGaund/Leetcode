1f72fe2e38ad7e23bb335d7738f888123f818210
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-03-12 12:18:35 +0530

    Added solution - LeetHub

---
+++ Pattern 21 - GFG/pattern-21.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        //border is having star and inner is space
+       for(int i=0;i<n;i++){
+         
+         // inner loop for printing the stars at borders only.
+         for(int j=0;j<n;j++){
+             
+             if(i==0 || j==0 || i==n-1 || j==n-1)
+                cout<<"*";
+                
+             // if not border index, print space.
+             else cout<<" ";
+         }
+         
+          // As soon as the stars for each iteration are printed, we move to the
+          // next row and give a line break otherwise all stars
+          // would get printed in 1 line.
+          cout<<endl;
+       }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
