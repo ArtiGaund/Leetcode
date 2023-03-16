bc43d7e2588ec196797dff571a2129a13ee6f9cb
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-03-16 15:43:46 +0530

    Time: 678 ms (72.80%), Space: 15.6 MB (52.94%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    struct trie
+    {
+        bool end;
+        trie *child[26];
+    };
+    vector<string> res;
+    void insert(trie* root,string s)
+    {
+        trie *cur=root;
+        for(char ch:s)
+        {
+            int index=ch-'a';
+            if(cur->child[index]==nullptr)
+                cur->child[index]=new trie();
+            cur=cur->child[index];
+        }
+        cur->end=true;
+    }
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        trie *root=new trie();
+        for(string &s:words) insert(root,s);
+        string s;
+        for(int i=0;i<board.size();i++)
+            for(int j=0;j<board[0].size();j++)
+                dfs(i,j,board,root,s);
+        return res;
+    }
+    void dfs(int i,int j,vector<vector<char>> &b,trie *root,string &word)
+    {
+        if(!(i>=0 and i<b.size() and j>=0 and j<b[0].size() and b[i][j]!=' ')) return;
+        char ch=b[i][j];
+        if(root->child[ch-'a']==nullptr) return;
+        root=root->child[ch-'a'];
+        word.push_back(ch);
+        if(root->end)
+        {
+            res.push_back(word);
+            root->end=false;
+        }
+        b[i][j]=' ';
+        dfs(i+1,j,b,root,word);
+        dfs(i-1,j,b,root,word);
+        dfs(i,j+1,b,root,word);
+        dfs(i,j-1,b,root,word);
+        b[i][j]=ch;
+        word.pop_back();
+    }
+};
