261b6e3deb9f513992ab283a15ec1861311a5c91
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-03-22 18:53:29 +0530

    Time: 495 ms (67.69%), Space: 125.3 MB (74.00%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,35 +1,41 @@
+class UnionFind
+{
+    public:
+    vector<int> rank,parent;
+    UnionFind(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+    int findParent(int u)
+    {
+        if(parent[u]==u) return u;
+        else return parent[u]=findParent(parent[u]);
+    }
+    void unionset(int u,int v)
+    {
+        int x=findParent(u);
+        int y=findParent(v);
+        if(rank[x]<rank[y]) parent[x]=y;
+        else if(rank[x]>rank[y]) parent[y]=x;
+        else
+        {
+            parent[x]=y;
+            rank[y]++;
+        }
+    }
+};
 class Solution {
 public:
-    int bfs(int n,vector<pair<int,int>> adj[])
-    {
-        vector<bool> vis(n+1,0);
-        queue<int> q;
-        int res=INT_MAX;
-        q.push(1);
-        vis[1]=1;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(auto e:adj[node])
-            {
-                res=min(res,e.second);
-                if(!vis[e.first])
-                {
-                    vis[e.first]=1;
-                    q.push(e.first);
-                }
-            }
-        }
-        return res;
-    }
-        int minScore(int n, vector<vector<int>>& roads) {
-        vector<pair<int,int>> adj[n+1];
+    int minScore(int n, vector<vector<int>>& roads) {
+        UnionFind uf(n+1);
         for(auto r:roads)
-        {
-            adj[r[0]].push_back({r[1],r[2]});
-            adj[r[1]].push_back({r[0],r[2]});
-        }
-        return bfs(n,adj);
+            uf.unionset(r[0],r[1]);
+        int res=INT_MAX;
+        for(auto r:roads)
+            if(uf.findParent(1)==uf.findParent(r[0]))
+                res=min(res,r[2]);
+        return res;
     }
 };
