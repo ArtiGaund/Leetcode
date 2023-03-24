898f9e19511b30311e8325a94033c024abaa62b4
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-03-24 18:46:17 +0530

    Time: 24 ms (50.19%), Space: 16.9 MB (57.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0;
-        for(int a:nums) res=res^a;
-        return res;
+        int xor1=0;
+        for(int i=0;i<nums.size();i++)
+            xor1=xor1^nums[i];
+        return xor1;
     }
 };
