b49dba2c8655ac260f5fadde45bc6321844c4b01
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-04-01 16:32:34 +0530

    Time: 19 ms (41.52%), Space: 13.3 MB (42.33%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    bool solve(int index,string s,set<string> &dict,vector<int> &dp)
-    {
-        if(index==s.size()) return true;
-        if(dp[index]!=-1) return dp[index];
-        for(int i=index;i<s.size();i++)
-        {
-            string cur=s.substr(index,i-index+1);
-            if(dict.find(cur)!=dict.end() and solve(i+1,s,dict,dp))
-                return dp[index]=true;
-        }
-        return dp[index]=false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         set<string> dict(wordDict.begin(),wordDict.end());
-        vector<int> dp(s.size(),-1);
-        return solve(0,s,dict,dp);
+        int n=s.size();
+        vector<int> dp(n+1,false);
+        dp[n]=true;
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int i=index;i<n;i++)
+            {
+                string cur=s.substr(index,i-index+1);
+                if(dict.find(cur)!=dict.end() and dp[i+1])
+                {
+                    dp[index]=true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
 };
