a02f417d937afcb22ec875cc284520c4463864ac
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-04-01 16:57:27 +0530

    Time: 424 ms (30.79%), Space: 95.1 MB (38.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(auto a:nums) sum+=a;
+        for(int a:nums) sum+=a;
         if(sum%2!=0) return false;
+        int k=sum/2;
         int n=nums.size();
-        sum=sum/2;
-        vector<vector<bool>> dp(n,vector<bool>(sum+1));
-        for(int i=0;i<n;i++) dp[i][0]=true;
-        for(int s=1;s<=sum;s++)
-            dp[0][s]=(nums[0]==s?true:false);
-        for(int i=1;i<n;i++)
+        vector<vector<int>> dp(n,vector<int>(k+1));
+        if(k>=nums[0]) dp[0][nums[0]]=true;
+        for(int index=1;index<n;index++)
         {
-            for(int s=1;s<=sum;s++)
+            for(int target=1;target<=k;target++)
             {
-                if(dp[i-1][s]) dp[i][s]=dp[i-1][s];
-                else if(s>=nums[i])
-                    dp[i][s]=dp[i-1][s-nums[i]];
+                int nottaken=dp[index-1][target];
+                int taken=false;
+                if(target>=nums[index]) taken=dp[index-1][target-nums[index]];
+                dp[index][target]=(taken or nottaken);
             }
         }
-        return dp[n-1][sum];
+        return dp[n-1][k];
     }
 };
