c37bb77e8e988140c812e9670dab7b76219eec6d
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-04-04 18:50:32 +0530

    Time: 59 ms (95.96%), Space: 18.5 MB (48.12%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        int n=words.size();
         unordered_map<string,int> mp;
+        int n=words.size();
         for(int i=0;i<n;i++)
             mp[words[i]]=i;
-        //build graph
+        // graph
         vector<int> graph[n];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<words[i].size();j++)
             {
                 auto temp=words[i].substr(0,j)+words[i].substr(j+1);
-                if(mp.count(temp)) graph[mp[temp]].push_back(i);
+                if(mp.count(temp))
+                    graph[mp[temp]].push_back(i);
             }
         }
         //topo sort
         vector<int> indegree(n,0);
         for(auto it:graph)
         {
-            for(int x:it) indegree[x]++;
+            for(int x:it)
+                indegree[x]++;
         }
         queue<int> q;
         for(int i=0;i<n;i++)
@@ -27,13 +29,13 @@ public:
         int res=0;
         while(!q.empty())
         {
-            int num=q.size();
+            int size=q.size();
             res++;
-            while(num--)
+            while(size--)
             {
-                int t=q.front();
+                auto temp=q.front();
                 q.pop();
-                for(auto it:graph[t])
+                for(auto it:graph[temp])
                 {
                     indegree[it]--;
                     if(indegree[it]==0) q.push(it);
