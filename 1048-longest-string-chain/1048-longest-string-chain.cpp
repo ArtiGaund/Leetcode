95677084647ada0dd3b35ac081d0263fecd2846b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-04-04 18:12:48 +0530

    Time: 60 ms (95.36%), Space: 16.7 MB (57.62%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -4,36 +4,24 @@ public:
     {
         return s1.size()<s2.size();
     }
-    bool checkPossible(string &s1,string &s2)
-    {
-        if(s1.size()!=s2.size()+1) return false;
-        int first=0,sec=0;
-        while(first<s1.size())
-        {
-            if(s1[first]==s2[sec])
-            {
-                first++;
-                sec++;
-            }
-            else first++;
-        }
-        if(first==s1.size() and sec==s2.size()) return true;
-        return false;
-    }
     int longestStrChain(vector<string>& words) {
         int n=words.size();
+        unordered_map<string,int> dp;
         sort(words.begin(),words.end(),comp);
-        vector<int> dp(n,1);
-        int maxi=1;
+        int res=1;
         for(int i=0;i<n;i++)
         {
-            for(int prev=0;prev<i;prev++)
+            dp[words[i]]=1;
+            int maxPosSubChain=0;
+            for(int j=0;j<words[i].size();j++)
             {
-                if(checkPossible(words[i],words[prev]) and dp[i]<dp[prev]+1)
-                    dp[i]=dp[prev]+1;
+                string predecessor=words[i].substr(0,j)+words[i].substr(j+1);
+                if(dp.find(predecessor)!=dp.end())
+                    maxPosSubChain=max(maxPosSubChain,dp[predecessor]);
             }
-            maxi=max(maxi,dp[i]);
+            dp[words[i]]+=maxPosSubChain;
+            res=max(res,dp[words[i]]);
         }
-        return maxi;
+        return res;
     }
 };
