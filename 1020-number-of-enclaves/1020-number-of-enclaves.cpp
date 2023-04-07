2e7ad6f49ab98d4c20218c642554a12e648be25e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-04-07 18:56:35 +0530

    Time: 71 ms (86.45%), Space: 32.9 MB (33.12%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int n,m;
-    void dfs(int r,int c,vector<vector<int>> &grid,vector<vector<int>> &vis)
+    void dfs(vector<vector<int>> &vis,vector<vector<int>> &grid,int r,int c)
     {
         vis[r][c]=1;
-        if(r>0 and !vis[r-1][c] and grid[r-1][c]==1)
-            dfs(r-1,c,grid,vis);
-        if(r<n-1 and !vis[r+1][c] and grid[r+1][c]==1)
-            dfs(r+1,c,grid,vis);
-        if(c>0 and !vis[r][c-1] and grid[r][c-1]==1)
-            dfs(r,c-1,grid,vis);
-        if(c<m-1 and !vis[r][c+1] and grid[r][c+1]==1)
-            dfs(r,c+1,grid,vis);
+        if(r>0 and grid[r-1][c]==1 and !vis[r-1][c])
+            dfs(vis,grid,r-1,c);
+        if(r<n-1 and grid[r+1][c]==1 and !vis[r+1][c])
+            dfs(vis,grid,r+1,c);
+        if(c>0 and grid[r][c-1]==1 and !vis[r][c-1])
+            dfs(vis,grid,r,c-1);
+        if(c<m-1 and grid[r][c+1] and !vis[r][c+1])
+            dfs(vis,grid,r,c+1);
     }
     int numEnclaves(vector<vector<int>>& grid) {
         n=grid.size();
@@ -24,14 +24,14 @@ public:
                 if(i==0 or i==n-1 or j==0 or j==m-1)
                 {
                     if(!vis[i][j] and grid[i][j]==1)
-                        dfs(i,j,grid,vis);
+                        dfs(vis,grid,i,j);
                 }
             }
         }
         int count=0;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=1;j<m;j++)
             {
                 if(!vis[i][j] and grid[i][j]==1)
                     count++;
