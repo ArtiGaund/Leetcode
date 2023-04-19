0a92b1d20f1a6448c07511f8a1e269f2198c0e3d
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-04-19 17:31:17 +0530

    Time: 15 ms (55.92%), Space: 15.9 MB (60.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,15 +2,39 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int> mp;
-        int min=(int)(n/3)+1;
-        vector<int> res;
+        int cnt1=0,cnt2=0;
+        int ele1=INT_MIN,ele2=INT_MIN;
         for(int i=0;i<n;i++)
         {
-            mp[nums[i]]++;
-            if(mp[nums[i]]==min) res.push_back(nums[i]);
-            if(res.size()==2) break;
+            if(cnt1==0 and nums[i]!=ele2)
+            {
+                cnt1=1;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 and nums[i]!=ele1)
+            {
+                cnt2=1;
+                ele2=nums[i];
+            }
+            else if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
         }
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ele1==nums[i]) cnt1++;
+            if(ele2==nums[i]) cnt2++;
+        }
+        int min=(int)(n/3)+1;
+        vector<int> res;
+        if(cnt1>=min) res.push_back(ele1);
+        if(cnt2>=min) res.push_back(ele2);
         return res;
     }
 };
