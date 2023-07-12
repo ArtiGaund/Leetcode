7c13c1f588d8930b773aa1c482858f5ed5fcdd9a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-07-12 15:15:57 +0530

    Time: 12 ms (46.55%), Space: 13.5 MB (71.25%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res(2,-1);
-        res[0]=search(nums,target,false);
-        if(res[0]!=-1)
-            res[1]=search(nums,target,true);
-        return res;
-    }
     int search(vector<int> &nums,int target,bool findMaxIndex)
     {
+        int low=0,high=nums.size()-1;
         int keyIndex=-1;
-        int start=0,end=nums.size()-1;
-        while(start<=end)
+        while(low<=high)
         {
-            int mid=start+(end-start)/2;
-            if(target<nums[mid]) end=mid-1;
-            else if(target>nums[mid]) start=mid+1;
+            int mid=low+(high-low)/2;
+            if(target<nums[mid]) high=mid-1;
+            else if(target>nums[mid]) low=mid+1;
             else
             {
                 keyIndex=mid;
-                if(findMaxIndex) start=mid+1;
-                else end=mid-1;
+                if(findMaxIndex) low=mid+1;
+                else high=mid-1;
             }
         }
         return keyIndex;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2,-1);
+        res[0]=search(nums,target,false);
+        if(res[0]!=-1) res[1]=search(nums,target,true);
+        return res;
     }
 };
