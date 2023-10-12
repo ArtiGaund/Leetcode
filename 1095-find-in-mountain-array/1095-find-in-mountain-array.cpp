19eef835f6cccf72ddbcce687c7d5df1c2db03a1
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-10-12 18:11:03 +0530

    Time: 6 ms (44.79%), Space: 7.3 MB (27.99%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -10,15 +10,15 @@
 
 class Solution {
 public:
-    int findInMountainArray(int target, MountainArray &Arr) {
-        int length=Arr.length();
-        // find the index of the peak element
+    int findInMountainArray(int target, MountainArray &arr) {
+        int length=arr.length();
+        //find peak element index
         int low=1,high=length-2;
         while(low!=high)
         {
             int index=(low+high)>>1;
-            int cur=Arr.get(index);
-            int next=Arr.get(index+1);
+            int cur=arr.get(index);
+            int next=arr.get(index+1);
             if(cur<next)
             {
                 if(cur==target) return index;
@@ -28,29 +28,28 @@ public:
             else high=index;
         }
         int peakIndex=low;
-        // find in the strictly increasing part
+        //search in strictly increasing part
         low=0;
         high=peakIndex;
         while(low<=high)
         {
             int index=(low+high)>>1;
-            int cur=Arr.get(index);
+            int cur=arr.get(index);
             if(cur==target) return index;
             else if(cur<target) low=index+1;
             else high=index-1;
         }
-        //find in the strictly decreasing part
+        //search in strictly decreasing part
         low=peakIndex+1;
         high=length-1;
         while(low<=high)
         {
             int index=(low+high)>>1;
-            int cur=Arr.get(index);
+            int cur=arr.get(index);
             if(cur==target) return index;
             else if(cur>target) low=index+1;
             else high=index-1;
         }
-        // not present
         return -1;
     }
 };
