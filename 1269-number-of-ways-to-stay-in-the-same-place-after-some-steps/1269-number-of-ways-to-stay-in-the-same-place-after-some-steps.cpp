38da96e206763f5207cadbd0f84cae93968832a5
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-10-15 17:56:35 +0530

    Time: 25 ms (47.40%), Space: 13.1 MB (53.03%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int mod=1e9+7;
-    int arrLen;
-    int solve(int cur,int remain)
-    {
-        if(remain==0)
-        {
-            if(cur==0) return 1;
-            return 0;
-        }
-        if(dp[cur][remain]!=-1)
-            return dp[cur][remain];
-        int res=solve(cur,remain-1);
-        if(cur>0) res=(res+solve(cur-1,remain-1))%mod;
-        if(cur<arrLen-1) res=(res+solve(cur+1,remain-1))%mod;
-        dp[cur][remain]=res;
-        return res;
-    }
     int numWays(int steps, int arrLen) {
+        int mod=1e9+7;
         arrLen=min(steps,arrLen);
-        this->arrLen=arrLen;
-        dp=vector(arrLen,vector(steps+1,-1));
-        return solve(0,steps);
+        vector dp(arrLen,vector<int>(steps+1,0));
+        dp[0][0]=1;
+        for(int remain=1;remain<=steps;remain++)
+        {
+            for(int cur=arrLen-1;cur>=0;cur--)
+            {
+                int res=dp[cur][remain-1];
+                if(cur>0) res=(res+dp[cur-1][remain-1])%mod;
+                if(cur<arrLen-1) res=(res+dp[cur+1][remain-1])%mod;
+                dp[cur][remain]=res;
+            }
+        }
+        return dp[0][steps];
     }
 };
