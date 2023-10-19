5888da3e77c9c7510ce8df70fe1e8adcde9d6f38
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-10-19 17:24:13 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (69.01%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int index1=s.size()-1,index2=t.size()-1;
+        while(index1>=0 or index2>=0)
+        {
+            int a=getNextValidChar(s,index1);
+            int b=getNextValidChar(t,index2);
+            if(a<0 and b<0) return true;
+            if(a<0 or b<0) return false;
+            if(s[a]!=t[b]) return false;
+            index1=a-1;
+            index2=b-1;
+        }
+        return true;
+    }
+    int getNextValidChar(string &str,int index)
+    {
+        int backspace=0;
+        while(index>=0)
+        {
+            if(str[index]=='#') backspace++;
+            else if(backspace>0) backspace--;
+            else break;
+            index--;
+        }
+        return index;
+    }
+};
