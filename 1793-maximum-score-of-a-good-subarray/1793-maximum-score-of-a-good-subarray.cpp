57585c8c3cb80009dbe0ab97e4c2f87da778f7be
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-10-22 18:31:09 +0530

    Time: 138 ms (62.28%), Space: 89.8 MB (73.03%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int> left(n,-1),st;
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() and nums[st.back()]>nums[i])
-            {
-                left[st.back()]=i;
-                st.pop_back();
+        int n = nums.size();
+        int left = k;
+        int right = k;
+        int ans = nums[k];
+        int currMin = nums[k];
+        
+        while (left > 0 || right < n - 1) {
+            if ((left > 0 ? nums[left - 1]: 0) < (right < n - 1 ? nums[right + 1] : 0)) {
+                right++;
+                currMin = min(currMin, nums[right]);
+            } else {
+                left--;
+                currMin = min(currMin, nums[left]);
             }
-            st.push_back(i);
+            
+            ans = max(ans, currMin * (right - left + 1));
         }
-        vector<int> right(n,n);
-        st.clear();
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() and nums[st.back()]>nums[i])
-            {
-                right[st.back()]=i;
-                st.pop_back();
-            }
-            st.push_back(i);
-        }
-        int res=0;
-        for(int i=0;i<n;i++)
-        {
-            if(left[i]<k and right[i]>k)
-                res=max(res,nums[i]*(right[i]-left[i]-1));
-        }
-        return res;
+        
+        return ans;
     }
 };
