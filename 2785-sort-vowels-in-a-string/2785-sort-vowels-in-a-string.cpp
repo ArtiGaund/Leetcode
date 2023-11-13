f6061c7be49aa612da3d7d1e8cb73fbfe383993f
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-11-13 17:49:26 +0530

    Time: 48 ms (29.43%), Space: 13.1 MB (51.70%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -4,18 +4,21 @@ public:
         return (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U');
     }
     string sortVowels(string s) {
-        string temp;
+       unordered_map<char,int> count;
         for(char ch:s){
-            if(isVowel(ch)) temp+=ch;
+            if(isVowel(ch)){
+                count[ch]++;
+            }
         }
-        sort(temp.begin(),temp.end());
-        int j=0;
+        string sortedVowel="AEIOUaeiou";
         string res;
+        int j=0;
         for(int i=0;i<s.size();i++){
-            if(isVowel(s[i])){
-                res+=temp[j++];
-            } else{
-                res+=s[i];
+            if(!isVowel(s[i])) res+=s[i];
+            else{
+                while(count[sortedVowel[j]]==0) j++;
+                res+=sortedVowel[j];
+                count[sortedVowel[j]]--;
             }
         }
         return res;
