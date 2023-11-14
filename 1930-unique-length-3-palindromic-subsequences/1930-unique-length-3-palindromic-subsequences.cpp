e9b27fd0d401fce7d20919016090cf7568f6c38a
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-11-14 17:34:23 +0530

    Time: 279 ms (56.62%), Space: 13.2 MB (80.37%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<char> letters;
-        for(char ch:s) letters.insert(ch);
+        vector<int> first(26,-1);
+        vector<int> last(26,-1);
+        for(int i=0;i<s.size();i++){
+            int cur=s[i]-'a';
+            if(first[cur]==-1) first[cur]=i;
+            last[cur]=i;
+        }
         int res=0;
-        for(char ch:letters){
-            int i=-1,j=0;
-            for(int k=0;k<s.size();k++){
-                if(s[k]==ch){
-                    if(i==-1) i=k;
-                    j=k;
-                }
-            }
+        for(int i=0;i<26;i++){
+            if(first[i]==-1) continue;
             unordered_set<char> between;
-            for(int k=i+1;k<j;k++) between.insert(s[k]);
+            for(int j=first[i]+1;j<last[i];j++) between.insert(s[j]);
             res+=between.size();
         }
         return res;
