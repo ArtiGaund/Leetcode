a46e10a7d63d7eb3e4d66d534a66baf40c5d0c1e
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-11-22 17:37:34 +0530

    Time: 115 ms (92.73%), Space: 65.9 MB (99.57%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        vector<int> res;
+        while(!q.empty()){
+            auto [row,col] =q.front();
+            q.pop();
+            res.push_back(nums[row][col]);
+            if(col==0 and row+1<nums.size()) q.push({row+1,col});
+            if(col+1<nums[row].size()) q.push({row,col+1});
+        }
+        return res;
+    }
+};
