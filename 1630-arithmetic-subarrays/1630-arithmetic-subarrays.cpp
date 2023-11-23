16aaa271ceb4a5dcf139f5a7f80b0710e110aa61
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-11-23 18:38:34 +0530

    Time: 180 ms (5.19%), Space: 81 MB (5.03%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     bool check(vector<int> &temp){
-        sort(temp.begin(),temp.end());
-        int diff=temp[1]-temp[0];
-        for(int i=2;i<temp.size();i++)
-            if(temp[i]-temp[i-1]!=diff) return false;
+       int minE=INT_MAX;
+        int maxE=INT_MIN;
+        unordered_set<int> st;
+        for(int num:temp){
+            minE=min(minE,num);
+            maxE=max(maxE,num);
+            st.insert(num);
+        }
+        if((maxE-minE)%(temp.size()-1)!=0) return false;
+        int diff=(maxE-minE)/(temp.size()-1);
+        int cur=minE+diff;
+        while(cur<maxE){
+            if(st.find(cur)==st.end()) return false;
+            cur+=diff;
+        }
         return true;
     }
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
