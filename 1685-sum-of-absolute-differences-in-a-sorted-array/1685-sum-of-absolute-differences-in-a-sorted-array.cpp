7f489718c7e0d48390169e329f044d22e8bca7cf
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-11-25 17:53:50 +0530

    Time: 79 ms (94.32%), Space: 87.7 MB (40.81%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> prefix= {nums[0]};
-        for(int i=1;i<n;i++){
-            prefix.push_back(prefix[i-1]+nums[i]);
-        }
+       int n=nums.size();
+        int totalSum= accumulate(nums.begin(),nums.end(),0);
+        int leftSum=0;
         vector<int> res;
         for(int i=0;i<n;i++){
-            int leftSum=prefix[i]-nums[i];
-            int rightSum=prefix[n-1]-prefix[i];
+            int rightSum=totalSum-leftSum-nums[i];
             int leftCount=i;
             int rightCount=n-1-i;
-            int leftTotal= leftCount*nums[i]-leftSum;
+            int leftTotal=leftCount*nums[i]-leftSum;
             int rightTotal=rightSum-rightCount*nums[i];
             res.push_back(leftTotal+rightTotal);
+            leftSum+=nums[i];
         }
         return res;
     }
