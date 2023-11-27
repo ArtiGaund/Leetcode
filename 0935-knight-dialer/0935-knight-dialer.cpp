c6565450e82f3a816c09b4f8583a727e0185ddbb
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2023-11-27 22:09:58 +0530

    Time: 35 ms (87.72%), Space: 9.7 MB (64.77%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int knightDialer(int n) {
+        if(n==1) return 10;
+        vector<vector<long>> A = {
+            {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+            {1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 1, 0, 0, 0, 0, 0}
+        };
+        
+        vector<vector<long>> v = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+        n--;
+        while(n>0){
+            if((n&1)!=0){
+                v=multiply(v,A);
+            }
+            A=multiply(A,A);
+            n>>=1;
+        }
+        int res=0;
+        for(long num:v[0]){
+            res=(res+num)%mod;
+        }
+        return res;
+    }
+     vector<vector<long>> multiply(vector<vector<long>>& A, vector<vector<long>>& B) {
+        vector<vector<long>> result(A.size(), vector<long>(B[0].size(), 0));
+        
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < B[0].size(); j++) {
+                for (int k = 0; k < B.size(); k++) {
+                    result[i][j] = (result[i][j] + A[i][k] * B[k][j]) % mod;
+                }
+            }
+        }
+        
+        return result;
+     }
+};
