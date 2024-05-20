109aaecd44e1ecfc3b6839637ee91316050e5078
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2024-05-20 18:53:55 +0530

    Time: 4 ms (55.14%) | Memory: 8.4 MB (65.34%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int res=0;
+    int subsetXORSum(vector<int>& nums) {
+        backtrack(nums,0,0);
+        return res;
+    }
+    void backtrack(vector<int> &nums,int start,int sum){
+        if(start==nums.size()){
+            res+=sum;
+            return;
+        }
+        backtrack(nums,start+1,sum);
+        sum^=nums[start];
+        backtrack(nums,start+1,sum);
+        return;
+    }
+};
