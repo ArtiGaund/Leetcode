49217ffa74d81099564349465ca7499284b4b90c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2024-05-22 19:19:48 +0530

    Time: 113 ms (30.77%) | Memory: 87.6 MB (25.81%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+    vector<string> cur;
+    vector<vector<string>> partition(string s) {
+        backtrack(s,0);
+        return res;
+    }
+    void backtrack(string s,int index)
+    {
+        if(index==s.size())
+        {
+            res.push_back(cur);
+            return;
+        }
+        for(int i=index;i<s.size();i++)
+        {
+            if(ispalindrome(s,index,i))
+            {
+                cur.push_back(s.substr(index,i-index+1));
+                backtrack(s,i+1);
+                cur.pop_back();
+            }
+        }
+    }
+    bool ispalindrome(string s,int start,int end)
+    {
+        while(start<=end)
+        {
+            if(s[start]!=s[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
