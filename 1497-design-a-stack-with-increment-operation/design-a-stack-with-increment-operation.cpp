28076b720eb457a553140637286bdb031915044b
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2024-09-30 22:17:41 +0530

    Time: 27 ms (65.70%) | Memory: 26.1 MB (29.82%) - LeetSync

---
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,52 @@
+class CustomStack {
+private:
+    // Vector to store stack elements
+    vector<int> stackArray;
+
+    // Vector to store increments for lazy propagation
+    vector<int> incrementArray;
+
+    // Current top index of the stack
+    int topIndex;
+
+public:
+    CustomStack(int maxSize) {
+        stackArray.resize(maxSize);
+        incrementArray.resize(maxSize);
+        topIndex = -1;
+    }
+
+    void push(int x) {
+        if (topIndex < (int)(stackArray.size()) - 1) {
+            stackArray[++topIndex] = x;
+        }
+    }
+
+    int pop() {
+        if (topIndex < 0) {
+            return -1;
+        }
+
+        // Calculate the actual value with increment
+        int result = stackArray[topIndex] + incrementArray[topIndex];
+
+        // Propagate the increment to the element below
+        if (topIndex > 0) {
+            incrementArray[topIndex - 1] += incrementArray[topIndex];
+        }
+
+        // Reset the increment for this position
+        incrementArray[topIndex] = 0;
+
+        topIndex--;
+        return result;
+    }
+
+    void increment(int k, int val) {
+        if (topIndex >= 0) {
+            // Apply increment to the topmost element of the range
+            int incrementIndex = min(topIndex, k - 1);
+            incrementArray[incrementIndex] += val;
+        }
+    }
+};
