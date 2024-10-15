f8dd2b1afc1098d3e3291b33017e3cd6e6367ce6
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2024-10-15 22:56:40 +0530

    Time: 27 ms (89.47%) | Memory: 15.7 MB (45.68%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long totalSwaps=0;
+        int blackBallCount=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='0') totalSwaps+=(long long)blackBallCount;
+            else blackBallCount++;
+        }
+        return totalSwaps;
+    }
+};
