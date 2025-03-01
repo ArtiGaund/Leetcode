2221b46fb69b184206bbabb941c28a40df869e92
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-03-01 19:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (23.58%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        int writeIndex=0;
+        for(int index=0;index<n;index++){
+            if(index<n-1 and nums[index]==nums[index+1] and nums[index]!=0){
+                nums[index]*=2;
+                nums[index+1]=0;
+            }
+            if(nums[index]!=0){
+                if(index!=writeIndex)
+                swap(nums[index],nums[writeIndex]);
+                writeIndex++;
+            }
+        }
+        return nums;
+    }
+};
