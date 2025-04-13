3db4395ec771e06c6e0b0bcfdc18e41c817c7ca4
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-04-13 18:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.45%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static constexpr int mod=1000000007;
+    int countGoodNumbers(long long n) {
+        auto quickmul=[](int x,long long y) -> int{
+            int ret=1,mul=x;
+            while(y>0){
+                if(y%2==1) ret=(long long)ret*mul%mod;
+                mul=(long long)mul*mul%mod;
+                y/=2;
+            }
+            return ret;
+        };
+        return (long long)quickmul(5,(n+1)/2)*quickmul(4,n/2)%mod;
+    }
+};
