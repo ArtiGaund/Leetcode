372ab3f433f11b8afb36f665773b8b97ed641f39
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-07-02 19:46:33 +0530

    Time: 389 ms (42.86%) | Memory: 272.3 MB (11.90%) - LeetSync

---
+++ 3618-find-the-original-typed-string-ii/find-the-original-typed-string-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+static const int mod = 1000000007;
+    int possibleStringCount(string word, int k) {
+        int n=word.size(),cnt=1;
+        vector<int> freq;
+        for(int i=1;i<n;i++){
+            if(word[i]==word[i-1]) cnt++;
+            else{
+                freq.push_back(cnt);
+                cnt=1;
+            }
+        }
+        freq.push_back(cnt);
+        int res=1;
+        for(int cnt:freq){
+            res=static_cast<long long>(res)*cnt%mod;
+        }
+        if(freq.size()>=k) return res;
+        vector<int> f(k),g(k,1);
+        f[0]=1;
+        for(int i=0;i<freq.size();i++){
+            vector<int> f_new(k);
+            for(int j=1;j<k;j++){
+                f_new[j]=g[j-1];
+                if(j-freq[i]-1>=0){
+                    f_new[j]=(f_new[j]-g[j-freq[i]-1]+mod)%mod;
+                }
+            }
+            vector<int> g_new(k);
+            g_new[0]=f_new[0];
+            for(int j=1;j<k;j++){
+                g_new[j]=(g_new[j-1]+f_new[j])%mod;
+            }
+            f=move(f_new);
+            g=move(g_new);
+            
+        }
+        return (res-g[k-1]+mod)%mod;
+    }
+};
