ce21dfecdf92515a88232c9b76bb06ccee5568f0
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-07-13 18:21:23 +0530

    Time: 32 ms (31.16%) | Memory: 80.3 MB (24.63%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int m=players.size(),n=trainers.size();
+        int count=0;
+        for(int i=0,j=0;i<m && j<n;i++,j++){
+            while(j<n and players[i]>trainers[j]){
+                j++;
+            }
+            if(j<n) count++;
+        }
+        return count;
+    }
+};
