42854126b97db8e1e96fb08346277ea9d3efd5e9
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-07-18 18:59:27 +0530

    Time: 131 ms (85.05%) | Memory: 144.1 MB (85.05%) - LeetSync

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long minimumDifference(vector<int>& nums) {
+        priority_queue<int> L;
+        priority_queue<int,vector<int>,greater<>> R;
+        long n=nums.size()/3,left=0,right=0,ans=LLONG_MAX;
+        vector<long> temp(nums.size());
+        for(int i=nums.size()-1;i>=n;i--)
+        {
+            R.push(nums[i]);
+            right+=nums[i];
+            if(R.size()>n)
+            {
+                right-=R.top();
+                R.pop();
+            }
+            if(R.size()==n) temp[i]=right;
+        }
+        for(int i=0;i<nums.size()-n;i++)
+        {
+            L.push(nums[i]);
+            left+=nums[i];
+            if(L.size()>n)
+            {
+                left-=L.top();
+                L.pop();
+            }
+            if(L.size()==n) ans=min(ans,left-temp[i+1]);
+        }
+        return ans;
+    }
+};
