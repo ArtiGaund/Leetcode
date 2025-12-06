6df48263fd4de6e6ade4592095f9884093cf5789
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-12-06 17:00:08 +0530

    Time: 31 ms (95.33%) | Memory: 159.9 MB (58.41%) - LeetSync

---
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums, int k) {
+        int n=nums.size();
+        long long mod=1e9+7;
+        vector<long long> dp(n+1);
+        vector<long long> prefix(n+1);
+        deque<int> minQ,maxQ;
+        dp[0]=1;
+        prefix[0]=1;
+        for(int i=0,j=0;i<n;i++){
+            while(!maxQ.empty() and nums[maxQ.back()]<=nums[i]){
+                maxQ.pop_back();
+            }
+            maxQ.push_back(i);
+            while(!minQ.empty() and nums[minQ.back()]>=nums[i]){
+                minQ.pop_back();
+            }
+            minQ.push_back(i);
+            while(!maxQ.empty() and !minQ.empty() and nums[maxQ.front()]-nums[minQ.front()]>k){
+                if(maxQ.front()==j){
+                    maxQ.pop_front();
+                }
+                if(minQ.front()==j){
+                    minQ.pop_front();
+                }
+                j++;
+            }
+            dp[i+1]=(prefix[i]-(j>0 ? prefix[j-1]: 0)+mod)%mod;
+            prefix[i+1]=(prefix[i]+dp[i+1])%mod;
+        }
+        return dp[n];
+    }
+};
