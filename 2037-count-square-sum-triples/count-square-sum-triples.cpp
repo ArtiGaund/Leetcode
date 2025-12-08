d47e10af47ee786ebee5854a551dd8d547c9182c
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-12-08 15:38:41 +0530

    Time: 6 ms (68.58%) | Memory: 7.8 MB (84.51%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int res=0;
+        for(int a=1;a<=n;a++){
+            for(int b=1;b<=n;b++){
+                int c=int(sqrt(a*a+b*b+1.0));
+                if(c<=n and c*c == a*a+b*b) res++;
+            }
+        }
+        return res;
+    }
+};
