5feb4379bb683feb1f5cbcad6a46cef1f5909c66
Arti Gaund <38074085+ArtiGaund@users.noreply.github.com>
2025-12-23 11:03:56 +0530

    Time: 77 ms (64.48%) | Memory: 161.9 MB (70.27%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        vector<array<int,3>> times;
-        for(auto e:events){
-            times.push_back({e[0],1,e[2]}); //start time
-            times.push_back({e[1]+1,0,e[2]}); //end time
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        sort(events.begin(),events.end());
+        int maxVal=0,maxSum=0;
+        for(auto &event:events){
+            while(pq.size() and pq.top().first<event[0]){
+                maxVal=max(maxVal,pq.top().second);
+                pq.pop();
+            }
+            maxSum=max(maxSum,maxVal+event[2]);
+            pq.push({event[1],event[2]});
         }
-        int res=0,maxVal=0;
-        sort(times.begin(),times.end());
-        for(auto timeVal:times){
-            if(timeVal[1]) res=max(res,timeVal[2]+maxVal);
-            else maxVal=max(maxVal,timeVal[2]);
-        }
-        return res;
+        return maxSum;
     }
 };
